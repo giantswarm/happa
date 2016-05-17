d3bd7febbbdfc856398c8436ddbf610622354aec
Oliver Ponder <oliver.ponder@gmail.com>
2016-05-17 11:58:38 +0200

    rename things so they are inline with 'app' as entry point

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var HappaApp = require('./HappaApp');
+var logo = require('./logo');
 var React = require('react');
 var {Router, Route, browserHistory} = require('react-router');
 var render = require('react-dom').render;
@@ -8,11 +8,11 @@ var render = require('react-dom').render;
 require('normalize.css');
 require('../styles/app.scss');
 
-var content = document.getElementById('app');
+var appContainer = document.getElementById('app');
 
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={HappaApp}>
+    <Route path = "/" component={logo}>
     </Route>
   </Router>
-), content)
+), appContainer)
