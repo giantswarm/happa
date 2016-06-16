3a7fd472bce122a90e2b8602874ade976f6afd23
Oliver Ponder <oliver.ponder@gmail.com>
2016-06-16 13:46:49 +0200

    Adjustments to status messages

--- src/components/signup/status_message.js
+++ src/components/signup/status_message.js
@@ -4,15 +4,15 @@ var React = require('react');
 module.exports = React.createClass({
   statusMessage: function(key) {
     return {
-      "verify_started": <span>Verifying Token...</span>,
-      "verify_completed": <span>Verifying Token... <span className="success">VALID!</span></span>,
-      "verify_failed": <span>Verifying Token... <span className="error">FAILED!</span><br/> The server appears to be down. Please try again later.</span>,
-      "invalid_token": <span>Verifying Token... <span className="error">FAILED!</span><br/> This token is not valid. Perhaps you already used it?</span>,
+      "verify_started": <span>Verifying invite...</span>,
+      "verify_completed": <span>Verifying invite... <span className="success">VALID!</span></span>,
+      "verify_failed": <span>Verifying invite... <span className="error">FAILED!</span><br/> The server appears to be down. Please try again later.</span>,
+      "invalid_token": <span>Verifying invite... <span className="error">FAILED!</span><br/> This invite token is not valid. Perhaps you already used it?</span>,
       "enter_password": <span>Please enter your desired password</span>,
-      "create_account_starting": <span>Creating Account... </span>,
-      "create_account_started": <span>Creating Account... </span>,
-      "create_account_completed": <span>Creating Account... <span className="success">DONE!</span></span>,
-      "create_account_failed": <span>Creating Account... <span className="error">FAILED!</span></span>,
+      "create_account_starting": <span>Creating account... </span>,
+      "create_account_started": <span>Creating account... </span>,
+      "create_account_completed": <span>Creating account... <span className="success">DONE!</span></span>,
+      "create_account_failed": <span>Creating account... <span className="error">FAILED!</span></span>,
       "password_too_short": <span>Please use at least 8 characters</span>,
       "password_not_just_numbers": <span>Please add something else than only numbers</span>,
       "password_not_just_letters": <span>Please add some more diverse characters.</span>,
