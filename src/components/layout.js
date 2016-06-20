1a270a1e1d4e6d0d2615b17d16a58a0672f7b7f6
Oliver Ponder <oliver.ponder@gmail.com>
2016-06-20 15:44:51 +0200

    Add flash messages

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ var {Link, IndexLink}  = require('react-router');
 
 var UserActions = require('./reflux_actions/user_actions');
 var UserStore   = require('./reflux_stores/user_store');
+var FlashMessages = require('./flash_messages/index');
 
 //<Link to="/services/new" activeClassName="active">New Service</Link>
 
@@ -22,7 +23,11 @@ module.exports = React.createClass ({
             </div>
           </div>
         </nav>
-        {this.props.children}
+
+        <div className="main col-8">
+          <FlashMessages />
+          {this.props.children}
+        </div>
       </div>
     );
   }
