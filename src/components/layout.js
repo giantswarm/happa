abd5b8aea54416a1af2ed20effc21cbd3d892984
Oliver Ponder <oliver.ponder@gmail.com>
2016-06-22 15:26:32 +0200

    Add page not found behavior
    Uses react router recommended way of adding a catch all route.
    Layout now mixes in the UserStore so we are guaranteed to initialize it
    even when coming in to the page not found handler directly.
    
    Also made the logout and 'you are logged in as' bits of the main
    navigation only visibile if there is a currently logged in user.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+var Reflux = require('reflux');
 var {Link, IndexLink}  = require('react-router');
 
 var UserActions = require('./reflux_actions/user_actions');
@@ -9,6 +10,8 @@ var UserStore   = require('./reflux_stores/user_store');
 //<Link to="/services/new" activeClassName="active">New Service</Link>
 
 module.exports = React.createClass ({
+  mixins: [Reflux.connect(UserStore,'user'), Reflux.listenerMixin],
+
   render: function() {
     return (
       <div>
@@ -17,8 +20,8 @@ module.exports = React.createClass ({
             <a href="https://giantswarm.io" target="_blank"><img className="logo" src="/images/giantswarm_icon.svg" /></a>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
             <div className="subactions">
-              <span className="currentuser">Logged in as: {UserStore.currentUser().email}</span>
-              <Link to="/logout" activeClassName="active">Logout</Link>
+              { UserStore.currentUser().authenticated ? <span className="currentuser">Logged in as: {UserStore.currentUser().email}</span> : null }
+              { UserStore.currentUser().authenticated ? <Link to="/logout" activeClassName="active">Logout</Link> : null }
             </div>
           </div>
         </nav>
