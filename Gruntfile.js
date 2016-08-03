9912409b9e1081bdb243981c1e2e19bdfdd7b13e
Oliver Ponder <oliver.ponder@gmail.com>
2016-08-03 14:05:59 +0200

    Add cache busting for production app.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,6 +109,17 @@ module.exports = function (grunt) {
       }
     },
 
+    cacheBust: {
+      taskName: {
+        options: {
+            baseDir: '<%= pkg.dist %>',
+            assets: ['assets/**'],
+            deleteOriginals: true,
+        },
+        src: ['<%= pkg.dist %>/index.html']
+      }
+    },
+
     clean: {
       dist: {
         options: {
@@ -138,7 +149,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('test', ['karma']);
 
-  grunt.registerTask('build', ['copy', 'webpack']);
+  grunt.registerTask('build', ['copy', 'webpack', 'cacheBust']);
 
   grunt.registerTask('default', []);
 };
