bc4bb2897365455b1266c6a3aa39ad9dc39ae468
Puja <puja108@gmail.com>
2016-11-03 12:12:51 +0100

    removed the yaml

--- src/components/docs/3_simple_example.js
+++ src/components/docs/3_simple_example.js
@@ -76,63 +76,9 @@ var SimpleExample = React.createClass ({
         <Slide>
           <h1>Let&apos;s create an example application</h1>
           <p>To check if every part of your cluster is running as it should, let&apos;s create an entire application. When set up, this application will provide a little web server running in multiple pods.</p>
-          <p>Here is the manifest we need:</p>
-
-          <FileBlock fileName='helloworld-manifest.yaml'>
-          {`
-          apiVersion: v1
-          kind: Service
-          metadata:
-            name: helloworld
-            labels:
-              app: helloworld
-            namespace: default
-          spec:
-            type: NodePort
-            ports:
-            - port: 8080
-            selector:
-              app: helloworld
-          ---
-          apiVersion: extensions/v1beta1
-          kind: Deployment
-          metadata:
-            name: helloworld
-            labels:
-              app: helloworld
-          spec:
-            replicas: 2
-            selector:
-              matchLabels:
-                app: helloworld
-            template:
-              metadata:
-                labels:
-                  app: helloworld
-                namespace: default
-              spec:
-                containers:
-                - name: helloworld
-                  image: giantswarm/helloworld:latest
-                  ports:
-                  - containerPort: 8080
-          ---
-          apiVersion: extensions/v1beta1
-          kind: Ingress
-          metadata:
-            labels:
-              app: helloworld
-            name: helloworld
-            namespace: default
-          spec:
-            backend:
-              serviceName: helloworld
-              servicePort: 8080
-          `}
-          </FileBlock>
+          <p>We use <code>kubectl</code> to create the service, deployment, and ingress resource from a manifest hosted on GitHub.</p>
 
           <p><i className='fa fa-graduation-cap' title='For learners'></i> If you&apos;re new to Kubernetes: A manifest describes things to create in Kubernetes. In this case the manifest describes two different things, a service and a deployment. The service is there to expose containers (here: the ones with the label app: helloworld) inside your cluster via a certain hostname and port. The deployment describes your helloworld deployment. It manages a replica set, which ensures that a number of pods (two, actually) containing Docker containers from a certain image are running.</p>
-          <p>Now use <code>kubectl</code> to create the service, deployment, and ingress resource:</p>
 
           <CodeBlock>
             <Prompt>
