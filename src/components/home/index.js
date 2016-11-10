184c78a06fddcb9b86f35044c2554421e1082b70
Oliver Ponder <oliver.ponder@gmail.com>
2016-11-10 09:50:19 +0100

    Add link to prom guide

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -69,6 +69,7 @@ var Home = React.createClass({
             <ClusterDashboard cluster={cluster} key={cluster.id + 'error'} className='empty-slate'>
               <h1>Couldn't load metrics for cluster <code>{cluster.id}</code></h1>
               <p>We're currently improving our metrics gathering.</p>
+              <p>If you need metrics, you can <a href='https://docs.giantswarm.io/guides/kubernetes-prometheus/' target="_blank">set up your own monitoring with Prometheus easily</a></p>
               <p>Thanks for your patience! If you have any questions don't hesitate to contact support: <a href='mailto:support@giantswarm.io'>support@giantswarm.io</a></p>
             </ClusterDashboard>
             :
