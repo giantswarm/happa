4cc641e14b2ef55717c07b50af58c52f89ec4e2b
Oliver Ponder <oliver.ponder@gmail.com>
2016-11-14 11:48:57 +0100

    Also apply some fading out to barcharts and show ellipses on labels if they become outdated

--- src/components/home/node_row.js
+++ src/components/home/node_row.js
@@ -33,6 +33,7 @@ var NodeRow = React.createClass({
             percentage={this.props.node.ram_used.value / this.props.node.ram_available.value}
             label={this.labelifyBytes(this.props.node.ram_used.value)}
             animate={this.props.animate}
+            outdated={this.props.node.ram_used.outdated || this.props.node.ram_available.outdated}
             color="#003c78" />
         </td>
         <td className="node-table--barchart-container">
@@ -40,6 +41,7 @@ var NodeRow = React.createClass({
             percentage={this.props.node.cpu_used.value}
             label={(this.props.node.cpu_used.value * 100).toFixed(0) + '%'}
             animate={this.props.animate}
+            outdated={this.props.node.cpu_used.outdated}
             color="#3ab6c7" />
         </td>
         <td className="node-table--barchart-container node-table--barchart-storage">
@@ -47,22 +49,47 @@ var NodeRow = React.createClass({
             percentage={this.props.node.node_storage_used.value / this.props.node.node_storage_limit.value}
             label={this.labelifyBytes(this.props.node.node_storage_used.value)}
             animate={this.props.animate}
+            outdated={this.props.node.node_storage_used.outdated}
             color="#d68a10"/>
 
         </td>
         <td className="node-table--pod-count">
-          {this.props.node.pod_count.value}
+          {
+            this.props.node.pod_count.outdated ?
+            '...'
+            :
+            this.props.node.pod_count.value
+          }
         </td>
         <td className="node-table--container-count">
-          {this.props.node.container_count.value}
+          {
+            this.props.node.container_count.outdated ?
+            '...'
+            :
+            this.props.node.container_count.value
+          }
         </td>
         <td className="node-table--net-in-value">
-          {humanFileSize(this.props.node.network_traffic_incoming.value).value}
-          <span className="node-table--metric-unit">{humanFileSize(this.props.node.network_traffic_outgoing.value).unit}/Sec</span>
+          {
+            this.props.node.network_traffic_incoming.outdated ?
+            '...'
+            :
+            <div>
+              {humanFileSize(this.props.node.network_traffic_incoming.value).value}
+              <span className="node-table--metric-unit">{humanFileSize(this.props.node.network_traffic_outgoing.value).unit}/Sec</span>
+            </div>
+          }
         </td>
         <td className="node-table--net-out-value">
-          {humanFileSize(this.props.node.network_traffic_outgoing.value).value}
-          <span className="node-table--metric-unit">{humanFileSize(this.props.node.network_traffic_outgoing.value).unit}/Sec</span>
+          {
+            this.props.node.network_traffic_outgoing.outdated ?
+            '...'
+            :
+            <div>
+              {humanFileSize(this.props.node.network_traffic_outgoing.value).value}
+              <span className="node-table--metric-unit">{humanFileSize(this.props.node.network_traffic_outgoing.value).unit}/Sec</span>
+            </div>
+          }
         </td>
       </tr>
     );
