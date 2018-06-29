8dcf0f4c6b60b8134909714479fd8bef526cada7
Marian Steinbach <marian@giantswarm.io>
2018-06-29 11:10:11 +0200

    Allow upgrading for Azure

--- src/components/organizations/cluster_detail.js
+++ src/components/organizations/cluster_detail.js
@@ -130,14 +130,18 @@ class ClusterDetail extends React.Component {
 
   // Determine whether the current cluster can be upgraded
   canClusterUpgrade() {
-    // provider must be AWS
-    if (this.props.provider !== 'aws') return false;
+    // provider must be AWS or Azure
+    if (this.props.provider !== 'aws' && this.props.provider !== 'azure') return false;
 
     // cluster must have a release_version
     if (this.props.cluster.release_version === '') return false;
 
-    // cluster release_version must be > 3
-    if (cmp(this.props.cluster.release_version, '3.0.0') === -1) return false;
+    // cluster release_version must be > 3 on AWS and > 1 on Azure
+    if (this.props.provider === 'aws') {
+      if (cmp(this.props.cluster.release_version, '3.0.0') === -1) return false;
+    } else if (this.props.provider === 'azure') {
+      if (cmp(this.props.cluster.release_version, '1.0.0') === -1) return false;
+    }
 
     // a target release to upgrade to must be defined
     if (!!this.props.targetRelease !== true) return false;
