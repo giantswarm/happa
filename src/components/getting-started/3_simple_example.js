d28fee8a579956d5990914066f8b5fc7ee861dc0
Oliver Ponder <oliver.ponder@gmail.com>
2018-07-11 14:33:37 +0800

    Upgrade eslint, fix reported errors.

--- src/components/getting-started/3_simple_example.js
+++ src/components/getting-started/3_simple_example.js
@@ -72,7 +72,7 @@ class SimpleExample extends React.Component {
       } else {
         var url = `http://helloworld.${this.clusterBaseDomain()}`;
         return (
-          <a href={url} target='_blank'>{url}</a>
+          <a href={url} target='_blank' rel='noopener noreferrer'>{url}</a>
         );
       }
     }
