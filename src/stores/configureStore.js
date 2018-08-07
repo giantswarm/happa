6c259540134ae57a65b61cb40deacbc92e8a666c
Oliver Nicolaas Ponder <oliver.ponder@gmail.com>
2018-08-07 17:50:23 +0800

    Get the unauthorized action firing again instead of redirecting the browser. (#360)
    * Use update js client, remove old js client.
    
    * uncomment accidentally commented code.
    
    * Dispatch unauthorized event instead of window.location change.

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -4,10 +4,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+import history from './history';
 
 var store;
 
-export default function configureStore(initialState, history) {
+export default function configureStore(initialState) {
   if (store) {
     return store;
   } else {
---
+++ src/stores/history.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export default history;
