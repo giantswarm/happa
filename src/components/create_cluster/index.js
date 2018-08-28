f982b5d68735c57ccf7a7bb701d9c67654f73167
Marian Steinbach <marian@giantswarm.io>
2018-08-28 11:40:54 +0200

    Remove master sizing row from cluster creation

--- src/components/create_cluster/index.js
+++ src/components/create_cluster/index.js
@@ -325,15 +325,6 @@ class CreateCluster extends React.Component {
               </div>
             </div>
 
-            <div className='row'>
-              <div className='col-3'>
-                <h3 className='table-label'>Master Sizing</h3>
-              </div>
-              <div className='col-9'>
-                <p>Auto Sized (Default)</p>
-              </div>
-            </div>
-
             <div className='row section new-cluster--launch'>
               <div className='col-12'>
                 <p>Create this cluster now and it will be available for you to use as soon as possible.</p>
