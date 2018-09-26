e9bd8e8c414bd2069bf8c3f2c76c3cac23fd51e1
Marian Steinbach <marian@giantswarm.io>
2018-09-26 12:17:45 +0200

    Unlock upgrade for KVM release version >= 2.7.0

--- src/components/organizations/cluster_detail.js
+++ src/components/organizations/cluster_detail.js
@@ -136,11 +136,6 @@ class ClusterDetail extends React.Component {
       return false;
     }
 
-    // provider must be AWS or Azure
-    if (this.props.provider !== 'aws' && this.props.provider !== 'azure') {
-      return false;
-    }
-
     // cluster must have a release_version
     if (this.props.cluster.release_version === '') return false;
 
@@ -149,13 +144,17 @@ class ClusterDetail extends React.Component {
       return false;
     }
 
-    // cluster release_version must be > 3 on AWS and > 1 on Azure
+    // cluster release_version must be >= 3 on AWS, >= 1 on Azure, >= 2.7.0
     if (this.props.provider === 'aws') {
       if (cmp(this.props.cluster.release_version, '3.0.0') === -1) {
         return false;
       }
     } else if (this.props.provider === 'azure') {
       if (cmp(this.props.cluster.release_version, '1.0.0') === -1) {
+        return false;
+      }
+    } else if (this.props.provider === 'kvm') {
+      if (cmp(this.props.cluster.release_version, '2.7.0') === -1) {
         return false;
       }
     }
