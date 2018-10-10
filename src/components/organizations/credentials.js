e6246b97d0dc0739405431315762b2b25329aa8a
Marian Steinbach <marian@giantswarm.io>
2018-10-10 21:11:46 +0200

    More changes to credential display

--- src/components/organizations/credentials.js
+++ src/components/organizations/credentials.js
@@ -13,7 +13,6 @@ class Credentials extends React.Component {
   render() {
     var provider = this.props.app.info.general.provider;
 
-    console.log('this.props.credentials:', this.props.credentials);
     if (this.props.credentials.isFetching) {
       return (
         <span>
@@ -23,11 +22,23 @@ class Credentials extends React.Component {
       );
     } else {
       if (this.props.credentials.items.length === 0) {
+
+        var gsctlInfo = <p>You can set credentials to use your own subscription using <a href='https://docs.giantswarm.io/reference/gsctl/update-org-set-credentials/' target='_blank' rel='noopener noreferrer'>our CLI</a></p>;
         // TODO: show button for setting credentials
         if (provider === 'azure') {
-          return <p>No credentials set. Clusters of this organization will be created in the default tenant cluster subscription configured for this installation.</p>;
+          return (
+            <div>
+              <p>No credentials set. Clusters of this organization will be created in the default tenant cluster subscription configured for this installation.</p>
+              { gsctlInfo }
+            </div>
+          );
         }
-        return <p>No credentials set. Clusters of this organization will be created in the default tenant cluster account of this installation.</p>;
+        return (
+          <div>
+            <p>No credentials set. Clusters of this organization will be created in the default tenant cluster account of this installation.</p>
+            { gsctlInfo }
+          </div>
+        );
 
       } else {
         /**
@@ -35,50 +46,61 @@ class Credentials extends React.Component {
          * API currently won't allow for more than that.
          */
 
+        var providerWarning;
         if (typeof this.props.credentials.items[0][provider] === 'undefined') {
-          return <p>Credentials details not matching expectations for provider {provider}</p>;
+          providerWarning = <p>Credentials details not matching expectations for provider {provider}</p>;
         }
 
-        if (provider === 'aws') {
+        // AWS credential
+        if (typeof this.props.credentials.items[0].aws !== 'undefined') {
           // get account ID from ARN like 'arn:aws:iam::<YOUR_ACCOUNT_ID>:role/GiantSwarmAdmin'
           var parts = this.props.credentials.items[0].aws.roles.awsoperator.split(':');
           var accountID = parts[4];
           return (
-            <table className='table resource-details'>
-              <tbody>
-                <tr key='account_id'>
-                  <td>AWS account ID</td>
-                  <td className='value'>{ accountID }</td>
-                </tr>
-                <tr key='awsoperator_role'>
-                  <td>AWSOperator role ARN</td>
-                  <td className='value code'>{ this.props.credentials.items[0].aws.roles.awsoperator }</td>
-                </tr>
-                <tr key='admin_role'>
-                  <td>Admin role ARN</td>
-                  <td className='value code'>{ this.props.credentials.items[0].aws.roles.admin }</td>
-                </tr>
-              </tbody>
-            </table>
+            <div>
+              {providerWarning}
+              <table className='table resource-details' id={'credential-' + this.props.credentials.items[0].id}>
+                <tbody>
+                  <tr key='account_id'>
+                    <td>AWS account ID</td>
+                    <td className='value code'>{ accountID } <a href={'https://' + accountID + '.signin.aws.amazon.com/console'} target='_blank' rel='noopener noreferrer'><i className='fa fa-external-link'></i></a></td>
+                  </tr>
+                  <tr key='awsoperator_role'>
+                    <td>AWSOperator role ARN</td>
+                    <td className='value code'>{ this.props.credentials.items[0].aws.roles.awsoperator }</td>
+                  </tr>
+                  <tr key='admin_role'>
+                    <td>Admin role ARN</td>
+                    <td className='value code'>{ this.props.credentials.items[0].aws.roles.admin }</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           );
-        } else if (provider === 'azure') {
+        
+        // Azure credential
+        } else if (typeof this.props.credentials.items[0].azure !== 'undefined') {
+          console.log(this.props.credentials.items[0].azure);
           return (
-            <table className='table resource-details'>
-              <tbody>
-                <tr key='account_id'>
-                  <td>Azure subscription ID</td>
-                  <td className='value'>{ this.props.credentials.items[0].azure.credential.subscription_id }</td>
-                </tr>
-                <tr key='awsoperator_role'>
-                  <td>Azure tenant ID</td>
-                  <td className='value code'>{ this.props.credentials.items[0].azure.credential.tenant_id }</td>
-                </tr>
-                <tr key='admin_role'>
-                  <td>Service principal client ID</td>
-                  <td className='value code'>{ this.props.credentials.items[0].azure.credential.client_id }</td>
-                </tr>
-              </tbody>
-            </table>
+            <div>
+              {providerWarning}
+              <table className='table resource-details'>
+                <tbody>
+                  <tr key='account_id'>
+                    <td>Azure subscription ID</td>
+                    <td className='value'>{ this.props.credentials.items[0].azure.credential.subscription_id }</td>
+                  </tr>
+                  <tr key='awsoperator_role'>
+                    <td>Azure tenant ID</td>
+                    <td className='value code'>{ this.props.credentials.items[0].azure.credential.tenant_id }</td>
+                  </tr>
+                  <tr key='admin_role'>
+                    <td>Service principal client ID</td>
+                    <td className='value code'>{ this.props.credentials.items[0].azure.credential.client_id }</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           );
         }
       }
