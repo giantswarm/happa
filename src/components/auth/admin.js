fa8e0da23da881d29e39a7940aad5b8e85401c09
Oliver Ponder <oliver.ponder@gmail.com>
2018-11-30 16:28:25 +0800

    Make admin-login page a bit smarter and faster.

--- src/components/auth/admin.js
+++ src/components/auth/admin.js
@@ -7,12 +7,41 @@ import { flashClearAll } from '../../actions/flashMessageActions';
 import * as userActions from '../../actions/userActions';
 import { bindActionCreators } from 'redux';
 import Auth0 from '../../lib/auth0';
+import {isJwtExpired} from '../../lib/helpers';
 import PropTypes from 'prop-types';
+import { push } from 'connected-react-router';
 
 class AdminLogin extends React.Component {
   componentDidMount() {
     const auth0 = new Auth0();
-    auth0.login();
+
+    if (this.props.user && this.props.user.auth && this.props.user.auth.scheme === 'Bearer') {
+      if(isJwtExpired(this.props.user.auth.token)) {
+        // Token is expired. Try to renew it silently, and if that succeeds, redirect
+        // the user to the dashboard. Otherwise, send them to Auth0 to refresh the token that way.
+        auth0
+          .renewToken()
+          .then(result => {
+            // Update state with new token.
+            this.props.dispatch(userActions.auth0Login(result));
+
+            // Redirect to dashboard.
+            this.props.dispatch(push('/'));
+          })
+          .catch(() => {
+            // Unable to refresh token silently, so send the down the auth0
+            // flow.
+            auth0.login();
+          });
+      } else {
+        // Token isn't expired yet, so just redirect the user to the dashboard.
+        this.props.dispatch(push('/'));
+      }
+    } else {
+      // User doesn't have any previous token at all, send them to auth0 so
+      // they can get one.
+      auth0.login();
+    }
   }
 
   componentWillUnmount() {
@@ -30,7 +59,7 @@ class AdminLogin extends React.Component {
           </div>
 
           <img className='loader' src='/images/loader_oval_light.svg' />
-          <p>Redirecting you to our authentication provider.</p>
+          <p>Verifying credentials, and redirecting to our authentication provider if necessary.</p>
           <p>If nothing happens please let us know in #support.</p>
         </div>
       </div>
@@ -42,6 +71,7 @@ AdminLogin.propTypes = {
   dispatch: PropTypes.func,
   flashMessages: PropTypes.object,
   actions: PropTypes.object,
+  user: PropTypes.object
 };
 
 function mapStateToProps(state) {
