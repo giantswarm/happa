b8b0519e58fc0406680c15f5c2b75a1dde2ba9c3
Tim Schindler <xh3b4sd@gmail.com>
2018-12-10 11:14:28 +0100

    adds first UI changes (#425)

--- src/components/create_cluster/index.js
+++ src/components/create_cluster/index.js
@@ -4,12 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import DocumentTitle from 'react-document-title';
 import Button from '../button';
-import _ from 'underscore';
 import { clusterCreate } from '../../actions/clusterActions';
-import update from 'react-addons-update';
-import NewKVMWorker from './new_kvm_worker.js';
-import NewAWSWorker from './new_aws_worker.js';
-import NewAzureWorker from './new_azure_worker.js';
 import NumberPicker from './number_picker.js';
 import ReleaseSelector from './release_selector.js';
 import ProviderCredentials from './provider_credentials.js';
@@ -23,37 +18,10 @@ class CreateCluster extends React.Component {
     availabilityZones: 1,
     releaseVersion: '',
     clusterName: 'My cluster',
-    workerCount: 3,
-    syncWorkers: true,
-    workers: [
-      {
-        id: 1,
-        cpu: 1,
-        memory: 1,
-        storage: 10,
-        instanceType: 'm3.large',
-        vmSize: 'Standard_D2s_v3',
-        valid: true,
-      },
-      {
-        id: 2,
-        cpu: 1,
-        memory: 1,
-        storage: 10,
-        instanceType: 'm3.large',
-        vmSize: 'Standard_D2s_v3',
-        valid: true,
-      },
-      {
-        id: 3,
-        cpu: 1,
-        memory: 1,
-        storage: 10,
-        instanceType: 'm3.large',
-        vmSize: 'Standard_D2s_v3',
-        valid: true,
-      },
-    ],
+    scaling: {
+      min: 3,
+      max: 3,
+    },
     submitting: false,
     valid: false, // Start off invalid now since we're not sure we have a valid release yet, the release endpoint could be malfunctioning.
     error: false,
@@ -65,81 +33,27 @@ class CreateCluster extends React.Component {
     });
   };
 
+  updateScalingMin = n => {
+    this.setState({
+      scaling: {
+        min: n,
+        max: this.state.scaling.max,
+      },
+    });
+  };
+
+  updateScalingMax = n => {
+    this.setState({
+      scaling: {
+        min: this.state.scaling.min,
+        max: n,
+      },
+    });
+  };
+
   updateClusterName = event => {
     this.setState({
       clusterName: event.target.value,
-    });
-  };
-
-  updateWorkerCount = e => {
-    this.setState({
-      workerCount: e.target.value,
-    });
-  };
-
-  deleteWorker = workerIndex => {
-    var workers = _.without(
-      this.state.workers,
-      this.state.workers[workerIndex]
-    );
-
-    this.setState({
-      workers: workers,
-    });
-  };
-
-  addWorker = () => {
-    var newDefaultWorker;
-
-    if (this.state.syncWorkers) {
-      newDefaultWorker = {
-        id: Date.now(),
-        cpu: this.state.workers[0].cpu,
-        memory: this.state.workers[0].memory,
-        storage: this.state.workers[0].storage,
-        instanceType: this.state.workers[0].instanceType,
-        vmSize: this.state.workers[0].vmSize,
-        valid: this.state.workers[0].valid,
-      };
-    } else {
-      newDefaultWorker = {
-        id: Date.now(),
-        cpu: 1,
-        memory: 1,
-        storage: 20,
-        instanceType: 'm3.large',
-        vmSize: 'Standard_D2s_v3',
-        valid: true,
-      };
-    }
-
-    var workers = [].concat(this.state.workers, newDefaultWorker);
-
-    this.setState({
-      workers: workers,
-    });
-  };
-
-  syncWorkersChanged = e => {
-    var workers = this.state.workers;
-    var worker1 = this.state.workers[0];
-
-    if (e.target.checked) {
-      workers = workers.map(worker => {
-        return {
-          id: worker.id,
-          cpu: worker1.cpu,
-          memory: worker1.memory,
-          storage: worker1.storage,
-          instanceType: worker1.instanceType,
-          valid: worker1.valid,
-        };
-      });
-    }
-
-    this.setState({
-      syncWorkers: e.target.checked,
-      workers: workers,
     });
   };
 
@@ -148,43 +62,48 @@ class CreateCluster extends React.Component {
       submitting: true,
     });
 
+    var i;
     var workers = [];
 
-    // TODO/FYI: This IF / ELSE on this.props.provider is a antipattern
-    // that will spread throughout the codebase if we are not careful.
-    // I am waiting for the 'third' cluster type that we support to be able to make
-    // decisions about a meaningful abstraction. For now, going with a easy solution.
+    // TODO/FYI: This IF / ELSE on this.props.provider is a antipattern that
+    // will spread throughout the codebase if we are not careful. I am waiting
+    // for the 'third' cluster type that we support to be able to make decisions
+    // about a meaningful abstraction. For now, going with a easy solution.
 
     if (this.props.provider === 'aws') {
-      workers = this.state.workers.map(worker => {
-        return {
+      for (i = 0; i < this.state.scaling.min; i++) {
+        workers.push({
           aws: {
-            instance_type: worker.instanceType,
+            instance_type: 'm3.large', // TODO
           },
-        };
-      });
+        });
+      }
     } else if (this.props.provider === 'azure') {
-      workers = this.state.workers.map(worker => {
-        return {
+      for (i = 0; i < this.state.scaling.min; i++) {
+        workers.push({
           azure: {
-            vm_size: worker.vmSize,
+            vm_size: 'Standard_A2_v2', // TODO
           },
-        };
-      });
+        });
+      }
     } else {
-      workers = this.state.workers.map(worker => {
-        return {
-          memory: { size_gb: worker.memory },
-          storage: { size_gb: worker.storage },
-          cpu: { cores: worker.cpu },
-        };
-      });
+      for (i = 0; i < this.state.scaling.min; i++) {
+        workers.push({
+          memory: { size_gb: 0 }, // TODO
+          storage: { size_gb: 0 }, // TODO
+          cpu: { cores: 0 }, // TODO
+        });
+      }
     }
 
     this.props
       .dispatch(
         clusterCreate({
           availability_zones: this.state.availabilityZones,
+          scaling: {
+            min: this.state.scaling.min,
+            max: this.state.scaling.max,
+          },
           name: this.state.clusterName,
           owner: this.props.selectedOrganization,
           release_version: this.state.releaseVersion,
@@ -215,47 +134,6 @@ class CreateCluster extends React.Component {
     this.cluster_name.select();
   }
 
-  updateWorker(index, newWorker) {
-    var newState;
-
-    if (this.state.syncWorkers) {
-      var workers = this.state.workers.map(worker => {
-        worker.storage = newWorker.storage;
-        worker.cpu = newWorker.cpu;
-        worker.memory = newWorker.memory;
-        worker.instanceType = newWorker.instanceType;
-        worker.vmSize = newWorker.vmSize;
-        return worker;
-      });
-      newState = update(this.state, {
-        workers: { $set: workers },
-      });
-    } else {
-      newState = update(this.state, {
-        workers: { $splice: [[index, 1, newWorker]] },
-      });
-    }
-
-    this.setState(newState);
-  }
-
-  valid = () => {
-    // If any of the workers aren't valid, return false
-    var workers = this.state.workers;
-    for (var i = 0; i < workers.length; i++) {
-      if (!workers[i].valid) {
-        return false;
-      }
-    }
-
-    // If any of the releaseVersion hasn't been set yet, return false
-    if (this.state.releaseVersion === '') {
-      return false;
-    }
-
-    return true;
-  };
-
   selectRelease = releaseVersion => {
     this.setState({
       releaseVersion,
@@ -277,6 +155,10 @@ class CreateCluster extends React.Component {
         )}
       </div>
     );
+  }
+
+  valid() {
+    return true;
   }
 
   render() {
@@ -310,6 +192,7 @@ class CreateCluster extends React.Component {
                     Give your cluster a name so you can recognize it in a crowd.
                   </p>
                   <input
+                    className='col-4'
                     ref={i => {
                       this.cluster_name = i;
                     }}
@@ -378,88 +261,46 @@ class CreateCluster extends React.Component {
               </div>
             </div>
 
-            <div>
-              <div className='row section'>
-                <div className='col-12'>
-                  <h3 className='table-label'>Worker Node Configuration</h3>
-                  {this.props.provider === 'kvm' ? (
-                    <div className='checkbox'>
-                      <label htmlFor='syncWorkers'>
-                        <input
-                          type='checkbox'
-                          ref={i => {
-                            this.syncWorkers = i;
-                          }}
-                          id='syncWorkers'
-                          onChange={this.syncWorkersChanged}
-                          checked={this.state.syncWorkers}
-                        />
-                        Use same configuration for all worker nodes
-                      </label>
-                    </div>
-                  ) : (
-                    undefined
-                  )}
-                </div>
+            <div className='row section'>
+              <div className='col-3'>
+                <h3 className='table-label'>Node Count</h3>
               </div>
-              <div className='row'>
-                {this.state.workers.map((worker, index) => {
-                  if (this.props.provider === 'aws') {
-                    return (
-                      <NewAWSWorker
-                        key={'Worker ' + worker.id}
-                        allowedInstanceTypes={this.props.allowedInstanceTypes}
-                        worker={worker}
-                        index={index}
-                        readOnly={this.state.syncWorkers && index !== 0}
-                        deleteWorker={this.deleteWorker.bind(this, index)}
-                        onWorkerUpdated={this.updateWorker.bind(this, index)}
-                      />
-                    );
-                  } else if (this.props.provider === 'kvm') {
-                    return (
-                      <NewKVMWorker
-                        key={'Worker ' + worker.id}
-                        worker={worker}
-                        index={index}
-                        readOnly={this.state.syncWorkers && index !== 0}
-                        deleteWorker={this.deleteWorker.bind(this, index)}
-                        onWorkerUpdated={this.updateWorker.bind(this, index)}
-                      />
-                    );
-                  } else if (this.props.provider === 'azure') {
-                    return (
-                      <NewAzureWorker
-                        key={'Worker ' + worker.id}
-                        allowedVMSizes={this.props.allowedVMSizes}
-                        worker={worker}
-                        index={index}
-                        readOnly={this.state.syncWorkers && index !== 0}
-                        deleteWorker={this.deleteWorker.bind(this, index)}
-                        onWorkerUpdated={this.updateWorker.bind(this, index)}
-                      />
-                    );
-                  }
-                })}
-                <div
-                  className={
-                    'col-4 new-cluster--add-worker-button ' +
-                    (this.state.workers.length < 3 ? 'warning' : '')
-                  }
-                  onClick={this.addWorker}
+              <div className='col-9'>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                  }}
                 >
-                  <div className='new-cluster--add-worker-button-title'>
-                    Add a worker
-                  </div>
-                  {this.state.workers.length < 3 ? (
-                    <div className='new-cluster--low-worker-warning'>
-                      We recommend that you have at least three worker nodes in
-                      a cluster
+                  <div>
+                    <p>
+                      To disable autoscaling, set both numbers to the same value
+                    </p>
+                    <div className='col-3'>
+                      <p>Minimum</p>
+                      <NumberPicker
+                        label=''
+                        stepSize={1}
+                        value={this.state.scaling.min}
+                        min={1}
+                        max={this.state.scaling.max}
+                        onChange={this.updateScalingMin}
+                        readOnly={false}
+                      />
                     </div>
-                  ) : (
-                    ''
-                  )}
-                </div>
+                    <div className='col-3'>
+                      <p>Maximum</p>
+                      <NumberPicker
+                        label=''
+                        stepSize={1}
+                        value={this.state.scaling.max}
+                        min={this.state.scaling.min}
+                        max={99} // TODO
+                        onChange={this.updateScalingMax}
+                        readOnly={false}
+                      />
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
 
