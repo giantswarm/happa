59a325a876ee92740a481a4e5cf15d50aa88546c
Oliver Ponder <oliver.ponder@gmail.com>
2019-01-23 11:21:42 +0800

    Add versions, adjust titles to match what comes out of the release endpoint.

--- src/components/cluster_detail/cluster_apps.js
+++ src/components/cluster_detail/cluster_apps.js
@@ -3,25 +3,47 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
+// This component gets a list of managed services from the release endpoint
+// and tries to organize them in a predefined way.
+// The `preinstalledAppCategories` object below is a mapping of components
+// to logos and categories.
+
+// Since some components are not yet in the release endpoint output, but we do
+// still want to see them on this page, we manually add them to the release endpoint
+// response before running the mapping.
+
 class ClusterApps extends React.Component {
   preinstalledAppCategories = [
     {
       title: 'Essentials',
       apps: [
         {
-          title: 'Kubernetes Components',
-          logoUrl: '/images/app_icons/kubernetes_components@2x.png',
-        },
-        {
-          title: 'CoreDNS',
-          logoUrl: '/images/app_icons/coredns@2x.png',
-        },
-        {
-          title: 'Calico',
+          title: 'calico',
           logoUrl: '/images/app_icons/calico@2x.png',
         },
         {
+          title: 'containerlinux',
+          logoUrl: '/images/app_icons/container_linux@2x.png',
+        },
+        {
+          title: 'coredns',
+          logoUrl: '/images/app_icons/coredns@2x.png',
+        },
+        {
+          title: 'docker',
+          logoUrl: '/images/app_icons/docker@2x.png',
+        },
+        {
+          title: 'kubernetes',
+          logoUrl: '/images/app_icons/kubernetes@2x.png',
+        },
+        {
+          title: 'etcd',
+          logoUrl: '/images/app_icons/etcd@2x.png',
+        },
+        {
           title: 'RBAC and PSP defaults',
+          version: '',
           logoUrl: '/images/app_icons/rbac_and_psp_defaults@2x.png',
         },
       ],
@@ -30,20 +52,24 @@ class ClusterApps extends React.Component {
       title: 'Management',
       apps: [
         {
-          title: 'Tiller',
-          logoUrl: '/images/app_icons/tiller@2x.png',
-        },
-        {
-          title: 'chart-operator',
-          logoUrl: '/images/app_icons/chart_operator@2x.png',
+          title: 'kube-state-metrics',
+          logoUrl: '/images/app_icons/kube_state_metrics@2x.png',
         },
         {
           title: 'node-exporter',
           logoUrl: '/images/app_icons/node_exporter@2x.png',
         },
         {
-          title: 'kube-state-metrics',
-          logoUrl: '/images/app_icons/kube_state_metrics@2x.png',
+          title: 'chart-operator',
+          logoUrl: '/images/app_icons/chart_operator@2x.png',
+        },
+        {
+          title: 'cert-exporter',
+          logoUrl: '/images/app_icons/chart_operator@2x.png',
+        },
+        {
+          title: 'net-exporter',
+          logoUrl: '/images/app_icons/chart_operator@2x.png',
         },
       ],
     },
@@ -51,7 +77,7 @@ class ClusterApps extends React.Component {
       title: 'Ingress',
       apps: [
         {
-          title: 'NGINX Ingress Controller',
+          title: 'nginx-ingress-controller',
           logoUrl: '/images/app_icons/nginx_ingress_controller@2x.png',
         },
       ],
@@ -77,6 +103,7 @@ class ClusterApps extends React.Component {
                       <div className='cluster-apps--app' key={app.title}>
                         <img src={app.logoUrl} alt={app.title + ' icon'} />
                         {app.title}
+                        <small>{app.version}&nbsp;</small>
                       </div>
                     );
                   })}
