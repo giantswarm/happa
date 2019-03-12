97e0e0d7ac753ada1d212565b786b8c5abc93e10
Oliver Ponder <oliver.ponder@gmail.com>
2019-03-12 19:15:36 +0800

    Finalise and small tweaks to app catalog detail page.

--- src/components/app_catalog/detail/index.js
+++ src/components/app_catalog/detail/index.js
@@ -26,22 +26,112 @@ class AppDetail extends React.Component {
           }}
         >
           <div className='app-detail'>
+            <Link to='/app-katalog'>
+              <i className='fa fa-chevron-left' aria-hidden='true' />
+              Back to the App Katalog
+            </Link>
+
+            <br />
+            <br />
             <div className='app-detail--header clearfix'>
-              <img src={this.props.appVersions[0].icon} />
-              <h2>{this.props.appVersions[0].name}</h2>
-              {this.props.appVersions[0].keywords.map(x => (
-                <span key={x} className='keyword'>
-                  {x}
-                </span>
-              ))}
+              <div className='app-detail--icon'>
+                <img src={this.props.appVersions[0].icon} />
+              </div>
+
+              <div className='app-detail--title'>
+                <h2>{this.props.appVersions[0].name}</h2>
+                <div className='keywords'>
+                  {this.props.appVersions[0].keywords
+                    ? this.props.appVersions[0].keywords.map(x => (
+                        <span key={x} className='keyword'>
+                          {x}
+                        </span>
+                      ))
+                    : ''}
+                </div>
+
+                <div className='version'>
+                  <small>Version</small>{' '}
+                  <code>{this.props.appVersions[0].version}</code>
+                  <small>App Version</small>{' '}
+                  <code>{this.props.appVersions[0].appVersion}</code>
+                </div>
+              </div>
+
+              <div className='app-detail--install'>
+                <Button disabled>Install</Button>
+                <small>Coming soon</small>
+              </div>
             </div>
 
             <div className='app-detail--body'>
-              <p>{this.props.appVersions[0].description}</p>
+              {this.props.appVersions[0].description &&
+              this.props.appVersions[0].description != '' ? (
+                <React.Fragment>
+                  <small>Description</small>
+                  <p>{this.props.appVersions[0].description}</p>
+                </React.Fragment>
+              ) : (
+                ''
+              )}
+
+              {this.props.appVersions[0].home &&
+              this.props.appVersions[0].home != '' ? (
+                <React.Fragment>
+                  <small>Home</small>
+                  <p>
+                    <code>
+                      <a
+                        href={this.props.appVersions[0].home}
+                        rel='noopener noreferrer'
+                      >
+                        {this.props.appVersions[0].home}
+                      </a>
+                    </code>
+                  </p>
+                </React.Fragment>
+              ) : (
+                ''
+              )}
+
+              {this.props.appVersions[0].sources ? (
+                <React.Fragment>
+                  <small>Sources</small>
+                  <ul>
+                    {this.props.appVersions[0].sources.map(source => (
+                      <li key={source} className='source'>
+                        <code>
+                          <a href={source} rel='noopener noreferrer'>
+                            {source}
+                          </a>
+                        </code>
+                      </li>
+                    ))}
+                  </ul>
+                </React.Fragment>
+              ) : (
+                ''
+              )}
+
+              {this.props.appVersions[0].urls ? (
+                <React.Fragment>
+                  <small>URLS</small>
+                  <ul>
+                    {this.props.appVersions[0].urls.map(url => (
+                      <li key={url} className='source'>
+                        <code>
+                          <a href={url} rel='noopener noreferrer'>
+                            {url}
+                          </a>
+                        </code>
+                      </li>
+                    ))}
+                  </ul>
+                </React.Fragment>
+              ) : (
+                ''
+              )}
             </div>
-            <Button>
-              <Link to='/app-katalog'>Back to the App Katalog</Link>
-            </Button>
           </div>
         </Breadcrumb>
       </DocumentTitle>
