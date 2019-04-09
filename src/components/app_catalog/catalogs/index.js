ded8013ebea75dc024f1ec41f9654129da86f2e7
Oliver Ponder <oliver.ponder@gmail.com>
2019-04-09 12:05:58 +0800

    Wire up to new api endpoint. Finishing navigation and appearance.

--- src/components/app_catalog/catalogs/index.js
+++ src/components/app_catalog/catalogs/index.js
@@ -15,32 +15,55 @@ class Catalogs extends React.Component {
             The App Katalog provides you with an easy way to browse and install
             charts from helm repositories.
           </p>
-          <p>
+          <br />
+          {Object.keys(this.props.catalogs.items).length === 0 ? (
+            <p className='well'>
+              <b>Could not find any appcatalogs:</b>
+              <br />
+              It appears we haven&apos;t configured the <code>
+                appcatalog
+              </code>{' '}
+              resources yet for your installation. Please come back later!
+            </p>
+          ) : (
+            <React.Fragment>
+              <small>Pick a repository:</small>
+              <div className='app-catalog--repos'>
+                {Object.keys(this.props.catalogs.items).map(catalogName => {
+                  return (
+                    <Link
+                      key={this.props.catalogs.items[catalogName].metadata.name}
+                      to={'/app-katalog/' + catalogName + '/'}
+                      className='app-catalog--repo'
+                    >
+                      <div className='app-catalog--card'>
+                        <h3>
+                          {this.props.catalogs.items[catalogName].spec.title}
+                        </h3>
+                        <img
+                          src={
+                            this.props.catalogs.items[catalogName].spec.logoURL
+                          }
+                        />
+                      </div>
+                      <p>
+                        {
+                          this.props.catalogs.items[catalogName].spec
+                            .description
+                        }
+                      </p>
+                    </Link>
+                  );
+                })}
+              </div>
+            </React.Fragment>
+          )}
+          <p className='well'>
             <b>Preview Limitations:</b>
             <br />
             During the preview you&apos;ll only be able to browse the App
-            Katalog. Installing charts is not yet available.
+            Katalog. Installing apps is coming soon!
           </p>
-
-          <br />
-          <small>Pick a repository:</small>
-          <div className='app-catalog--repos'>
-            {Object.keys(this.props.catalogs.items).map(catalogName => {
-              return (
-                <Link
-                  key={this.props.catalogs.items[catalogName].name}
-                  to={'/app-katalog/' + catalogName + '/'}
-                  className='app-catalog--repo'
-                >
-                  <div className='app-catalog--card'>
-                    <h3>{this.props.catalogs.items[catalogName].title}</h3>
-                    <img src={this.props.catalogs.items[catalogName].logoUrl} />
-                  </div>
-                  <p>{this.props.catalogs.items[catalogName].description}</p>
-                </Link>
-              );
-            })}
-          </div>
         </React.Fragment>
       </DocumentTitle>
     );
