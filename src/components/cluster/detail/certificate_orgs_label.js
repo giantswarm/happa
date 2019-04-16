471d115cda6ac1f50e53eba2278134638b5a660e
Steffen Windoffer <steffen@wind0r.de>
2019-04-17 00:03:59 +0200

    heavily opinionated CertificateOrgsLabel refactoring

--- src/components/cluster/detail/certificate_orgs_label.js
+++ src/components/cluster/detail/certificate_orgs_label.js
@@ -1,34 +1,26 @@
-'use strict';
-
 import PropTypes from 'prop-types';
 import React from 'react';
 
 class CertificateOrgsLabel extends React.Component {
-  render() {
-    if (this.props.value === '') {
-      return null;
-    }
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+  };
 
-    var orgs = this.props.value.split(',');
-    orgs.sort();
-    var orgLabels = [];
-    orgs.forEach(element => {
-      var classNames = 'orglabel';
-      if (element === 'system:masters') {
-        classNames += ' isadmin';
-      }
-      orgLabels.push(
-        <span className={classNames} key={element}>
+  render() {
+    return this.props.value
+      .split(',')
+      .sort()
+      .map(element => (
+        <span
+          className={
+            'orglabel ' + (element === 'system:masters' ? 'isadmin' : null)
+          }
+          key={element}
+        >
           {element}
         </span>
-      );
-    });
-    return orgLabels;
+      ));
   }
 }
-
-CertificateOrgsLabel.propTypes = {
-  value: PropTypes.string.isRequired,
-};
 
 export default CertificateOrgsLabel;
