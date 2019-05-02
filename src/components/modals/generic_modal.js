6efabb5169d951bc08215592ef80964e592dbddb
Marian Steinbach <marian@giantswarm.io>
2019-05-02 14:43:12 +0200

    Add generic modal component

---
+++ src/components/modals/generic_modal.js
@@ -0,0 +1,34 @@
+import BootstrapModal from 'react-bootstrap/lib/Modal';
+import Button from '../shared/button';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const GenericModal = props => {
+  return (
+    <BootstrapModal
+      className={'modal ' + props.className}
+      show={props.visible}
+      onHide={props.onClose}
+    >
+      <BootstrapModal.Header closeButton>
+        <BootstrapModal.Title>{props.title}</BootstrapModal.Title>
+      </BootstrapModal.Header>
+      <BootstrapModal.Body>{props.children}</BootstrapModal.Body>
+      <BootstrapModal.Footer>
+        <Button bsStyle='link' onClick={props.onClose}>
+          Close
+        </Button>
+      </BootstrapModal.Footer>
+    </BootstrapModal>
+  );
+};
+
+GenericModal.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func,
+  title: PropTypes.node,
+  visible: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default GenericModal;
