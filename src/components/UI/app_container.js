4c0b55259aaae06e49012a8a86794b313df3af8f
Oriol Grau <ograup@gmail.com>
2019-06-26 17:43:44 +0200

    Smalls refactors in app_container

--- src/components/UI/app_container.js
+++ src/components/UI/app_container.js
@@ -2,49 +2,29 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-
 const AppContainer = props => {
   const { appVersions, catalog, searchQuery, iconErrors, imgError } = props;
+  const to = `/apps/${catalog.metadata.name}/${appVersions[0].name}?q=${searchQuery}`;
 
   return (
-    <div
-      className='app-container'
-    >
+    <div className='app-container'>
       <Link
         className='app'
-        to={
-          '/apps/' +
-          catalog.metadata.name +
-          '/' +
-          appVersions[0].name +
-          '?q=' +
-          searchQuery
-        }
+        to={to}
       >
-        {appVersions[0].repoName === 'managed' ? (
+        {appVersions[0].repoName === 'managed' && ( // This is always false. Is that normal?
           <div className='badge'>MANAGED</div>
-        ) : (
-            undefined
-          )}
-
+        )}
         <div className='app-icon'>
-          {appVersions[0].icon &&
-            !iconErrors[
-            appVersions[0].icon
-            ] ? (
-              <img
-                src={appVersions[0].icon}
-                onError={imgError}
-              />
-            ) : (
+          {appVersions[0].icon && !iconErrors[appVersions[0].icon] ? (
+            <img src={appVersions[0].icon} onError={imgError} />
+          ) : (
               <h3>{appVersions[0].name}</h3>
             )}
         </div>
         <div className='app-details'>
           <h3>{appVersions[0].name}</h3>
-          <span className='app-version'>
-            {appVersions[0].version}
-          </span>
+          <span className='app-version'>{appVersions[0].version}</span>
         </div>
       </Link>
     </div>
