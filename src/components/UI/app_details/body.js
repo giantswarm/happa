a496a921c88fb3ee796364bb448490f263a4edd8
ograu <ograup@gmail.com>
2019-06-28 15:33:50 +0200

    App catalog detail (#616)
    * App Catalog Details to UI Library
    
    * Definitely I will add prettier format when save file in VSCode ...
    
    * AppDetailsBody with description list html element
    
    * Minor corrections
    
    * Move styles to AppDetails & subfolder app_details in UI
    
    * Wrapper to more semantic name DescriptionList
    
    * Fixed styles
    
    * Styles from sass to component
    
    * Emotion styled to css syntax

---
+++ src/components/UI/app_details/body.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const DescriptionList = styled.dl`
+  margin: 0;
+
+  dd {
+    margin: 0;
+
+    ul {
+      margin-bottom: 15px;
+      padding: 0;
+      list-style-type: none;
+    }
+
+    li {
+      margin: 0;
+    }
+  }
+`;
+
+const AppDetailsBody = props => {
+  const { description, children } = props;
+  return (
+    <DescriptionList>
+      {description && description !== '' && (
+        <React.Fragment>
+          <dt>
+            <small>Description</small>
+          </dt>
+          <dd>
+            <p>{description}</p>
+          </dd>
+        </React.Fragment>
+      )}
+      {children}
+    </DescriptionList>
+  );
+};
+
+AppDetailsBody.propTypes = {
+  description: PropTypes.string,
+  children: PropTypes.any,
+};
+
+export default AppDetailsBody;
