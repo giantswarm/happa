f291bfa6c47f71f4fd4989261dbbdff5b5c7e1fc
Oriol Grau <ograup@gmail.com>
2019-07-01 16:08:56 +0200

    Use render from RTL

--- src/components/cluster/detail/__tests__/tabs.js
+++ src/components/cluster/detail/__tests__/tabs.js
@@ -1,21 +1,19 @@
-// import { mount } from 'enzyme';
 import '@testing-library/react/cleanup-after-each';
 import 'jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Tab from 'react-bootstrap/lib/Tab';
 
 import Tabs from '../tabs';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Tabs />, div);
+  render(<Tabs />, div);
 });
 
 it('renders only the first tab', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
+  const { container } = render(
     <Tabs>
       <Tab eventKey={1} title='first'>
         <h1>First Tab</h1>
@@ -30,5 +28,5 @@ it('renders only the first tab', () => {
     div
   );
 
-  expect(div.querySelector('#tabs-pane-1')).toBeVisible();
+  expect(container.querySelector('#tabs-pane-1')).toBeVisible();
 });
