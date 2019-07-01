42e37ce6a946c670fadf194021a4c895a49bb6ba
ograu <ograup@gmail.com>
2019-07-01 10:00:30 +0200

    Deleted class .loader-button--status-indicator (#615)

--- src/components/UI/loading_indicator.js
+++ src/components/UI/loading_indicator.js
@@ -1,22 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import styled from '@emotion/styled';
+
+const ProgressButtonStatusIndicator = styled.div({
+  display: 'inline',
+});
 
 const LoadingIndicator = props => (
-  <div className='progress_button--status-indicator'>
+  <ProgressButtonStatusIndicator>
     <ReactCSSTransitionGroup
       transitionEnterTimeout={200}
       transitionLeaveTimeout={200}
       transitionName={`slide-${props.loadingPosition}`}
     >
-      {props.loading ? (
+      {props.loading && (
         <img
           className={'loader ' + props.loadingPosition}
           src='/images/loader_oval_light.svg'
         />
-      ) : null}
+      )}
     </ReactCSSTransitionGroup>
-  </div>
+  </ProgressButtonStatusIndicator>
 );
 
 LoadingIndicator.propTypes = {
