390cd225c710ef7354660a0c08d01af7f974ff6c
Oriol Grau <ograup@gmail.com>
2019-07-03 19:10:57 +0200

    Typo in login_form

--- src/components/UI/login_form_container.js
+++ src/components/UI/login_form_container.js
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
 
-const LoginWrapper = withTheme(styled.div`
-  position: relative;
-  margin: auto;
-  margin-top: -40px;
-  width: 33%;
-  z-index: 1;
-  button {
-    z-index: 10;
-  }
-  form {
-    margin-top: 20px;
-    margin-bottom: 40px;
-  }
-  a {
-    font-size: 14px;
-  }
-`);
+const LoginWrapper = withTheme(
+  styled.div`
+    position: relative;
+    margin: auto;
+    margin-top: -40px;
+    width: 33%;
+    z-index: 1;
+    button {
+      z-index: 10;
+    }
+    form {
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+    a {
+      font-size: 14px;
+    }
+  `
+);
 
 const LoginFormContainer = props => {
   return <LoginWrapper>{props.children}</LoginWrapper>;
