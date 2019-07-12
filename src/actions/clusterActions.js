62f16a642cd7238e673be02180095ab41e739f3f
Oliver Ponder <oliver.ponder@gmail.com>
2019-07-12 12:24:56 +0800

    Also pass in the provider.

--- src/actions/clusterActions.js
+++ src/actions/clusterActions.js
@@ -309,7 +309,7 @@ export function clusterLoadDetails(clusterId) {
         return dispatch(clusterLoadStatus(clusterId));
       })
       .then(() => {
-        cluster.capabilities = computeCapabilities(cluster);
+        cluster.capabilities = computeCapabilities(cluster, getState().app.info.general.provider);
         dispatch(clusterLoadDetailsSuccess(cluster));
         return cluster;
       })
