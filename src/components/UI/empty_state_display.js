816665f66ac2affc5809437d496d8d3d9b1e37e9
Oliver Ponder <oliverponder@giantswarm.io>
2019-07-16 23:37:37 +0800

    Refactor conditional into new EmptyStateDisplay component.

---
+++ src/components/UI/empty_state_display.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+// EmptyStateDisplay conditionally displays its children or a empty state
+// component depending on a boolean prop.
+//
+// displayEmptyState bool
+// emptyState object
+// children object
+const EmptyStateDisplay = props => {
+  if (props.displayEmptyState) {
+    return props.emptyState;
+  } else {
+    return props.children;
+  }
+};
+
+EmptyStateDisplay.propTypes = {
+  children: PropTypes.object,
+  displayEmptyState: PropTypes.bool,
+  emptyState: PropTypes.object,
+};
+
+export default EmptyStateDisplay;
