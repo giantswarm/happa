0e8defa62c166742f8ef371e15fc98468fc67f20
Oliver Ponder <oliver.ponder@gmail.com>
2019-08-07 20:36:04 +0800

    Use newly generated client.

--- src/lib/giantswarm_client_wrapper.js
+++ src/lib/giantswarm_client_wrapper.js
@@ -1,5 +1,6 @@
-// A wrapper for the GiantSwarm V4 JS Client
-// It initializes the client with the right end point.
+// A wrapper for the GiantSwarm JS Client
+// It checks if the JWT token is expired and tries to renew it, before making
+// the api call.
 
 import { auth0Login } from 'actions/userActions';
 import { isJwtExpired } from 'lib/helpers';
@@ -17,17 +18,9 @@ var defaultClient = GiantSwarm.ApiClient.instance;
 var origCallApi = defaultClient.callApi.bind(defaultClient);
 
 defaultClient.callApi = function callApi(
-  path,
-  httpMethod,
-  pathParams,
-  queryParams,
-  headerParams,
-  formParams,
-  bodyParam,
-  authNames,
-  contentTypes,
-  accepts,
-  returnType
+  path, httpMethod, pathParams,
+  queryParams, collectionQueryParams, headerParams, formParams, bodyParam, authNames, contentTypes, accepts,
+  returnType, apiBasePath
 ) {
   var defaultClientAuth = this.authentications['AuthorizationHeaderToken'];
 
@@ -45,17 +38,9 @@ defaultClient.callApi = function callApi(
           headerParams['Authorization'] = 'Bearer ' + result.accessToken;
 
           return origCallApi(
-            path,
-            httpMethod,
-            pathParams,
-            queryParams,
-            headerParams,
-            formParams,
-            bodyParam,
-            authNames,
-            contentTypes,
-            accepts,
-            returnType
+            path, httpMethod, pathParams,
+            queryParams, collectionQueryParams, headerParams, formParams, bodyParam, authNames, contentTypes, accepts,
+            returnType, apiBasePath
           );
         })
         .catch(err => {
@@ -69,17 +54,9 @@ defaultClient.callApi = function callApi(
 
   // JWT token is not expired, or we're not using a JWT token, so just do the call.
   return origCallApi(
-    path,
-    httpMethod,
-    pathParams,
-    queryParams,
-    headerParams,
-    formParams,
-    bodyParam,
-    authNames,
-    contentTypes,
-    accepts,
-    returnType
+    path, httpMethod, pathParams,
+    queryParams, collectionQueryParams, headerParams, formParams, bodyParam, authNames, contentTypes, accepts,
+    returnType, apiBasePath
   );
 };
 
