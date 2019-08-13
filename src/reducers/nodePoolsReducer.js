55147369c010dc7ba5b235015d96075efdef793f
ograu <ograup@gmail.com>
2019-08-13 14:28:28 +0200

    Node pools reducer with immer (#707)
    * Added immer
    
    * Immutable NPs reducer
    
    * Fix NPs load success
    
    * Remove comma
    
    * Ran prettier

--- src/reducers/nodePoolsReducer.js
+++ src/reducers/nodePoolsReducer.js
@@ -1,39 +1,30 @@
+/* eslint-disable no-fallthrough */
 import * as types from 'actions/actionTypes';
+import produce from 'immer';
 
-export const nodePools = (state = {}, action) => {
+// With immer the idea is you put your state inside the draft and immer will take
+// care of everything else. Draft is mutable, so we can use push(), splice(), etc.
+
+// From the docs: "Notice that it is not needed to handle the default case, a producer
+// that doesn't do anything will simply return the original state."
+
+export const nodePools = produce((draft, action) => {
   switch (action.type) {
-    case types.NODEPOOLS_LOAD_SUCCESS: {
-      return {
-        ...action.nodePools,
-      };
-    }
+    case types.NODEPOOLS_LOAD_SUCCESS:
+      return action.nodePools;
 
     case types.NODEPOOLS_LOAD_ERROR:
-      return {
-        ...state,
-        errorLoading: true,
-      };
+      draft.errorLoading = true;
 
     case types.NODEPOOL_PATCH:
-      return {
-        ...state,
-        [action.nodePool.id]: {
-          ...action.nodePool,
-          ...action.payload,
-        },
-      };
+      Object.keys(action.payload).forEach(key => {
+        draft[action.nodePool.id][key] = action.payload[key];
+      });
 
     case types.NODEPOOL_PATCH_ERROR:
-      return {
-        ...state,
-        [action.nodePool.id]: {
-          ...action.nodePool,
-        },
-      };
-
-    default:
-      return state;
+      draft[action.nodePool.id] = action.nodePool;
   }
-};
+  // This empty obeject is the default state.
+}, {});
 
 export default nodePools;
