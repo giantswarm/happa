561768e1e66ce5ec4096ae8f9ce47c4371162424
Oriol Grau <ograup@gmail.com>
2019-08-13 14:36:47 +0200

    Moved logic from reducer to actions file

--- src/reducers/clusterReducer.js
+++ src/reducers/clusterReducer.js
@@ -1,5 +1,7 @@
 import * as types from 'actions/actionTypes';
-import moment from 'moment';
+import produce from 'immer';
+
+// const spread = produce(Object.assign);
 
 /**
  * Since the API omits the 'aws' key from workers on kvm installations, I will
@@ -19,237 +21,123 @@ var ensureWorkersHaveAWSkey = function(clusterDetails) {
   return clusterDetails;
 };
 
-export default function clusterReducer(
-  state = {
-    lastUpdated: null,
-    isFetching: false,
-    items: {},
-    nodePoolsClusters: [],
-  },
-  action = undefined
-) {
-  let items = { ...state.items };
+const initialState = {
+  lastUpdated: null,
+  isFetching: false,
+  items: {},
+  nodePoolsClusters: [],
+};
 
+const clusterReducer = produce((draft, action) => {
   switch (action.type) {
-    case types.CLUSTERS_LOAD_SUCCESS_V4: {
-      const { clusters, lastUpdated } = action;
+    case types.CLUSTERS_LOAD_SUCCESS_V4:
+      Object.keys(action.clusters).forEach(clusterId => {
+        draft.items[clusterId] = action.clusters[clusterId];
+      });
+      draft.lastUpdated = action.lastUpdated;
+      return;
 
-      return {
-        ...state,
-        lastUpdated,
-        items: { ...state.items, ...clusters },
-      };
-    }
-
-    case types.CLUSTERS_LOAD_SUCCESS_V5: {
-      const { clusters, nodePoolsClusters, lastUpdated } = action;
-
-      return {
-        ...state,
-        nodePoolsClusters,
-        lastUpdated,
-        items: { ...state.items, ...clusters },
-      };
-    }
+    case types.CLUSTERS_LOAD_SUCCESS_V5:
+      Object.keys(action.clusters).forEach(clusterId => {
+        draft.items[clusterId] = action.clusters[clusterId];
+      });
+      draft.lastUpdated = action.lastUpdated;
+      draft.nodePoolsClusters = action.nodePoolsClusters;
+      return;
 
     case types.CLUSTERS_LOAD_ERROR_V4:
     case types.CLUSTERS_LOAD_ERROR_V5:
-      return {
-        ...state,
-        errorLoading: true,
-        items,
-      };
+      draft.errorLoading = true;
+      return;
 
     case types.CLUSTER_LOAD_DETAILS_SUCCESS:
-      items[action.cluster.id] = Object.assign(
+      draft.items[action.cluster.id] = Object.assign(
         {},
-        items[action.cluster.id],
+        draft.items[action.cluster.id],
         ensureWorkersHaveAWSkey(action.cluster)
       );
 
       // Fill in scaling values when they aren't supplied.
       if (
-        items[action.cluster.id].scaling.min === undefined &&
-        items[action.cluster.id].scaling.max === undefined
+        draft.items[action.cluster.id].scaling.min === undefined &&
+        draft.items[action.cluster.id].scaling.max === undefined
       ) {
-        items[action.cluster.id].scaling.min =
-          items[action.cluster.id].workers.length;
-        items[action.cluster.id].scaling.max =
-          items[action.cluster.id].workers.length;
+        draft.items[action.cluster.id].scaling.min =
+          draft.items[action.cluster.id].workers.length;
+        draft.items[action.cluster.id].scaling.max =
+          draft.items[action.cluster.id].workers.length;
       }
 
-      return {
-        ...state,
-        items,
-      };
+      return;
 
     case types.CLUSTER_LOAD_DETAILS_ERROR:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        errorLoading: true,
-      });
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.cluster.id].errorLoading = true;
+      return;
 
     case types.CLUSTERS_LOAD_NODEPOOLS_SUCCESS:
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items: {
-          ...state.items,
-          [action.clusterId]: {
-            ...state.items[action.clusterId],
-            nodePools: [...action.nodePools],
-          },
-        },
-      };
+      draft.items[action.clusterId].nodePools = action.nodePools;
+      return;
 
     case types.CLUSTER_LOAD_STATUS_SUCCESS:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        status: action.status,
-      });
-
-      items[action.clusterId].status.lastUpdated = Date.now();
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.clusterId].status = action.status;
+      draft.items[action.clusterId].status.lastUpdated = Date.now();
+      return;
 
     case types.CLUSTER_LOAD_STATUS_NOT_FOUND:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        status: null,
-      });
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.clusterId].status = null;
+      return;
 
     case types.CLUSTER_LOAD_STATUS_ERROR:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        errorLoading: true,
-      });
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.clusterId].errorLoading = true;
+      return;
 
     case types.CLUSTER_LOAD_APPS:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingApps: true,
-      });
-
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items,
-      };
+      draft.items[action.clusterId].isFetchingApps = true;
+      return;
 
     case types.CLUSTER_LOAD_APPS_SUCCESS:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingApps: false,
-
-        // For some reason the array that we get back
-        // from the generated js client does not have
-        // .map on it. So I make a new one here.
-        apps: Array(...action.apps),
-      });
-
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items,
-      };
+      draft.items[action.clusterId].isFetchingApps = false;
+      // For some reason the array that we get back
+      // from the generated js client does not have
+      // .map on it. So I make a new one here.
+      draft.items[action.clusterId].apps = Array(...action.apps);
+      draft.items[action.clusterId].lastUpdated = Date.now();
+      return;
 
     case types.CLUSTER_LOAD_APPS_ERROR:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingApps: false,
-      });
-
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items,
-      };
+      draft.items[action.clusterId].isFetchingApps = false;
+      draft.items[action.clusterId].lastUpdated = Date.now();
+      return;
 
     case types.CLUSTER_LOAD_KEY_PAIRS:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingKeyPairs: true,
-      });
+      draft.items[action.clusterId].isFetchingKeyPairs = true;
 
-      return {
-        ...state,
-        items,
-      };
+      return;
 
     case types.CLUSTER_LOAD_KEY_PAIRS_SUCCESS:
-      // Add expire_date to keyPairs based on ttl_hours
-      var keyPairs = Object.entries(action.keyPairs).map(([, keyPair]) => {
-        keyPair.expire_date = moment(keyPair.create_date)
-          .utc()
-          .add(keyPair.ttl_hours, 'hours');
-        return keyPair;
-      });
-
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingKeyPairs: false,
-        keyPairs: keyPairs,
-      });
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.clusterId].isFetchingKeyPairs = false;
+      draft.items[action.clusterId].keyPairs = action.keyPairs;
+      return;
 
     case types.CLUSTER_LOAD_KEY_PAIRS_ERROR:
-      items[action.clusterId] = Object.assign({}, items[action.clusterId], {
-        isFetchingKeyPairs: false,
-      });
-
-      return {
-        ...state,
-        items,
-      };
+      draft.items[action.clusterId].isFetchingKeyPairs = false;
+      return;
 
     case types.CLUSTER_DELETE_SUCCESS:
-      delete items[action.clusterId];
-
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items,
-      };
+      delete draft.items[action.clusterId];
+      draft.items[action.clusterId].lastUpdated = Date.now();
+      return;
 
     case types.CLUSTER_PATCH:
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items: {
-          ...state.items,
-          [action.cluster.id]: {
-            ...state.items[action.cluster.id],
-            ...action.payload,
-          },
-        },
-      };
+      Object.keys(action.payload).forEach(key => {
+        draft.items[action.cluster.id][key] = action.payload[key];
+      });
+      return;
 
     case types.CLUSTER_PATCH_ERROR:
-      return {
-        ...state,
-        lastUpdated: Date.now(),
-        items: {
-          ...state.items,
-          [action.cluster.id]: {
-            ...action.cluster,
-          },
-        },
-      };
-
-    default:
-      return state;
+      draft.items[action.cluster.id] = action.cluster;
+      return;
   }
-}
+}, initialState);
+
+export default clusterReducer;
