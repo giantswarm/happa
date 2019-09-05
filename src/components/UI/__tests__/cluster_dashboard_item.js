b17fdf83ce55ed7aff059741ac2b4cdd91926ced
Oliver Ponder <oliver.ponder@gmail.com>
2019-09-05 10:21:55 +0200

    Add test placeholders for pair programing / tips.

--- src/components/UI/__tests__/cluster_dashboard_item.js
+++ src/components/UI/__tests__/cluster_dashboard_item.js
@@ -7,3 +7,39 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   render(<ClusterDashboardItem />, div);
 });
+
+it('has links to the cluster detail page', () => {
+  //
+});
+
+it('shows the clusters name', () => {
+  //
+});
+
+it('has links to the clusters release version', () => {
+  //
+});
+
+it('shows a relative creation date', () => {
+  //
+});
+
+it('shows a nodepool count if the cluster has nodepools', () => {
+  //
+});
+
+it('shows storage count if we are on kvm', () => {
+  //
+});
+
+it('shows CPU cores count', () => {
+  //
+});
+
+it('shows Memory GB count', () => {
+  //
+});
+
+it('shows a Get Started button if the cluster is less than 30 days old', () => {
+  //
+});
