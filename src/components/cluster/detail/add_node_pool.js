f43333a273f40e8c41f204d8ae6b1bc946db4fc2
Oriol Grau <ograup@gmail.com>
2019-09-17 09:30:03 +0200

    Added styles

--- src/components/cluster/detail/add_node_pool.js
+++ src/components/cluster/detail/add_node_pool.js
@@ -1,43 +1,79 @@
 import { connect } from 'react-redux';
 import AWSInstanceTypeSelector from '../new/aws_instance_type_selector';
 import Button from 'UI/button';
+import NodeCountSelector from 'shared/node_count_selector';
 import NumberPicker from 'UI/number_picker';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import styled from '@emotion/styled';
 import theme from 'styles/theme';
 
-const Wrapper = styled.div`
+const WrapperDiv = styled.div`
   background-color: ${props => props.theme.colors.shade10};
   padding: 20px;
   border-radius: 5px;
+  h3 {
+    margin-bottom: 20px;
+  }
 `;
 
-const FlexColumn = styled.form`
+const FlexWrapperDiv = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 13px;
+`;
+
+const FlexColumnForm = styled.form`
   display: flex;
   justify-content: space-between;
   /* align-items: flex-start; */
   flex-direction: column;
   margin: 0 auto;
-  max-width: 600px;
+  max-width: 650px;
   label {
     display: flex;
     justify-content: space-between;
     flex-direction: column;
+    margin: 0 0 31px;
+    p {
+      line-height: 1.4;
+    }
   }
   .label-span {
-    font-size: 16px;
     color: ${props => props.theme.colors.white1};
-    margin-top: 31px;
   }
   .label-span,
   input,
   select {
+    font-size: 16px;
     margin-bottom: 13px;
     font-weight: 400;
   }
+  & > input {
+    margin-bottom: 13px !important;
+  }
   p {
-    line-height: 1.3;
+    margin: 0;
+    font-size: 14px;
+    color: ${props => props.theme.colors.white1};
+  }
+  /* Overrides for AWSInstanceTypeSelector */
+  .textfield label,
+  .message {
+    display: none;
+    margin: 0;
+  }
+  .textfield,
+  .textfield input {
+    margin: 0;
+  }
+  /* Overrides for NumberPicker */
+  .availability-zones {
+    & > div > div,
+    & > div > div > div {
+      margin: 0;
+    }
   }
 `;
 
@@ -48,64 +84,71 @@ class AddNodePool extends Component {
 
   render() {
     return (
-      <Wrapper>
+      <WrapperDiv>
         <h3 className='table-label'>Add Node Pool</h3>
-        <FlexColumn>
+        <FlexColumnForm>
           <label htmlFor='name'>
             <span className='label-span'>Name</span>
             <input id='name' type='text'></input>
+            <p>
+              Pick a name that helps team mates to understand what these nodes
+              are here for. You can change this later. Each pool also gets a
+              unique identifier.
+            </p>
           </label>
-          <p>
-            Pick a name that helps team mates to understand what these nodes are
-            here for. You can change this later. Each pool also gets a unique
-            identifier.
-          </p>
           <label htmlFor='instance-type'>
             <span className='label-span'>Instance type</span>
-            <AWSInstanceTypeSelector
-              // allowedInstanceTypes={this.props.allowedInstanceTypes}
-              allowedInstanceTypes={['m3.large', 'm5.large']}
-              // onChange={this.updateAWSInstanceType}
-              readOnly={false}
-              // value={this.state.aws.instanceType.value}
-              value='m3.large'
-            />
-            <p>2 CPU cores, 8GB RAM each</p>
+            <FlexWrapperDiv>
+              <AWSInstanceTypeSelector
+                // allowedInstanceTypes={this.props.allowedInstanceTypes}
+                allowedInstanceTypes={['m3.large', 'm5.large']}
+                // onChange={this.updateAWSInstanceType}
+                readOnly={false}
+                // value={this.state.aws.instanceType.value}
+                value='m3.large'
+              />
+              <p>2 CPU cores, 8GB RAM each</p>
+            </FlexWrapperDiv>
           </label>
-          <label htmlFor='availability-zones'>
+          <label className='availability-zones' htmlFor='availability-zones'>
             <span className='label-span'>Availability Zones</span>
-            <input
-              id='availability-zones'
-              name='availability-zones'
-              type='number'
-            />
-            <p>or Select distinct availability zones</p>
+            <FlexWrapperDiv>
+              <NumberPicker
+                // label=''
+                max={this.props.maxAvailabilityZones}
+                min={this.props.minAvailabilityZones}
+                // onChange={this.updateAvailabilityZonesPicker}
+                readOnly={false}
+                stepSize={1}
+                // value={this.state.availabilityZonesPicker.value}
+                value={3}
+              />
+              <p>or Select distinct availability zones</p>
+            </FlexWrapperDiv>
+            <p>
+              Covering one availability zone, the worker nodes of this node pool
+              will be placed in the same availability zones as the
+              cluster&apos;s master node.
+            </p>
           </label>
-          <p>
-            Covering one availability zone, the worker nodes of this node pool
-            will be placed in the same availability zones as the cluster&apos;s
-            master node.
-          </p>
-          <div>Scaling range</div>
           <label htmlFor='scaling-range-min'>
-            <span className='label-span'>Availability Zones</span>
-            <input
-              id='scaling-range-min'
-              name='scaling-range-min'
-              type='number'
-            />
-          </label>
-          <label htmlFor='scaling-range-max'>
-            Availability Zones
-            <NumberPicker
-              label=''
-              max={this.props.maxAvailabilityZones}
-              min={this.props.minAvailabilityZones}
-              // onChange={this.updateAvailabilityZonesPicker}
+            <span className='label-span'>Scaling range</span>
+            <NodeCountSelector
+              // autoscalingEnabled={this.isScalingAutomatic(
+              //   this.props.provider,
+              //   this.state.releaseVersion
+              // )}
+              autoscalingEnabled={true}
+              // onChange={this.updateScaling}
               readOnly={false}
-              stepSize={1}
-              // value={this.state.availabilityZonesPicker.value}
-              value={3}
+              // scaling={this.state.scaling}
+              scaling={{
+                automatic: false,
+                min: 3,
+                minValid: true,
+                max: 3,
+                maxValid: true,
+              }}
             />
           </label>
           <Button
@@ -128,8 +171,8 @@ class AddNodePool extends Component {
           >
             Cancel
           </Button>
-        </FlexColumn>
-      </Wrapper>
+        </FlexColumnForm>
+      </WrapperDiv>
     );
   }
 }
