20540edb536647c5455021a94092d27172ae51bd
Oliver Ponder <oliverponder@giantswarm.io>
2019-09-19 17:01:58 +0200

    Fix history and initialState sharing. (#762)
    * Fix history and initialState sharing.
    
    * Don't expose the store to the tests. Take initial route as before.
    
    * prettier.

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
-import history from './history';
 import rootReducer from 'reducers';
 import thunk from 'redux-thunk';
 
@@ -8,16 +7,12 @@ var store;
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore(initialState) {
-  if (store) {
-    return store;
-  } else {
-    store = createStore(
-      rootReducer(history),
-      initialState,
-      composeEnhancers(applyMiddleware(routerMiddleware(history), thunk))
-    );
+export default function configureStore(initialState, history) {
+  store = createStore(
+    rootReducer(history),
+    initialState,
+    composeEnhancers(applyMiddleware(routerMiddleware(history), thunk))
+  );
 
-    return store;
-  }
+  return store;
 }
--- test_utils/renderRouteWithStore.js
+++ test_utils/renderRouteWithStore.js
@@ -1,5 +1,7 @@
-import { MemoryRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
+import { push } from 'connected-react-router';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'emotion-theming';
 import Routes from 'routes';
@@ -8,6 +10,7 @@ import initialState from 'test_utils/initialState';
 import React from 'react';
 import theme from 'styles/theme';
 
+
 /**
  * This function will render the whole app with a mocked store in the route
  * provided.
@@ -15,20 +18,26 @@ import theme from 'styles/theme';
  * @param {Object} container An HTMLElement.
  */
 export function renderRouteWithStore(
-  route = '/',
+  initialRoute = '/',
   container,
-  state = initialState
+  state = initialState(),
+  history = createMemoryHistory(),
 ) {
-  const store = configureStore(state);
 
-  return render(
+  const store = configureStore(state, history);
+
+  const app = render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <MemoryRouter initialEntries={[route]}>
+        <ConnectedRouter history={history}>
           <Routes />
-        </MemoryRouter>
+        </ConnectedRouter>
       </ThemeProvider>
     </Provider>,
     container
   );
+
+  store.dispatch(push(initialRoute));
+
+  return app;
 }
