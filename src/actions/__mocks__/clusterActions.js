77d8868f15d0a5607382e6e0b8399ec7b5be9bcd
Oriol Grau <ograup@gmail.com>
2019-09-19 15:16:50 +0200

    More cleanup

--- src/actions/__mocks__/clusterActions.js
+++ src/actions/__mocks__/clusterActions.js
@@ -1,5 +1,3 @@
 export const clustersLoad = () => () => Promise.resolve();
 export const clusterLoadDetails = () => () => Promise.resolve();
 export const clusterLoadKeyPairs = () => () => Promise.resolve();
-
-// export const clusterCreate = () => () => Promise.resolve();
