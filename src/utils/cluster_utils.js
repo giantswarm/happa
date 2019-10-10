d7d6ad231e39b34304185477d8a01a180f0fe628
Oriol Grau <ograup@gmail.com>
2019-10-10 11:48:10 +0200

    Changes in cluster_utils deleted

--- src/utils/cluster_utils.js
+++ src/utils/cluster_utils.js
@@ -114,11 +114,7 @@ export function getCpusTotalNodePools(nodePools = []) {
 // Finds node pools for a cluster and returns an array of node pools objects
 export const clusterNodePools = (nodePools, cluster) => {
   // This is to avoid a TypeError when trying to map an undefined variable
-  if (
-    nodePools &&
-    nodePools.length !== 0 &&
-    Object.entries(nodePools).length !== 0
-  ) {
+  if (nodePools && Object.entries(nodePools).length !== 0) {
     return cluster.nodePools.map(np => nodePools[np]);
   }
 };
