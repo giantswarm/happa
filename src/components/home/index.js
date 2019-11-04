a4091fd30eb4399ab4583bffd27e126e6e2d7030
Oriol Grau <ograup@gmail.com>
2019-11-04 13:56:33 +0100

    Fix interval

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -33,7 +33,7 @@ class Home extends React.Component {
    * Load clusters list periodically
    */
   registerRefreshInterval = () => {
-    var refreshIntervalDuration = 3 * 1000; // 30 seconds
+    var refreshIntervalDuration = 30 * 1000; // 30 seconds
     this.refreshInterval = window.setInterval(
       this.refreshClustersList,
       refreshIntervalDuration
