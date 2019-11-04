c4594871746be84f2ac566a49f456c535c78eb99
Adrian Barsan <mnlk123@yahoo.com>
2019-11-04 10:28:58 +0100

    Refactor App Catalog list components (#805)
    * Refactor App Catalog List inner component for better readilibity and to follow React best practices
    
    * Lazy load app container icon
    
    * Run prettier and reformat code
    
    * Extract lazy loaded image logic to a generic component
    
    * Run prettier
    
    * Refactor dumb components as functional components
    
    * Run prettier
    
    * Simplify code and improve readibility
    
    * Fix renamed filename in import
    
    * Make AppContainer forward its ref, since it can't accept an own one
    
    * Remove the lunr dependency and implement a basic native sorting algorithm
    
    * Remove redundant storage of searchQuery inside state, and rely to just storing it in the URL parameters

---
+++ src/components/app_catalog/app_list/AppListSearch.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const AppListSearch = ({ value, ...props }) => (
+  <form>
+    <div className='input-with-icon'>
+      <i className='fa fa-search' />
+      <input onChange={props.onChange} type='text' value={value} />
+      {value && (
+        <a className='clearQuery' onClick={props.onReset}>
+          <i className='fa fa-close' />
+        </a>
+      )}
+    </div>
+  </form>
+);
+
+AppListSearch.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onReset: PropTypes.func,
+};
+
+export default AppListSearch;
