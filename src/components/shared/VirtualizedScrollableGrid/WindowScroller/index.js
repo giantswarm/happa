92a7c1491f47fff3b5552714bf60df4e6dbdca29
axbarsan <mnlk123@yahoo.com>
2019-11-06 10:00:10 +0100

    Include 'WindowScroller' from 'react-virtualized' instead of having it bundled in the app

--- src/components/shared/VirtualizedScrollableGrid/WindowScroller/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Module taken from [react-virtualized] (https://github.com/bvaughn/react-virtualized)
- */
-import WindowScroller, { IS_SCROLLING_TIMEOUT } from './WindowScroller';
-
-export default WindowScroller;
-export { WindowScroller, IS_SCROLLING_TIMEOUT };
