0563e287a32fdd26d1724201ed452d98add0db86
axbarsan <mnlk123@yahoo.com>
2019-11-06 17:45:41 +0100

    Make version links have an underline to make them actually look like links

--- src/components/cluster/detail/Versions.js
+++ src/components/cluster/detail/Versions.js
@@ -5,6 +5,15 @@ import PropTypes from 'prop-types';
 import React, { useRef } from 'react';
 import RefreshableLabel from 'UI/refreshable_label';
 import ReleaseDetailsModal from 'modals/release_details_modal';
+import styled from '@emotion/styled';
+
+const ReleaseDetail = styled.span`
+  text-decoration: underline;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
 // Versions data and icons at the top of cluster details view.
 function Versions({
@@ -25,17 +34,17 @@ function Versions({
           <RefreshableLabel dataItems={[releaseVersion]}>
             <>
               <Dot style={{ paddingRight: 0 }} />
-              <i className='fa fa-version-tag' />
-              <span
+              <ReleaseDetail
                 className='pointer'
                 onClick={() => releaseDetailsModal.current.show()}
               >
+                <i className='fa fa-version-tag' />
                 {releaseVersion ? releaseVersion : 'n/a'}
-              </span>
+              </ReleaseDetail>
             </>
           </RefreshableLabel>
         </span>
-        <span
+        <ReleaseDetail
           className='pointer'
           onClick={() => releaseDetailsModal.current.show()}
         >
@@ -55,7 +64,7 @@ function Versions({
             k8sVersion !== '' &&
             k8sVersion !== undefined &&
             <i className='fa fa-kubernetes' /> + k8sVersion}
-        </span>
+        </ReleaseDetail>
       </div>
       {canUpgrade && (
         <a className='upgrade-available' onClick={showUpgradeModal}>
