9443b68fc1b9f2ada59e0c46fc7b4f100ac215c3
Oliver Ponder <oliver.ponder@gmail.com>
2019-11-07 16:20:41 +0100

    Add an airbrake notifier to the project that can report errors to our exception notification endpoint.

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
 import { ConnectedRouter } from 'connected-react-router';
 import { hot } from 'react-hot-loader';
+import { Notifier } from '@airbrake/browser';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import { Requester } from 'lib/patched_airbrake_requester';
 import { ThemeProvider } from 'emotion-theming';
 import configureStore from 'stores/configureStore';
 import history from 'stores/history';
@@ -35,6 +37,26 @@ const store = configureStore({}, history);
 // update the store with the new token.
 monkeyPatchGiantSwarmClient(store);
 
+// Configure an airbrake notifier for exception notification.
+// We use the airbrake notifier here instead of rolling our own notification
+// client, it is a stable project used by many. Though instead of sending our
+// exception reports to airbrake we send it to an endpoint of our own API, to
+// ensure no customer data is being shared with a third party.
+const airbrake = new Notifier({
+  // projectId and projectKey are not relevant to our exception notification endpoint, but are required to create a valid notifier.
+  projectId: 1,
+  projectKey: 'happa',
+  environment: window.config.environment,
+});
+
+// Reach into the airbrake notifier instance and modify the private _url and
+// _requester attributes since the constructor does not allow us to edit the
+// url or the headers used during the request easily. We need to set headers so
+// that we can authenticate against our API endpoint.
+airbrake._url = window.config.apiEndpoint + '/v5/exception-notifications/';
+airbrake._requester = new Requester(store).request;
+
+// Scroll to the top when we change the URL.
 history.listen(() => {
   window.scrollTo(0, 0);
 });
---
+++ src/lib/patched_airbrake_requester.js
@@ -0,0 +1,38 @@
+// This file is a edited version of
+// https://github.com/airbrake/airbrake-js/blob/master/packages/browser/src/http_req/fetch.ts
+
+// Airbrake doesn't expose a easy interface for setting custom headers for each
+// request. We need to set headers for authentication against our API.
+import fetch from 'cross-fetch';
+
+export class Requester {
+  constructor(store) {
+    this.store = store;
+  }
+
+  request = (req) => {
+    let scheme = this.store.getState().app.loggedInUser.auth.scheme;
+    let token = this.store.getState().app.loggedInUser.auth.token;
+    let authHeader =  scheme + ' ' + token;
+
+    let opt = {
+      method: req.method,
+      body: req.body,
+      headers: {
+        'Authorization': authHeader
+      },
+    };
+    return fetch(req.url, opt).then((resp) => {
+      if (resp.status === 201) {
+        return { json: null };
+      }
+
+      return resp.text().then((body) => {
+        let err = new Error(
+          `airbrake: fetch: unexpected response: code=${resp.status} body='${body}'`,
+        );
+        throw err;
+      });
+    });
+  }
+}
