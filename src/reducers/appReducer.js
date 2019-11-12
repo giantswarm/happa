509aa7554f3f1742a4857243c788af89fac774a3
Oliver Ponder <oliver.ponder@gmail.com>
2019-11-12 16:37:37 +0100

    Fix early local storage initialisation

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -23,50 +23,52 @@ const initialState = () => ({
   },
 });
 
-const appReducer = produce((draft, action) => {
-  switch (action.type) {
-    case types.REFRESH_USER_INFO_SUCCESS:
-      draft.loggedInUser = action.userData;
-      return;
+const makeAppReducer = () => {
+  return produce((draft, action) => {
+    switch (action.type) {
+      case types.REFRESH_USER_INFO_SUCCESS:
+        draft.loggedInUser = action.userData;
+        return;
 
-    case types.INFO_LOAD_SUCCESS:
-      draft.info = action.info;
-      return;
+      case types.INFO_LOAD_SUCCESS:
+        draft.info = action.info;
+        return;
 
-    case types.LOGIN_SUCCESS:
-      // TODO This is a Side effect.
-      // Is there a better place for setUserToStorage()?
-      setUserToStorage(action.userData);
-      draft.loggedInUser = action.userData;
-      return;
+      case types.LOGIN_SUCCESS:
+        // TODO This is a Side effect.
+        // Is there a better place for setUserToStorage()?
+        setUserToStorage(action.userData);
+        draft.loggedInUser = action.userData;
+        return;
 
-    case types.LOGIN_ERROR:
-    case types.LOGOUT_SUCCESS:
-    case types.LOGOUT_ERROR:
-    case types.UNAUTHORIZED:
-      // TODO This is a Side effect.
-      // Is there a better place for removeUserFromStorage()?
-      removeUserFromStorage();
-      draft.loggedInUser = {};
-      draft.firstLoadComplete = false;
-      return;
+      case types.LOGIN_ERROR:
+      case types.LOGOUT_SUCCESS:
+      case types.LOGOUT_ERROR:
+      case types.UNAUTHORIZED:
+        // TODO This is a Side effect.
+        // Is there a better place for removeUserFromStorage()?
+        removeUserFromStorage();
+        draft.loggedInUser = {};
+        draft.firstLoadComplete = false;
+        return;
 
-    case types.ORGANIZATION_SELECT:
-      draft.selectedOrganization = action.orgId;
-      return;
+      case types.ORGANIZATION_SELECT:
+        draft.selectedOrganization = action.orgId;
+        return;
 
-    case types.ORGANIZATIONS_LOAD_SUCCESS:
-      draft.selectedOrganization = action.selectedOrganization;
-      return;
+      case types.ORGANIZATIONS_LOAD_SUCCESS:
+        draft.selectedOrganization = action.selectedOrganization;
+        return;
 
-    case types.CLUSTERS_LOAD_SUCCESS:
-      draft.firstLoadComplete = true;
-      return;
+      case types.CLUSTERS_LOAD_SUCCESS:
+        draft.firstLoadComplete = true;
+        return;
 
-    case types.CLUSTER_SELECT:
-      draft.selectedClusterID = action.clusterID;
-      return;
-  }
-}, initialState());
+      case types.CLUSTER_SELECT:
+        draft.selectedClusterID = action.clusterID;
+        return;
+    }
+  }, initialState())
+};
 
-export default appReducer;
+export default makeAppReducer;
