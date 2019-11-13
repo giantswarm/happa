f042037b6ae95fee22ef13ec3256ee6ca7eff297
Oriol Grau <ograup@gmail.com>
2019-11-13 12:13:07 +0100

    Fixes v5 cluster tests

--- __tests__/V5ClusterDetailsTable.js
+++ __tests__/V5ClusterDetailsTable.js
@@ -1,44 +1,90 @@
 import '@testing-library/jest-dom/extend-expect';
+import {
+  API_ENDPOINT,
+  appCatalogsResponse,
+  getPersistedMockCall,
+  infoResponse,
+  nodePoolsResponse,
+  ORGANIZATION,
+  orgResponse,
+  orgsResponse,
+  releasesResponse,
+  userResponse,
+  V5_CLUSTER,
+  v5ClusterResponse,
+  v5ClustersResponse,
+} from 'test_utils/mockHttpCalls';
 import { fireEvent, render, wait } from '@testing-library/react';
-import { nodePoolPatch as mockNodePoolPatch } from 'actions/nodePoolActions';
 import { renderRouteWithStore } from 'test_utils/renderRouteWithStore';
 import { ThemeProvider } from 'emotion-theming';
 import { truncate } from 'lib/helpers';
-import initialState from 'test_utils/initialState';
+import nock from 'nock';
 import React from 'react';
-import statusState from 'test_utils/statusState';
 import theme from 'styles/theme';
 
 // Components
 import NodePoolDropdownMenu from 'cluster/detail/NodePoolDropdownMenu';
 
-// Mock actions to return nothing, we don't want them perform API calls and we don't
-// want them to return any values either cause we are using a mocked store.
-jest.mock('actions/userActions');
-jest.mock('actions/organizationActions');
-jest.mock('actions/clusterActions');
-jest.mock('actions/releaseActions');
-jest.mock('actions/catalogActions');
-jest.mock('actions/nodePoolActions', () => {
-  return {
-    nodePoolPatch: jest.fn(() => () => Promise.resolve()),
-  };
+// Cluster and route we are testing with.
+const ROUTE = `/organizations/${ORGANIZATION}/clusters/${V5_CLUSTER.id}`;
+
+// Tests setup
+const requests = {};
+
+// Responses to requests
+beforeAll(() => {
+  requests.userInfo = getPersistedMockCall('/v4/user/', userResponse);
+  requests.info = getPersistedMockCall('/v4/info/', infoResponse);
+  requests.organizations = getPersistedMockCall(
+    '/v4/organizations/',
+    orgsResponse
+  );
+  requests.organization = getPersistedMockCall(
+    `/v4/organizations/${ORGANIZATION}/`,
+    orgResponse
+  );
+  requests.clusters = getPersistedMockCall('/v4/clusters/', v5ClustersResponse);
+  requests.cluster = getPersistedMockCall(
+    `/v5/clusters/${V5_CLUSTER.id}/`,
+    v5ClusterResponse
+  );
+  requests.credentials = getPersistedMockCall(
+    `/v4/organizations/${ORGANIZATION}/credentials/`
+  );
+  requests.releases = getPersistedMockCall('/v4/releases/', releasesResponse);
+  requests.nodePools = getPersistedMockCall(
+    `/v5/clusters/${V5_CLUSTER.id}/nodepools/`,
+    nodePoolsResponse
+  );
+  requests.appcatalogs = getPersistedMockCall(
+    '/v4/appcatalogs/',
+    appCatalogsResponse
+  );
 });
 
-// Cluster and route we are testing with.
-const clusterId = 'm0ckd';
-const route = `/organizations/acme/clusters/${clusterId}`;
+// Stop persisting responses
+afterAll(() => {
+  Object.keys(requests).forEach(req => {
+    requests[req].persist(false);
+  });
+});
 
-it.skip('renders all node pools in store', async () => {
+/************ TESTS ************/
+
+it('renders all node pools in store', async () => {
   const div = document.createElement('div');
-  const { getByText } = renderRouteWithStore(route, div);
 
-  const nodePools = Object.keys(initialState().entities.nodePools.items);
+  const { getByText, findAllByTestId, debug } = renderRouteWithStore(
+    ROUTE,
+    div,
+    {}
+  );
 
-  await wait(() => {
-    nodePools.forEach(nodePool => {
-      expect(getByText(nodePool)).toBeInTheDocument();
-    });
+  debug();
+  await wait(() => findAllByTestId('node-pool-id'));
+
+  nodePoolsResponse.forEach(nodePool => {
+    expect(getByText(nodePool.id)).toBeInTheDocument();
   });
 });
 
@@ -55,70 +101,109 @@ it('shows the dropdown when the three dots button is clicked', () => {
   expect(menu).toBeInTheDocument();
 });
 
-// Here we are testing everything UI related.
-// We are NOT testing the http call and redux internals, just checking that we are
-// calling the right method with the right arguments when clicking the submit button
-it.skip('patches node pool name correctly', async () => {
+it('patches node pool name correctly and re-sort node pools accordingly', async () => {
+  const newNodePoolName = 'New NP name';
+  const nodePoolName = nodePoolsResponse[0].name;
+  const nodePoolId = nodePoolsResponse[0].id;
+
+  // Response to request should be the exact same NP with the new name
+  const nodePoolPatchResponse = {
+    ...nodePoolsResponse[0],
+    name: newNodePoolName,
+  };
+
+  // Request
+  const nodePoolPatchRequest = nock(API_ENDPOINT)
+    .intercept(
+      `/v5/clusters/${V5_CLUSTER.id}/nodepools/${nodePoolsResponse[0].id}/`,
+      'PATCH'
+    )
+    .reply(200, nodePoolPatchResponse);
+
+  // Mounting
   const div = document.createElement('div');
-  const { getAllByText, getByText, container } = renderRouteWithStore(
-    route,
-    div
+  const { getAllByTestId, getByText, container } = renderRouteWithStore(
+    ROUTE,
+    div,
+    {}
   );
 
-  const nodePools = Object.keys(initialState().entities.nodePools.items);
-  const nodePool = initialState().entities.nodePools.items[nodePools[0]];
-  const nodePoolName = initialState().entities.nodePools.items[nodePools[0]]
-    .name;
-  const newNodePoolName = 'New NP name';
+  await wait(() => getByText(truncate(nodePoolName, 14)));
 
-  await wait(() => {
-    // All mock node pools have the same first 14 characters.
-    const nodePoolNameEls = getAllByText(truncate(nodePoolName, 14));
-    fireEvent.click(nodePoolNameEls[0]);
-  });
+  // All mock node pools have the same first 14 characters.
+  const nodePoolNameEl = getByText(truncate(nodePoolName, 14));
+  const nodePools = getAllByTestId('node-pool-id');
 
+  // Is this NP the first in the list?
+  expect(nodePools[0]).toContainHTML(nodePoolNameEl);
+  fireEvent.click(nodePoolNameEl);
+
+  // Write the new name and submit it
   container.querySelector(
     `input[value="${nodePoolName}"]`
   ).value = newNodePoolName;
 
-  // TODO change it and look for rendered changes once we have API calls mocked
-  // instead of mocked action creators
   const submitButton = getByText(/ok/i);
   fireEvent.click(submitButton);
-  expect(mockNodePoolPatch).toHaveBeenCalledTimes(1);
-  expect(mockNodePoolPatch).toHaveBeenCalledWith(clusterId, nodePool, {
-    name: newNodePoolName,
+
+  //Wait for the Flash message to appear
+  await wait(() => {
+    getByText(/succesfully edited node pool name/i);
   });
+
+  // Is the new NP name in the document?
+  expect(getByText(newNodePoolName)).toBeInTheDocument();
+
+  // Is it now the 2nd node pool in list?
+  const reSortedNodePools = getAllByTestId('node-pool-id');
+  expect(reSortedNodePools[1]).toHaveTextContent(nodePoolId);
+
+  // Assert that the mocked responses got called, tell them to stop waiting for
+  // a request.
+  nodePoolPatchRequest.done();
 });
 
-// The modal is opened calling a function that lives in the parent component of
-// <NodePoolDropdownMenu>, so we can't test it in isolation, we need to render
-// the full tree.
-it.skip(`shows the modal when the button is clicked with default values and calls
-  the action creator with the correct arguments`, async () => {
+// TO BE FINISHED YET
+it(`shows the scaling modal when the button is clicked with default values and scales 
+node pools correctly`, async () => {
+  const nodePool = nodePoolsResponse[0];
+  const newScaling = { min: 4, max: 11 };
+  const nodePoolPatchResponse = {
+    ...nodePoolsResponse[0],
+    scaling: newScaling,
+  };
+
+  // Request
+  const nodePoolPatchRequest = nock(API_ENDPOINT)
+    .intercept(
+      `/v5/clusters/${V5_CLUSTER.id}/nodepools/${nodePoolsResponse[0].id}/`,
+      'PATCH'
+    )
+    .reply(200, nodePoolPatchResponse);
+
   const div = document.createElement('div');
-  const state = initialState();
+  const {
+    getByText,
+    getAllByText,
+    getAllByTestId,
+    getByLabelText,
+  } = renderRouteWithStore(ROUTE, div, {});
 
-  const { getByText, getAllByText, getAllByTestId } = renderRouteWithStore(
-    route,
-    div,
-    state
-  );
+  await wait(() => getAllByTestId('node-pool-id'));
 
-  await wait(() => {
-    getAllByTestId('node-pool-id')[0];
-  });
-
+  // Expect first nodePool is
   const nodePoolId = getAllByTestId('node-pool-id')[0].textContent;
-  const nodePool = state.entities.nodePools.items[nodePoolId];
-  fireEvent.click(getAllByText('•••')[0]);
+  expect(nodePoolId).toBe(nodePool.id);
 
+  fireEvent.click(getAllByText('•••')[0]);
   fireEvent.click(getByText(/edit scaling limits/i));
   const modalTitle = getByText(/edit scaling settings for/i);
+  expect(modalTitle).toHaveTextContent('3jx');
 
   // Is the modal in the document?
   expect(modalTitle).toBeInTheDocument();
 
-  // TODO expect correct values and call to action creator.
-  // Continue when scaling modal development for NPs is finished.
+  // Can't edit inputs because of bootstrap implementation of inputs, or at least
+  // that' what I guess, so mocking the patch response and expecting to see the values
+  // in the view:
 });
