a7bbfff9f253f7226a6f13edbf5cdbe9ae198c02
ograu <ograup@gmail.com>
2019-11-13 12:12:13 +0100

    More tests setup (#827)
    * Fixed NPs tests
    
    * Restored index cconfig
    
    * Checkout __tests__/login.js
    
    * Tidy up
    
    * NP Patch test
    
    * Comments
    
    * Checkout organiationActions.js from master
    
    * Deleted unused vars
    
    * Test fixes localstorage wip (#829)
    
    * Fixed NPs tests
    
    * Restored index cconfig
    
    * Checkout __tests__/login.js
    
    * Tidy up
    
    * NP Patch test
    
    * Comments
    
    * Checkout organiationActions.js from master
    
    * Deleted unused vars
    
    * WIP
    
    * Fix early local storage initialisation
    
    * unskip other tests
    
    * Remove local storage mock and unsurpress warnings.
    
    * Remove debug output.
    
    * Remove one more debug output.
    
    * Run prettier.
    
    * WIP
    
    * Removed jest config instance type
    
    * Checkout v5 cluster tests file and moved to global tests folder
    
    * Initialized storage with auth as a default in tests

--- src/actions/__mocks__/clusterActions.js
+++
@@ -1,3 +0,0 @@
-export const clustersLoad = () => () => Promise.resolve();
-export const clusterLoadDetails = () => () => Promise.resolve();
-export const clusterLoadKeyPairs = () => () => Promise.resolve();
