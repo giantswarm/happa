cdbd8e5e2b4c76889924a870623e6fb0e89728ee
axbarsan <mnlk123@yahoo.com>
2019-11-14 19:54:37 +0100

    Use utility render function instead of repeating myself 1231 times

--- __tests__/NodeCountSelector.js
+++ __tests__/NodeCountSelector.js
@@ -6,33 +6,29 @@ import theme from 'styles/theme';
 // Components
 import NodeCountSelector from 'shared/NodeCountSelector';
 
+function renderWithProps(props) {
+  return render(
+    <ThemeProvider theme={theme}>
+      <NodeCountSelector {...props} />
+    </ThemeProvider>
+  );
+}
+
 describe('NodeCountSelector', () => {
   afterEach(cleanup);
 
   it('renders without crashing', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <NodeCountSelector />
-      </ThemeProvider>
-    );
+    renderWithProps({});
   });
 
   it('shows 1 picker with autoscale off', () => {
-    const { getAllByTestId } = render(
-      <ThemeProvider theme={theme}>
-        <NodeCountSelector autoscalingEnabled={false} />
-      </ThemeProvider>
-    );
+    const { getAllByTestId } = renderWithProps({ autoscalingEnabled: false });
 
     expect(getAllByTestId('node-count-selector-picker').length).toBe(1);
   });
 
   it('shows 2 pickers with autoscale on', () => {
-    const { getAllByTestId } = render(
-      <ThemeProvider theme={theme}>
-        <NodeCountSelector autoscalingEnabled={true} />
-      </ThemeProvider>
-    );
+    const { getAllByTestId } = renderWithProps({ autoscalingEnabled: true });
 
     expect(getAllByTestId('node-count-selector-picker').length).toBe(2);
   });
