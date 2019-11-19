7289d12e2dc25fcee0e2a21e9b4f2a5da8cd1534
axbarsan <mnlk123@yahoo.com>
2019-11-19 19:37:55 +0100

    Refactor 'Codeblock' and its internal components to be functional components

--- src/components/getting-started/codeblock.js
+++ src/components/getting-started/codeblock.js
@@ -2,7 +2,7 @@ import * as Helpers from 'lib/helpers';
 import copy from 'copy-to-clipboard';
 import Line from './line';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 // CodeBlock
@@ -24,60 +24,54 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 // Output and Prompt can be in any order. The copy to clipboard button will only
 // take the content in the Prompt tags.
 
-export class Prompt extends React.Component {
-  render() {
-    return <Line prompt={true} text={Helpers.dedent(this.props.children)} />;
-  }
-}
+export const Prompt = ({ children }) => {
+  return <Line prompt={true} text={Helpers.dedent(children)} />;
+};
 
-export class Output extends React.Component {
-  render() {
-    return <Line prompt={false} text={Helpers.dedent(this.props.children)} />;
-  }
-}
+export const Output = ({ children }) => {
+  return <Line prompt={false} text={Helpers.dedent(children)} />;
+};
 
-export class CodeBlock extends React.Component {
-  state = {
-    hovering: false,
-  };
+const getPromptLinesAsString = children => {
+  const string = React.Children.toArray(children)
+    .filter(x => {
+      return x.type.displayName === 'Prompt';
+    })
+    .map(x => {
+      return x.props.children;
+    })
+    .join('\n');
 
-  promptLinesAsString() {
-    var string = React.Children.toArray(this.props.children)
-      .filter(function(x) {
-        return x.type === Prompt;
-      })
-      .map(function(x) {
-        return x.props.children;
-      })
-      .join('\n');
+  return Helpers.dedent(string);
+};
 
-    return Helpers.dedent(string);
-  }
+export const CodeBlock = ({ children }) => {
+  const [isHovering, setHovering] = useState(false);
+  const [isClicked, setClicked] = useState(false);
 
-  copyCodeToClipboard(e) {
+  const preElement = useRef(null);
+
+  const copyCodeToClipboard = e => {
     e.preventDefault();
 
-    copy(this.promptLinesAsString());
+    const contentToCopy = getPromptLinesAsString(children);
 
-    this.setState({ clicked: false });
-  }
+    copy(contentToCopy);
+    setClicked(false);
+  };
 
-  classNames() {
-    var classNames = [];
-
-    // this.props.children is either an array or in the case of 1 child
+  const classNames = () => {
+    // props.children is either an array or in the case of 1 child
     // just that child object
     // So this makes sure I always have an array, and flattens it.
-    // TODO: Use React.Children. It exists to do this kind of stuff for me.
-    var childrenArray = [this.props.children].reduce(function(a, b) {
-      return a.concat(b);
-    }, []);
+    const childrenArray = React.Children.toArray(children);
 
-    classNames.push('codeblock--container');
-    if (this.state.hovering) {
+    const classNames = ['codeblock--container'];
+
+    if (isHovering) {
       classNames.push('hovering');
     }
-    if (this.state.clicked) {
+    if (isClicked) {
       classNames.push('clicked');
     }
     if (childrenArray.length === 1) {
@@ -85,54 +79,44 @@ export class CodeBlock extends React.Component {
     }
 
     return classNames.join(' ');
-  }
+  };
 
-  render() {
-    return (
-      <div className={this.classNames()}>
-        <pre>
-          <div
-            className='content'
-            ref={d => {
-              this.pre = d;
+  return (
+    <div className={classNames()}>
+      <pre>
+        <div className='content' ref={preElement}>
+          {children}
+        </div>
+        <div className='codeblock--buttons'>
+          <a
+            href='#'
+            onClick={copyCodeToClipboard}
+            onMouseOut={() => {
+              setHovering(false);
+            }}
+            onMouseOver={() => {
+              setHovering(true);
+            }}
+            onMouseUp={() => {
+              setClicked(true);
             }}
           >
-            {this.props.children}
-          </div>
-          <div className='codeblock--buttons'>
-            <a
-              href='#'
-              onClick={this.copyCodeToClipboard.bind(this)}
-              onMouseOut={function() {
-                this.setState({ hovering: false });
-              }.bind(this)}
-              onMouseOver={function() {
-                this.setState({ hovering: true });
-              }.bind(this)}
-              onMouseUp={function() {
-                this.setState({ clicked: true });
-              }.bind(this)}
-            >
-              <i aria-hidden='true' className='fa fa-content-copy' />
-            </a>
-          </div>
-          <ReactCSSTransitionGroup
-            transitionEnterTimeout={1000}
-            transitionLeaveTimeout={1000}
-            transitionName={'checkmark'}
-          >
-            {this.state.clicked ? (
-              <i
-                aria-hidden='true'
-                className='fa fa-done codeblock--checkmark'
-              />
-            ) : null}
-          </ReactCSSTransitionGroup>
-        </pre>
-      </div>
-    );
-  }
-}
+            <i aria-hidden='true' className='fa fa-content-copy' />
+          </a>
+        </div>
+        <ReactCSSTransitionGroup
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={1000}
+          transitionName={'checkmark'}
+        >
+          {isClicked ? (
+            <i aria-hidden='true' className='fa fa-done codeblock--checkmark' />
+          ) : null}
+        </ReactCSSTransitionGroup>
+      </pre>
+    </div>
+  );
+};
 
 Prompt.propTypes = {
   children: PropTypes.node,
