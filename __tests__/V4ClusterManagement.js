77e9de36e20ac42c2176cc3209131682e68f997a
Oriol Grau <ograup@gmail.com>
2019-11-19 17:12:34 +0100

    Deleted old tests file

--- __tests__/V4ClusterManagement.js
+++ __tests__/V4ClusterManagement.js
@@ -257,7 +257,7 @@ it('deletes a v4 cluster', async () => {
   clusterDeleteRequest.done();
 });
 
-it.skip('patches v4 cluster name correctly', async () => {
+it('patches v4 cluster name correctly', async () => {
   const newClusterName = 'New cluster name';
   const clusterName = V4_CLUSTER.name;
 
