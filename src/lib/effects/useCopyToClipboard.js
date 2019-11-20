f874c4751c0353217497c53a9ab36c90bb283ff2
axbarsan <mnlk123@yahoo.com>
2019-11-20 18:17:50 +0100

    Add a comment describing its usage to the 'useCopyToClipboard' hook

--- src/lib/effects/useCopyToClipboard.js
+++ src/lib/effects/useCopyToClipboard.js
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import copy from 'copy-to-clipboard';
 
+/**
+ * Hook for copying text to the user's clipboard
+ *
+ * Use setter to set value to null for resetting the copy state.
+ * @returns {[Boolean, (textToCopy: string|null) => void]} [isCopiedToClipboard, setCopyToClipboard]
+ */
 function useCopyToClipboard() {
   const [copiedText, copyToClipboard] = useState(null);
 
