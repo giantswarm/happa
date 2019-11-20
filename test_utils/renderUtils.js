6f750e769eac474654d2c1d3bed983fdf40e66bc
Adrian Barsan <mnlk123@yahoo.com>
2019-11-20 14:39:22 +0100

    Respect worker node limit when creating cluster (#828)
    * Set the maximum number of worker nodes with the value received from the API
    
    - Make the NodeCountSelector component accept minimum and maximum values
    - Make the number of maximum worker nodes be dictated by the API
    
    * Get maximum node count selector from API for scaling clusters
    
    * Add the ability to use a partial constraints declaration for the 'NodeCountSelector' component
    
    * Use the default maximum value if there is no maximum value
    
    - Rename the 'maxWorkersPerClusters' to 'maxWorkerPerCluster'
    - Use the default max value of 'NodeCountSelector' if the maximum worker count from the API is 0
    
    * Add a better way of merging constraints, to prevent undefined values
    
    * Skip merging constraints if the current constraints are actually the default ones
    
    * Remove 'TODO' comments
    
    * Replace having a constraints object with having separate props for constraints
    
    * Destructure props
    
    * Remove unused state property
    
    * Refactor NodeCountSelector to not use internal state (since it's not needed), and clean up its implementation
    
    * Rename to pascal-cased name
    
    * Add default scaling props, for easier testing
    
    * Disconnect NumberPicker from store
    
    * Write some initial tests
    
    * Use utility render function instead of repeating myself 1231 times
    
    * Move NodeCountSelector test to the appropiate location
    
    * Add rest of NodeCountSelector component unit test
    
    * Rename testID variable so it makes more sense
    
    * Add new 'renderWithTheme()' and 'getComponentWithTheme()' helpers
    
    - Rename 'renderRouteWithStore' to 'renderUtils'
    - Add 'renderWithTheme()' and 'getComponentWithTheme()' utilities to 'renderUtils'
    - Replace the 'NodeCountSelector' component test to use the new 'getComponentWithTheme' helper
    
    * Don't run 'cleanup()' after each 'NodeCountSelector', because it is now a default behaviour with TestingLibrary + Jest

--- test_utils/renderUtils.js
+++ test_utils/renderUtils.js
@@ -2,12 +2,12 @@ import { ConnectedRouter } from 'connected-react-router';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import { push } from 'connected-react-router';
-import { render } from '@testing-library/react';
+import TestingLibrary, { render } from '@testing-library/react';
 import { ThemeProvider } from 'emotion-theming';
-import Routes from 'routes';
 import configureStore from 'stores/configureStore';
 import initialState from 'test_utils/initialState';
 import React from 'react';
+import Routes from 'routes';
 import theme from 'styles/theme';
 
 const initialStorage = {
@@ -19,7 +19,7 @@ const initialStorage = {
  * This function will render the whole app with a mocked store in the route
  * provided.
  * @param {String} route.
- * @param {Object} container An HTMLElement.
+ * @param {HTMLElement} container An HTMLElement.
  */
 export function renderRouteWithStore(
   initialRoute = '/',
@@ -46,4 +46,27 @@ export function renderRouteWithStore(
   store.dispatch(push(initialRoute));
 
   return app;
+}
+
+/**
+ * Render component in the Theme context
+ * @param {React.ReactType} component The React Component
+ * @param {<P extends Record<string, any>>React.ComponentProps<P>} props Props to pass to the component
+ * @param {<Q extends TestingLibrary.Queries>TestingLibrary.RenderOptions<Q>} [options] Testing library render options
+ */
+export function renderWithTheme(component, props, options) {
+  return render(getComponentWithTheme(component, props), options);
+}
+
+/**
+ * Include component in the Theme context and return it
+ * @param {React.ReactType} Component The React Component
+ * @param {<P extends Record<string, any>>React.ComponentProps<P>} props Props to pass to the component
+ */
+export function getComponentWithTheme(Component, props) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...props} />
+    </ThemeProvider>
+  );
 }
