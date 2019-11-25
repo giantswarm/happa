3557e224b62852eb5bd81a0e537f956e12306942
Marian Steinbach <marian@giantswarm.io>
2019-11-25 15:52:21 +0100

    Modify cluster overview for deleted clusters (#849)
    * Update giantswarm-js-client
    
    * Add color variables for dark blue gradient steps
    
    * Render reduced component for deleted cluster
    
    * Remove semicolons
    
    * Rename module, convert to styled component
    
    * Add dark blue shades to theme
    
    * Boyscouting: consolidte theme files
    
    * Fix import for renamed module
    
    * A bit of structure
    
    * Update src/components/home/ClusterDashboardItem.js
    
    Co-Authored-By: Oliver Ponder <oliverponder@giantswarm.io>
    
    * Boyscouting: Fix theme usage
    
    * Change composed styles syntax
    
    * Syntax fixed for styled
    
    * Change styled syntax
    
    * Fix error
    
    * Revert code style change

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,32 @@
 const theme = {
   colors: {
+    // dark blue as defined in Giant Swarm's style guide
+    darkBlue: '#234a61',
+
+    // Darker shades of above darkBlue.
+    // The higher the number, the darker the tone.
+    darkBlueDarker1: '#204358',
+    darkBlueDarker2: '#1c3c4f',
+    darkBlueDarker3: '#193545',
+    darkBlueDarker4: '#162e3d',
+    darkBlueDarker5: '#132834',
+    darkBlueDarker6: '#10212c',
+    darkBlueDarker7: '#0d1b23',
+    darkBlueDarker8: '#0a141b',
+    darkBlueDarker9: '#060c0f',
+
+    // Lighter shades of above darkBlue.
+    // The higher the number, the brighter the tone.
+    darkBlueLighter1: '#375b70',
+    darkBlueLighter2: '#4c6c7e',
+    darkBlueLighter3: '#617d8d',
+    darkBlueLighter4: '#768e9d',
+    darkBlueLighter5: '#8ca0ac',
+    darkBlueLighter6: '#a2b3bc',
+    darkBlueLighter7: '#b9c5cd',
+    darkBlueLighter8: '#d0d8dd',
+    darkBlueLighter9: '#e7ebee',
+
     shade1: '#1E4156',
     shade2: '#224A61',
     shade3: '#234D65',
@@ -10,9 +37,11 @@ const theme = {
     shade8: '#5c7f9a',
     shade9: '#316786',
     shade10: '#1b3848',
+
     white1: '#FFFFFF',
     white2: '#D7D7D7',
     white3: '#B9C1C8',
+
     whiteInput: '#f0f0f0',
     yellow1: '#ddb03a',
     gold: '#ce990f',
