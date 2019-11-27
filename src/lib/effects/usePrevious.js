e5c89efc1267f6d1ffd5f88c67ad647c20a0cbb1
Oriol Grau <ograup@gmail.com>
2019-11-27 17:47:28 +0100

    Credit

--- src/lib/effects/usePrevious.js
+++ src/lib/effects/usePrevious.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 
+// From https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
 function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
