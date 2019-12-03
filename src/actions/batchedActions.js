3b40435a4fe9cb991b64172f760368febdf32b0f
Oriol Grau <ograup@gmail.com>
2019-12-03 18:05:49 +0100

    All actions imported

--- src/actions/batchedActions.js
+++ src/actions/batchedActions.js
@@ -3,18 +3,18 @@ import { FlashMessage, messageTTL, messageType } from 'lib/flash_message';
 import GiantSwarm from 'giantswarm';
 
 // actions
-import { refreshUserInfo } from './userActions';
-import { organizationsLoad } from './organizationActions';
-import { clustersLoad } from './clusterActions';
-import { catalogsLoad } from './catalogActions';
+import * as userActions from './userActions';
+import * as organizationActions from './organizationActions';
+import * as clusterActions from './clusterActions';
+import * as catalogActions from './catalogActions';
 
 export function batchedLayout() {
   return async (dispatch, getState) => {
     try {
-      await dispatch(refreshUserInfo());
-      await dispatch(organizationsLoad());
-      await dispatch(clustersLoad());
-      await dispatch(catalogsLoad());
+      await dispatch(userActions.refreshUserInfo());
+      await dispatch(organizationActions.organizationsLoad());
+      await dispatch(clusterActions.clustersLoad());
+      await dispatch(catalogActions.catalogsLoad());
     } catch (err) {
       console.error('Error in batchedLayout', err);
     }
