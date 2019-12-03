ec573625048166201da0c3ecc545646b78f1324a
Gerald Pape <gerald@giantswarm.io>
2019-12-03 16:45:21 +0100

    components/home -> components/Home

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,7 @@ import AccountSettings from './account_settings';
 import AppCatalog from './app_catalog/AppCatalog';
 import DocumentTitle from 'react-document-title';
 import GiantSwarm from 'giantswarm';
-import Home from './home/Home';
+import Home from './Home/Home';
 import LoadingOverlay from './UI/loading_overlay';
 import Modals from './modals/Modals';
 import Navigation from './UI/navigation';
