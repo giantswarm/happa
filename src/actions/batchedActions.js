9bbce673b8d2f0aedc81107ef0e69d46aa1d4014
Oriol Grau <ograup@gmail.com>
2019-12-03 17:18:20 +0100

    Cleanup

--- src/actions/batchedActions.js
+++ src/actions/batchedActions.js
@@ -8,9 +8,6 @@ import { organizationsLoad } from './organizationActions';
 import { clustersLoad } from './clusterActions';
 import { catalogsLoad } from './catalogActions';
 
-// This is the first component that loads,
-// and refreshUserInfo and the subsequent organisationsLoad() are the
-// first calls happa makes to the API.
 export function batchedLayout() {
   return async (dispatch, getState) => {
     try {
