d198faae8fb3a3a46239615f60b27cd560ea0e04
Oriol Grau <ograup@gmail.com>
2019-12-11 12:17:25 +0100

    Mods in loadingReducer

--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -1,22 +1,23 @@
-import * as types from 'actions/actionTypes';
 import produce from 'immer';
 
-const loadingReducer = produce(
-  (draft, action) => {
-    const { type } = action;
-    const matches = /(.*)_(REQUEST|SUCCESS|FAILURE|FINISHED)/.exec(type);
+const initialState = {
+  CLUSTER_LIST: false,
+  CLUSTERS_DETAILS: false,
+};
 
-    // not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
-    if (!matches) return;
+const loadingReducer = produce((draft, action) => {
+  const { type } = action;
+  const matches = /(.*)_(REQUEST|SUCCESS|FAILURE|FINISHED)/.exec(type);
 
-    const [, requestName, requestState] = matches;
-    // Store whether a request is happening at the moment or not
-    // e.g. will be true when receiving GET_TODOS_REQUEST
-    //      and false when receiving GET_TODOS_SUCCESS / GET_TODOS_FAILURE
-    draft[requestName] = requestState === 'REQUEST';
-    return;
-  },
-  { CLUSTER_LIST: false }
-);
+  // not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
+  if (!matches) return;
+
+  const [, requestName, requestState] = matches;
+  // Store whether a request is happening at the moment or not
+  // e.g. will be true when receiving GET_TODOS_REQUEST
+  //      and false when receiving GET_TODOS_SUCCESS / GET_TODOS_FAILURE
+  draft[requestName] = requestState === 'REQUEST';
+  return;
+}, initialState);
 
 export default loadingReducer;
