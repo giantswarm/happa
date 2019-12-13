a12919434c54a91b29e9a4e5d70b8834801f06cb
Gerald Pape <gerald@giantswarm.io>
2019-12-13 11:19:48 +0100

    split mockHttpCalls into separate files

---
+++ testUtils/mockHttpCalls/index.js
@@ -0,0 +1,34 @@
+/***** Constants *****/
+export * from './constantsAndHelpers';
+
+/***** Responses *****/
+
+// Info
+export * from './info';
+
+// User
+export * from './user';
+
+// Auth
+export * from './auth';
+
+// Clusters
+export * from './clusters';
+
+// Organizations
+export * from './organizations';
+
+// Status
+export * from './status';
+
+// Apps
+export * from './apps';
+
+// Node Pools
+export * from './nodePools';
+
+// Releases
+export * from './releases';
+
+// App Catalogs
+export * from './appCatalogs';
