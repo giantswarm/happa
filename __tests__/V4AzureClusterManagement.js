75c61781d91b7f1b7be8ba7ab199d1ba5028da4c
axbarsan <mnlk123@yahoo.com>
2020-01-16 18:53:29 +0100

    Add failing azure scaling modal test

--- __tests__/V4AzureClusterManagement.js
+++ __tests__/V4AzureClusterManagement.js
@@ -1,5 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
+
+import { fireEvent, wait } from '@testing-library/react';
+import nock from 'nock';
+import { StatusCodes } from 'shared/constants';
 import {
+  API_ENDPOINT,
   appCatalogsResponse,
   appsResponse,
   azureInfoResponse,
@@ -14,12 +19,13 @@ import {
   v4AzureClusterStatusResponse,
   v4ClustersResponse,
 } from 'testUtils/mockHttpCalls';
-import { getNumberOfNodes } from 'utils/clusterUtils';
 import { renderRouteWithStore } from 'testUtils/renderUtils';
-import { wait } from '@testing-library/react';
+import { getNumberOfNodes } from 'utils/clusterUtils';
 
 // Cluster and route we are testing with.
 const ROUTE = `/organizations/${ORGANIZATION}/clusters/${V4_CLUSTER.id}`;
+
+const minNodesCount = 3;
 
 // Tests setup
 const requests = {};
@@ -95,5 +101,77 @@ it('renders all the v4 Azure cluster data correctly', async () => {
   });
 });
 
-it.skip(`shows the v4 Azure cluster scaling modal when the button is clicked with default values and 
-scales correctly`, async () => {});
+it(`shows the v4 Azure cluster scaling modal when the button is clicked with default values and
+scales correctly`, async () => {
+  const cluster = v4AzureClusterResponse;
+  const defaultScaling = cluster.scaling;
+  const increaseByCount = 1;
+
+  const newScaling = {
+    min: defaultScaling.min + increaseByCount,
+    max: defaultScaling.max + increaseByCount,
+  };
+  // Adding another worker
+  const newWorkers = [...cluster.workers, cluster.workers[0]];
+
+  const scaleResponse = {
+    ...cluster,
+    scaling: newScaling,
+    workers: newWorkers,
+  };
+
+  // Cluster scale request
+  const scaleRequest = nock(API_ENDPOINT)
+    .intercept(`/v4/clusters/${cluster.id}/`, 'PATCH')
+    .reply(StatusCodes.Ok, scaleResponse);
+
+  const { getByText, findByText, getByLabelText, getByDisplayValue } = renderRouteWithStore(ROUTE);
+
+  const nodesTitle = await findByText('Nodes');
+  const nodesCounter = nodesTitle.nextSibling;
+
+  expect(nodesCounter.textContent).toBe(String(minNodesCount));
+
+  // Replace status response
+  requests.status.persist(false);
+  requests.status = getPersistedMockCall(`/v4/clusters/${cluster.id}/status/`, {
+    ...v4AzureClusterStatusResponse,
+    cluster: {
+      ...v4AzureClusterStatusResponse.cluster,
+      scaling: { desiredCapacity: newScaling.max },
+    },
+  });
+
+  // Simulate click on the Edit button
+  fireEvent.click(getByText(/edit/i));
+
+  // Check if the modal is in the document
+  const modalTitle = await findByText(/edit scaling settings for/i);
+  expect(modalTitle).toHaveTextContent(cluster.id);
+
+  // Check if the node count selector is there, and has the right value
+  const nodeCountInput = getByDisplayValue(String(defaultScaling.min));
+
+  // Set the new node count
+  fireEvent.change(nodeCountInput, { target: { value: newScaling.min } });
+
+  // const textButton = `Increase minimum number of nodes by ${increaseByCount}`;
+  // const submitButton = getByText(textButton);
+  // fireEvent.click(submitButton);
+
+  // // Wait for the Flash message to appear
+  // await wait(() => {
+  //   getByText(/the cluster will be scaled within the next couple of minutes./i);
+  //   // Does the cluster have node values updated?
+  //   expect(getByText(`Pinned at ${newScaling.min}`));
+  // });
+
+  // scaleRequest.done();
+
+  // // Restore status response
+  // requests.status.persist(false);
+  // requests.status = getPersistedMockCall(
+  //   `/v4/clusters/${cluster.id}/status/`,
+  //   v4AzureClusterResponse
+  // );
+});
