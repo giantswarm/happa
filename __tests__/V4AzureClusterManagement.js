2f9df6bb79e6f8fec83f72e1850e2ec0a5c7941b
Adrian Barsan <mnlk123@yahoo.com>
2020-01-21 10:07:08 +0100

    Add cluster scaling tests (#998)
    * Add basic setup for ScaleClusterModal tests
    
    * Add helper for rendering components with store
    
    * Add prereqs for cancel button test
    
    * Add failing azure scaling modal test
    
    * Create scaling modal test for Azure clusters
    
    - Refactor V4AzureClusterManagement requests
    - Create Scaling test for Azure clusters
    
    * Add KVM cluster scaling test
    
    * Add UI tests for ScaleClusterModal component
    
    * Silence console error in the 'ScaleClusterModal' error test

--- __tests__/V4AzureClusterManagement.js
+++ __tests__/V4AzureClusterManagement.js
@@ -1,9 +1,15 @@
 import '@testing-library/jest-dom/extend-expect';
+
+import { fireEvent, wait } from '@testing-library/react';
+import nock from 'nock';
+import { StatusCodes } from 'shared/constants';
 import {
+  API_ENDPOINT,
   appCatalogsResponse,
   appsResponse,
   azureInfoResponse,
-  getPersistedMockCall,
+  getMockCall,
+  getMockCallTimes,
   ORGANIZATION,
   orgResponse,
   orgsResponse,
@@ -14,61 +20,48 @@ import {
   v4AzureClusterStatusResponse,
   v4ClustersResponse,
 } from 'testUtils/mockHttpCalls';
-import { getNumberOfNodes } from 'utils/clusterUtils';
 import { renderRouteWithStore } from 'testUtils/renderUtils';
-import { wait } from '@testing-library/react';
+import { getNumberOfNodes } from 'utils/clusterUtils';
 
 // Cluster and route we are testing with.
 const ROUTE = `/organizations/${ORGANIZATION}/clusters/${V4_CLUSTER.id}`;
 
-// Tests setup
-const requests = {};
+const minNodesCount = 3;
+
+beforeAll(() => {
+  nock.disableNetConnect();
+});
+
+afterAll(() => {
+  nock.enableNetConnect();
+});
 
 // Responses to requests
-beforeAll(() => {
-  requests.userInfo = getPersistedMockCall('/v4/user/', userResponse);
-  requests.info = getPersistedMockCall('/v4/info/', azureInfoResponse);
-  requests.organizations = getPersistedMockCall(
-    '/v4/organizations/',
-    orgsResponse
-  );
-  requests.organization = getPersistedMockCall(
-    `/v4/organizations/${ORGANIZATION}/`,
-    orgResponse
-  );
-  requests.clusters = getPersistedMockCall('/v4/clusters/', v4ClustersResponse);
-  requests.cluster = getPersistedMockCall(
-    `/v4/clusters/${V4_CLUSTER.id}/`,
-    v4AzureClusterResponse
-  );
-  requests.status = getPersistedMockCall(
+beforeEach(() => {
+  getMockCall('/v4/user/', userResponse);
+  getMockCall('/v4/info/', azureInfoResponse);
+  getMockCall('/v4/organizations/', orgsResponse);
+  getMockCallTimes(`/v4/organizations/${ORGANIZATION}/`, orgResponse, 2);
+  getMockCall('/v4/clusters/', v4ClustersResponse);
+  // eslint-disable-next-line no-magic-numbers
+  getMockCallTimes(`/v4/clusters/${V4_CLUSTER.id}/`, v4AzureClusterResponse, 4);
+  getMockCallTimes(
     `/v4/clusters/${V4_CLUSTER.id}/status/`,
-    v4AzureClusterStatusResponse
+    v4AzureClusterStatusResponse,
+    // eslint-disable-next-line no-magic-numbers
+    3
   );
-  requests.apps = getPersistedMockCall(
-    `/v4/clusters/${V4_CLUSTER.id}/apps/`,
-    appsResponse
-  );
-  // TODO we are not requesting this in v5 cluster calls
-  // Empty response
-  requests.keyPairs = getPersistedMockCall(
-    `/v4/clusters/${V4_CLUSTER.id}/key-pairs/`
-  );
-  requests.credentials = getPersistedMockCall(
-    `/v4/organizations/${ORGANIZATION}/credentials/`
-  );
-  requests.releases = getPersistedMockCall('/v4/releases/', releasesResponse);
-  requests.appcatalogs = getPersistedMockCall(
-    '/v4/appcatalogs/',
-    appCatalogsResponse
-  );
+  getMockCall(`/v4/clusters/${V4_CLUSTER.id}/apps/`, appsResponse);
+  getMockCallTimes(`/v4/clusters/${V4_CLUSTER.id}/key-pairs/`, [], 2);
+  getMockCallTimes(`/v4/organizations/${ORGANIZATION}/credentials/`, [], 2);
+  getMockCall('/v4/releases/', releasesResponse);
+  getMockCall('/v4/appcatalogs/', appCatalogsResponse);
 });
 
 // Stop persisting responses
-afterAll(() => {
-  Object.keys(requests).forEach(req => {
-    requests[req].persist(false);
-  });
+afterEach(() => {
+  expect(nock.isDone());
+  nock.cleanAll();
 });
 
 it('renders all the v4 Azure cluster data correctly', async () => {
@@ -95,5 +88,67 @@ it('renders all the v4 Azure cluster data correctly', async () => {
   });
 });
 
-it.skip(`shows the v4 Azure cluster scaling modal when the button is clicked with default values and 
-scales correctly`, async () => {});
+it(`shows the v4 Azure cluster scaling modal when the button is clicked with default values and
+scales correctly`, async () => {
+  const cluster = v4AzureClusterResponse;
+  const defaultScaling = cluster.scaling;
+  const increaseByCount = 1;
+
+  const newScaling = {
+    min: defaultScaling.min + increaseByCount,
+    max: defaultScaling.max + increaseByCount,
+  };
+  // Add another worker
+  const newWorkers = [...cluster.workers, cluster.workers[0]];
+
+  const scaleResponse = {
+    ...cluster,
+    scaling: newScaling,
+    workers: newWorkers,
+  };
+
+  getMockCall(`/v4/clusters/${cluster.id}/status/`, {
+    ...v4AzureClusterStatusResponse,
+    cluster: {
+      ...v4AzureClusterStatusResponse.cluster,
+      scaling: { desiredCapacity: newScaling.min },
+    },
+  });
+
+  // Cluster scale request
+  const scaleRequest = nock(API_ENDPOINT)
+    .intercept(`/v4/clusters/${cluster.id}/`, 'PATCH')
+    .reply(StatusCodes.Ok, scaleResponse);
+
+  const { getByText, findByText, getByDisplayValue } = renderRouteWithStore(
+    ROUTE
+  );
+
+  const nodesTitle = await findByText('Nodes');
+  const nodesCounter = nodesTitle.nextSibling;
+
+  expect(nodesCounter).toHaveTextContent(String(minNodesCount));
+
+  // Simulate click on the Edit button
+  fireEvent.click(getByText(/edit/i));
+
+  // Check if the modal is in the document
+  const modalTitle = await findByText(/edit scaling settings for/i);
+  expect(modalTitle).toHaveTextContent(cluster.id);
+
+  // Check if the node count selector is there, and has the right value
+  const nodeCountInput = getByDisplayValue(String(defaultScaling.min));
+
+  // Set the new node count
+  fireEvent.change(nodeCountInput, { target: { value: newScaling.min } });
+
+  const submitButtonLabel = `Add ${increaseByCount} worker node`;
+  const submitButton = getByText(submitButtonLabel);
+  fireEvent.click(submitButton);
+
+  await findByText(
+    /the cluster will be scaled within the next couple of minutes./i
+  );
+
+  scaleRequest.done();
+});
