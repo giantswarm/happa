f183e8c5e527924530054b8309796364e72f327c
axbarsan <mnlk123@yahoo.com>
2020-02-21 14:03:10 +0100

    Refactor ForgotPassword tests to use the new structure

--- __tests__/ForgotPassword.js
+++ __tests__/ForgotPassword.js
@@ -1,70 +1,95 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import { fireEvent, wait } from '@testing-library/react';
+import { forceRemoveAll } from 'lib/flashMessage';
 import nock from 'nock';
 import { StatusCodes } from 'shared/constants';
 import { AppRoutes } from 'shared/constants/routes';
+import { AWSInfoResponse, getMockCall } from 'testUtils/mockHttpCalls';
 import { renderRouteWithStore } from 'testUtils/renderUtils';
 
-it('takes us to the forgot password form when clicking on "Forgot your password?" from the login form', async () => {
-  // Given I am not logged in and visit the app.
-  const { getByText } = renderRouteWithStore(AppRoutes.Home, {}, {});
-
-  // Wait till the app is ready and we're on the login page.
-  await wait(() => {
-    expect(getByText(/Log in to Giant Swarm/i)).toBeInTheDocument();
+describe('ForgotPassword', () => {
+  beforeAll(() => {
+    nock.disableNetConnect();
   });
 
-  // When I click "Forgot your password?".
-  const forgotPasswordLink = getByText('Forgot your password?');
-  fireEvent.click(forgotPasswordLink);
-
-  // Then I should get redirected to the Forgot Password Form.
-  await wait(() => {
-    expect(
-      getByText(/Enter the email you used to sign-up/i)
-    ).toBeInTheDocument();
+  afterAll(() => {
+    nock.enableNetConnect();
   });
-});
 
-it('gives me a confirmation message after entering something into the email field and clicking submit ', async () => {
-  // Given Passage responds favourably to a password reset call.
-  const forgotPasswordRequest = nock('http://5.6.7.8')
-    .post('/recovery/', { email: 'iforgotmypassword@giantswarm.io' })
-    .reply(StatusCodes.Ok, {
-      email: 'iforgotmypassword@giantswarm.io',
-      message: 'Token created',
-      valid_until: '2019-09-21T18:14:11.347485+00:00',
+  afterEach(() => {
+    if (!nock.isDone()) {
+      // eslint-disable-next-line no-console
+      console.error('Nock has pending mocks:', nock.pendingMocks());
+    }
+
+    expect(nock.isDone()).toBeTruthy();
+    nock.cleanAll();
+
+    forceRemoveAll();
+  });
+
+  it('takes us to the forgot password form when clicking on "Forgot your password?" from the login form', async () => {
+    // Given I am not logged in and visit the app.
+    const { getByText } = renderRouteWithStore(AppRoutes.Home, {}, {});
+
+    // Wait till the app is ready and we're on the login page.
+    await wait(() => {
+      expect(getByText(/Log in to Giant Swarm/i)).toBeInTheDocument();
     });
 
-  // And I am not logged in and visit the app.
-  const { getByText, getByLabelText } = renderRouteWithStore(
-    AppRoutes.ForgotPassword,
-    {},
-    {}
-  );
+    // When I click "Forgot your password?".
+    const forgotPasswordLink = getByText('Forgot your password?');
+    fireEvent.click(forgotPasswordLink);
 
-  // Wait till the app is ready and we're on the forgot password page.
-  await wait(() => {
-    expect(
-      getByText(/Enter the email you used to sign-up/i)
-    ).toBeInTheDocument();
+    // Then I should get redirected to the Forgot Password Form.
+    await wait(() => {
+      expect(
+        getByText(/Enter the email you used to sign-up/i)
+      ).toBeInTheDocument();
+    });
   });
 
-  // When I type in my email.
-  const emailInput = getByLabelText('Email');
-  fireEvent.change(emailInput, {
-    target: { value: 'iforgotmypassword@giantswarm.io' },
+  it('gives me a confirmation message after entering something into the email field and clicking submit ', async () => {
+    getMockCall('/v4/info/', AWSInfoResponse);
+    getMockCall('/v4/appcatalogs/');
+    getMockCall('/v4/organizations/');
+    getMockCall('/v4/clusters/');
+
+    // Given Passage responds favourably to a password reset call.
+    nock(global.config.passageEndpoint)
+      .post('/recovery/', { email: 'iforgotmypassword@giantswarm.io' })
+      .reply(StatusCodes.Ok, {
+        email: 'iforgotmypassword@giantswarm.io',
+        message: 'Token created',
+        valid_until: '2019-09-21T18:14:11.347485+00:00',
+      });
+
+    // And I am not logged in and visit the app.
+    const { getByText, getByLabelText } = renderRouteWithStore(
+      AppRoutes.ForgotPassword
+    );
+
+    // Wait till the app is ready and we're on the forgot password page.
+    await wait(() => {
+      expect(
+        getByText(/Enter the email you used to sign-up/i)
+      ).toBeInTheDocument();
+    });
+
+    // When I type in my email.
+    const emailInput = getByLabelText('Email');
+    fireEvent.change(emailInput, {
+      target: { value: 'iforgotmypassword@giantswarm.io' },
+    });
+
+    // And click submit
+    const button = getByText('Submit');
+    fireEvent.click(button);
+
+    // Then I should see a confirmation message.
+    await wait(() => {
+      expect(getByText(/Check your mail!/i)).toBeInTheDocument();
+    });
   });
-
-  // And click submit
-  const button = getByText('Submit');
-  fireEvent.click(button);
-
-  // Then I should see a confirmation message.
-  await wait(() => {
-    expect(getByText(/Check your mail!/i)).toBeInTheDocument();
-  });
-
-  forgotPasswordRequest.done();
 });
