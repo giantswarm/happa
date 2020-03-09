59d8b1c95b71a394e09e80126761f08f3ffc027d
Oriol Grau <ograup@gmail.com>
2020-03-09 09:55:42 +0100

    Refactored filter

--- src/components/Organizations/Detail/Detail.js
+++ src/components/Organizations/Detail/Detail.js
@@ -68,9 +68,9 @@ function mapStateToProps(state, ownProps) {
   const allClusters = state.entities.clusters.items;
   let clusters = [];
 
-  clusters = Object.keys(allClusters)
-    .filter(id => allClusters[id].owner === ownProps.match.params.orgId)
-    .map(id => allClusters[id]);
+  clusters = Object.values(allClusters).filter(
+    cluster => cluster.owner === ownProps.match.params.orgId
+  );
 
   const membersForTable = state.entities.organizations.items[
     ownProps.match.params.orgId
