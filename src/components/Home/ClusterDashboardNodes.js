e6137db0887437e2e24d7af4473f7818a9e5fe49
Oriol Grau <ograup@gmail.com>
2020-03-13 15:37:32 +0100

    Split ClusterDashboardResources into 3 components

---
+++ src/components/Home/ClusterDashboardNodes.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FallbackMessages } from 'shared/constants';
+import { FallbackSpan } from 'styles';
+import { isClusterYoungerThanOneHour } from 'utils/clusterUtils';
+
+// This component outputs 'Nodes not ready' or the actual number of nodes in the cluster.
+function ClusterDashboardNodes({ numberOfNodes, createDate }) {
+  // If it was created more than an hour ago, then we should not show this message
+  // because something went wrong, so it's best to make it noticeable.
+  if (numberOfNodes === 0 && isClusterYoungerThanOneHour(createDate))
+    return <FallbackSpan>{FallbackMessages.NODES_NOT_READY}</FallbackSpan>;
+
+  return (
+    <span>{`${numberOfNodes} ${numberOfNodes === 1 ? 'node' : 'nodes'}`}</span>
+  );
+}
+
+ClusterDashboardNodes.propTypes = {
+  createDate: PropTypes.string,
+  numberOfNodes: PropTypes.number,
+};
+
+export default ClusterDashboardNodes;
---
+++ src/components/Home/ClusterDashboardResourcesV4.js
@@ -0,0 +1,78 @@
+import * as actionTypes from 'actions/actionTypes';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  selectLoadingFlagByAction,
+  selectResourcesV4,
+} from 'selectors/clusterSelectors';
+import { Dot } from 'styles';
+import RefreshableLabel from 'UI/RefreshableLabel';
+
+import ClusterDashboardLoadingPlaceholder from './ClusterDashboardLoadingPlaceholder';
+import ClusterDashboardNodes from './ClusterDashboardNodes';
+
+function ClusterDashboardResourcesV4({
+  cluster,
+  loadingClusters,
+  loadingStatus,
+  resources,
+}) {
+  const { storage, numberOfNodes } = resources;
+  const loading = loadingClusters || loadingStatus;
+
+  if (loading)
+    return <ClusterDashboardLoadingPlaceholder isV5Cluster={false} />;
+
+  return (
+    <div data-testid='cluster-resources'>
+      <RefreshableLabel value={numberOfNodes}>
+        <ClusterDashboardNodes
+          numberOfNodes={numberOfNodes}
+          createDate={cluster.create_date}
+        />
+      </RefreshableLabel>
+      {cluster.kvm && (
+        <span>
+          <Dot style={{ paddingLeft: 0 }} />
+          <RefreshableLabel value={storage}>
+            <span>{storage ? storage : '0'} GB storage</span>
+          </RefreshableLabel>
+        </span>
+      )}
+    </div>
+  );
+}
+
+ClusterDashboardResourcesV4.propTypes = {
+  cluster: PropTypes.object,
+  nodePools: PropTypes.array,
+  resources: PropTypes.object,
+  loadingClusters: PropTypes.bool,
+  loadingStatus: PropTypes.bool,
+};
+
+const makeMapStateToProps = () => {
+  const mapStateToProps = (state, props) => {
+    const resources = selectResourcesV4();
+
+    return {
+      resources: resources(state, props),
+      loadingClusters: selectLoadingFlagByAction(
+        state,
+        actionTypes.CLUSTERS_DETAILS_REQUEST
+      ),
+      loadingStatus: selectLoadingFlagByAction(
+        state,
+        actionTypes.CLUSTER_LOAD_STATUS_REQUEST
+      ),
+    };
+  };
+
+  return mapStateToProps;
+};
+
+export default connect(
+  makeMapStateToProps,
+  undefined
+)(ClusterDashboardResourcesV4);
