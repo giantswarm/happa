8144ba52f39c66932b76069862c6464da432571c
axbarsan <mnlk123@yahoo.com>
2020-03-20 10:50:21 +0100

    Address objects inside of the error fallback helper

--- src/components/UI/ErrorFallback.js
+++ src/components/UI/ErrorFallback.js
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const DEFAULT_MESSAGE =
+  'There was a problem. Please try again later and, if the problem persists, contact Giant Swarm support.';
+
 // ErrorFallback takes an error string or an array of error strings and will render
 //  the message/s. If it receives a falsy value or an array of falsy values it will
 // render its children.
@@ -11,18 +14,29 @@ const ErrorWrapperSpan = styled.span`
   font-weight: 300;
 `;
 
-// This function joins error messages into a single string if it receives an array,
-// or returns the error if it is a single value of type string or null.
-// If an array of null values are passed, it returns null.
+/**
+ * Helper that finds the best way to return a pretty-printed
+ * version of the error object passed to it
+ * @param {any} errors - Error object
+ */
 function parseErrors(errors) {
   if (typeof errors === 'string' || !errors) return errors;
 
-  // Remove null values and joins values in a string
-  // TODO maybe we want to show errors in a different manner.
-  const notNullErrors = errors.filter(error => error);
-  if (notNullErrors.length === 0) return null;
+  let message = '';
 
-  return notNullErrors.join('. ');
+  if (!Array.isArray(errors)) {
+    // Maybe we're lucky and this is an error object/response object
+    message = errors.message || DEFAULT_MESSAGE;
+  } else {
+    // Remove nullable values and join values in a string
+    // TODO maybe we want to show errors in a different manner.
+    const notNullErrors = errors.filter(error => error);
+    if (notNullErrors.length === 0) return null;
+
+    message = notNullErrors.join('. ');
+  }
+
+  return message;
 }
 
 function ErrorFallback({ errors, children }) {
