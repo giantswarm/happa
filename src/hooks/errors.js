1c46a774b1eb17fd7a4de0be2b62d934db581435
Oliver Ponder <oliverponder@giantswarm.io>
2020-03-25 18:19:41 +0800

    Reuse existing selector.

--- src/hooks/errors.js
+++ src/hooks/errors.js
@@ -1,12 +1,13 @@
 import { SINGLE_ERROR_CLEAR } from 'actions/actionTypes';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectErrorByAction } from 'selectors/clusterSelectors';
 import { typeWithoutSuffix } from 'selectors/selectorUtils';
 
 export const useError = (errorType) => {
   const dispatch = useDispatch();
 
   return useSelector((state) => {
-    const errorMessage = state.errors[typeWithoutSuffix(errorType)] ?? null;
+    const errorMessage = selectErrorByAction(state, errorType);
 
     const clear = () => {
       dispatch({
