7b2cf3846567037ea4bb45cb0014d700fe8ce03d
Marian Steinbach <marian@giantswarm.io>
2020-04-03 11:25:31 +0200

    Add tooltips to explain DESIRED node count (#1292)
    * Add tooltip to V5ClusterDetailTable
    
    * Add overlay and info icon to V4 cluster details
    
    * Use shared constant for label
    
    * Make only icon reactive, not the label
    
    * Add space
    
    * Make multiple child elements one
    
    * Re-arrange OverlayTrigger content
    
    * Replace HTML entity with unicode apostrophe

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -16,4 +16,8 @@ export const Constants = {
   DEFAULT_NODEPOOL_NAME: 'Unnamed node pool',
   AWS_V5_VERSION: '10.0.0',
   AZURE_MULTI_AZ_VERSION: '11.1.0',
+
+  // UI labels
+  DESIRED_NODES_EXPLANATION:
+    'Autoscaler’s idea how many nodes would be required for the workloads',
 };
