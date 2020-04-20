9d9119b13d7015f42b89fe6857569358a3c88e02
Oliver Ponder <oliverponder@giantswarm.io>
2020-04-20 15:36:54 +0800

    On the App Detail Page, hide most chart versions, but make them expandable. (#1351)
    * Hide most chart versions, and make them expandable.
    
    * Rename method, it doesn't toggle anymore.
    
    * Improve alignment for a more compact view.
    
    * Update src/components/UI/AppDetails/VersionsRow.js
    
    Co-Authored-By: Gerald Pape <ubergesundheit@users.noreply.github.com>
    
    Co-authored-by: Gerald Pape <ubergesundheit@users.noreply.github.com>

--- src/components/UI/AppDetails/ChartVersionsTable.js
+++ src/components/UI/AppDetails/ChartVersionsTable.js
@@ -1,29 +1,22 @@
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Copyable from 'shared/Copyable';
-import Truncated from 'UI/Truncated';
 
-const StyledCopyable = styled(Copyable)``;
+import VersionsRow from './VersionsRow';
 
 const ChartVersionTable = styled.table`
   border: 1px solid ${(props) => props.theme.colors.shade4};
   margin-top: 10px;
-  max-width: 320px;
+  max-width: 325px;
   table-layout: fixed;
-  white-space: nowrap;
   float: right;
 
   td {
-    vertical-align: top;
+    vertical-align: middle;
+    line-height: 30px;
     code {
       max-width: 150px;
       display: inline-block;
-    }
-
-    ${StyledCopyable} {
-      float: left;
-      margin-bottom: 10px;
     }
   }
 
@@ -32,19 +25,9 @@ const ChartVersionTable = styled.table`
   }
 
   td.appVersion {
+    vertical-align: top;
     border-left: 1px dashed ${(props) => props.theme.colors.shade1};
     text-align: center;
-
-    ${StyledCopyable} {
-      display: inline-block;
-      float: none;
-      position: relative;
-      left: 8px;
-      code {
-        background-color: ${(props) => props.theme.colors.darkBlueLighter8};
-        color: ${(props) => props.theme.colors.darkBlue};
-      }
-    }
   }
 
   tr:nth-of-type(even) {
@@ -80,30 +63,11 @@ const ChartVersionsTable = (props) => {
         {Object.entries(groupedAppVersions).map(
           ([appVersionString, entries]) => {
             return (
-              <tr key={appVersionString}>
-                <td>
-                  {entries.map((appVersionObject) => {
-                    return (
-                      <StyledCopyable
-                        key={appVersionObject.version}
-                        copyText={appVersionObject.version}
-                      >
-                        <Truncated numStart={12} as='code'>
-                          {appVersionObject.version}
-                        </Truncated>
-                      </StyledCopyable>
-                    );
-                  })}
-                </td>
-
-                <td className='appVersion'>
-                  <StyledCopyable copyText={appVersionString}>
-                    <Truncated numStart={8} as='code'>
-                      {appVersionString}
-                    </Truncated>
-                  </StyledCopyable>
-                </td>
-              </tr>
+              <VersionsRow
+                appVersion={appVersionString}
+                entries={entries}
+                key={appVersionString}
+              />
             );
           }
         )}
