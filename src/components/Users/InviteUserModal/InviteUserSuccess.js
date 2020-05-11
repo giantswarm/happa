9f8950abfa53be56a6ca336098a5b8ee77babf5c
axbarsan <mnlk123@yahoo.com>
2020-05-11 11:16:32 +0200

    Refactor InviteUserSuccess content to use existent URIBlock component

--- src/components/Users/InviteUserModal/InviteUserSuccess.js
+++ src/components/Users/InviteUserModal/InviteUserSuccess.js
@@ -1,14 +1,10 @@
-import useCopyToClipboard from 'lib/effects/useCopyToClipboard';
+import URIBlock from 'Cluster/ClusterDetail/URIBlock';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
+import React from 'react';
 
 import { getInitialState } from '.';
 
 const InviteUserSuccess = ({ inviteForm, invitationResult }) => {
-  const [hasContentInClipboard, setClipboardContent] = useCopyToClipboard();
-
   /* prettier-ignore */
   let sendEmailContent = 'You\'ve chosen not to send them an email. Send them the link below to accept the invitation';
 
@@ -16,40 +12,12 @@ const InviteUserSuccess = ({ inviteForm, invitationResult }) => {
     sendEmailContent = `An email has been sent to ${inviteForm.email} with further instructions.`;
   }
 
-  // eslint-disable-next-line react/no-multi-comp
-  const getOverlayTooltip = () => (
-    <Tooltip id='tooltip'>Copy to clipboard.</Tooltip>
-  );
-
-  useEffect(() => {
-    const resetClipboardTimeout = 1000;
-
-    setTimeout(() => {
-      setClipboardContent(null);
-    }, resetClipboardTimeout);
-  }, [hasContentInClipboard, setClipboardContent]);
-
   return (
     <>
       <p>Invitation has been created succesfully!</p>
       <p>{sendEmailContent}</p>
       <label>Invitation Accept Link:</label>
-      <br />
-      <code>{invitationResult.invitation_accept_link}</code>
-      &nbsp;
-      {hasContentInClipboard ? (
-        <i aria-hidden='true' className='fa fa-done' />
-      ) : (
-        <OverlayTrigger overlay={getOverlayTooltip()} placement='top'>
-          <i
-            aria-hidden='true'
-            className='copy-link fa fa-content-copy'
-            onClick={() =>
-              setClipboardContent(invitationResult.invitation_accept_link)
-            }
-          />
-        </OverlayTrigger>
-      )}
+      <URIBlock>{invitationResult.invitation_accept_link}</URIBlock>
     </>
   );
 };
