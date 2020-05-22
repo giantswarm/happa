c0fd5992bd5817ec7cf18a40bc240b0f4cd97f78
axbarsan <mnlk123@yahoo.com>
2020-05-22 16:19:46 +0200

    Write component that holds the conversion to HA form

---
+++ src/components/Cluster/ClusterDetail/MasterNodes/MasterNodesConverter.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Button from 'UI/Button';
+
+const Strong = styled.strong`
+  font-weight: 700;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 16px 0 8px;
+`;
+
+interface IMasterNodeConverterProps
+  extends React.ComponentPropsWithoutRef<'div'> {
+  onApply?: () => void;
+  onCancel?: () => void;
+}
+
+const MasterNodeConverter: React.FC<IMasterNodeConverterProps> = ({
+  onApply,
+  onCancel,
+  ...rest
+}) => {
+  const handleEventListener = (
+    callback?: () => void
+  ): ((e: React.MouseEvent<HTMLDivElement>) => void) => {
+    return (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      // eslint-disable-next-line no-unused-expressions
+      callback?.();
+    };
+  };
+
+  return (
+    <div {...rest}>
+      <p>
+        <Strong>
+          Do you want to convert this cluster to use three instead of one master
+          nodes?
+        </Strong>
+      </p>
+      <p>
+        This will improve the cluster&rsquo;s resilience against data center
+        failures and keep the Kubernetes API available during upgrades, and will
+        also increase the resource cost.
+      </p>
+      <p>
+        <Strong>Note:</Strong> there is no way to undo this switch.
+      </p>
+      <ButtonWrapper>
+        <Button bsStyle='primary' onClick={handleEventListener(onApply)}>
+          Switch to high availability
+        </Button>
+        <Button onClick={handleEventListener(onCancel)}>Cancel</Button>
+      </ButtonWrapper>
+    </div>
+  );
+};
+
+MasterNodeConverter.propTypes = {
+  onApply: PropTypes.func,
+  onCancel: PropTypes.func,
+};
+
+export default MasterNodeConverter;
