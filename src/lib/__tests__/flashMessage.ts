0aef6d13ef2351d89fd0242a08d0521326344be6
axbarsan <mnlk123@yahoo.com>
2020-06-17 18:58:26 +0200

    Add test for duplicate flash messages

---
+++ src/lib/__tests__/flashMessage.ts
@@ -0,0 +1,52 @@
+import {
+  FlashMessage,
+  forceRemoveAll,
+  messageTTL,
+  messageType,
+} from 'lib/flashMessage';
+
+describe('FlashMessage', () => {
+  it(`won't display messages that are already in the queue by default`, () => {
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.ERROR,
+      messageTTL.MEDIUM
+    );
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.ERROR,
+      messageTTL.MEDIUM
+    );
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.ERROR,
+      messageTTL.MEDIUM
+    );
+
+    let notificationWrapper = document.querySelector(
+      '.noty_layout'
+    ) as HTMLElement;
+    expect(notificationWrapper.children).toHaveLength(1);
+
+    forceRemoveAll();
+
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.ERROR,
+      messageTTL.MEDIUM
+    );
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.ERROR,
+      messageTTL.MEDIUM
+    );
+    new FlashMessage(
+      `Yo! Something went wrong.`,
+      messageType.INFO,
+      messageTTL.MEDIUM
+    );
+
+    notificationWrapper = document.querySelector('.noty_layout') as HTMLElement;
+    expect(notificationWrapper.children).toHaveLength(2);
+  });
+});
