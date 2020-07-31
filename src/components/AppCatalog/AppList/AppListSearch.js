d4c3a8817a39e2c6710d206028847d22efd84423
Oliver Ponder <oliver.ponder@gmail.com>
2020-07-31 12:31:38 +0800

    No nested styles.

--- src/components/AppCatalog/AppList/AppListSearch.js
+++ src/components/AppCatalog/AppList/AppListSearch.js
@@ -6,35 +6,49 @@ const Form = styled('form')`
   width: 300px;
   font-size: 14px;
   position: relative;
+`;
 
-  input {
-    padding: 8px 10px;
-  }
+const InputWithIcon = styled('div')`
+  display: flex;
+  align-items: stretch;
+`;
 
-  a {
-    position: absolute;
-    right: -3px;
-    top: -8px;
-  }
+const SearchIcon = styled('i')`
+  font-size: 24px;
+  position: relative;
+  top: 6px;
+  margin-right: 4px;
+  color: #b9c1c8;
+`
 
-  i {
-    color: #fff;
-    font-size: 13px;
-    font-weight: 800;
-  }
+const Input = styled('input')`
+  padding: 8px 10px !important;
+  margin-bottom: 0px !important;
+`;
+
+const ClearLink = styled('a')`
+  position: absolute;
+  right: 10px;
+  top: -1px;
+`;
+
+const ClearIcon = styled('i')`
+  color: ${(props) => props.theme.colors.white1};
+  font-size: 13px;
+  font-weight: 800;
 `;
 
 const AppListSearch = ({ value, ...props }) => (
   <Form>
-    <div className='input-with-icon'>
-      <i className='fa fa-search' />
-      <input onChange={props.onChange} type='text' value={value} />
+    <InputWithIcon>
+      <SearchIcon className='fa fa-search' />
+      <Input onChange={props.onChange} type='text' value={value} />
       {value && (
-        <a onClick={props.onReset}>
-          <i className='fa fa-close' />
-        </a>
+        <ClearLink onClick={props.onReset}>
+          <ClearIcon className='fa fa-close' />
+        </ClearLink>
       )}
-    </div>
+    </InputWithIcon>
   </Form>
 );
 
