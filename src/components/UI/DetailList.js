2c5aa82a880613702128385ed4d7c4782c31daa2
Oliver Ponder <oliver.ponder@gmail.com>
2020-08-05 04:10:59 +0800

    Extract remaining styles and refactor into UI component.

---
+++ src/components/UI/DetailList.js
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Label = styled.div`
+  font-size: 12px;
+  text-transform: uppercase;
+  color: #ccc;
+  font-weight: unset;
+  line-height: unset;
+`;
+
+const Value = styled.div`
+  margin-bottom: 1em;
+  line-height: unset;
+
+  &.code {
+    font-family: 'Inconsolata';
+  }
+
+  &.breaking {
+    word-break: break-all;
+  }
+
+  button {
+    margin-top: 5px;
+  }
+
+  &.well {
+    padding: 20px;
+    border-radius: 5px;
+    margin-top: 5px;
+  }
+`;
+
+const DetailItem = styled.div``;
+
+const DetailList = (props) => {
+  return (
+    <DetailItem>
+      <Label>{props.title}</Label>
+      <Value className={props.className}>{props.children}</Value>
+    </DetailItem>
+  );
+};
+
+DetailList.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+export default DetailList;
