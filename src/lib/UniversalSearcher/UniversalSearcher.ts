4392a2cd2cb5fe74bff85fceb29eb6acbaacfb9d
axbarsan <mnlk123@yahoo.com>
2020-08-18 15:11:46 +0200

    Write tests for UniversalSearcher

--- src/lib/UniversalSearcher/UniversalSearcher.ts
+++ src/lib/UniversalSearcher/UniversalSearcher.ts
@@ -1,15 +1,39 @@
 import { ReactNode } from 'react';
 
 export interface IUniversalSearcher {
+  /**
+   * Register a new search filter.
+   * @param filter
+   */
   registerFilter<T, S>(filter: IUniversalSearcherFilter<T, S>): void;
+
+  /**
+   * Get all registered filters.
+   */
   getFilters(): UniversalSearchFilterMap;
+
+  /**
+   * Search for a specific term in the given state object.
+   * @param searchTerm - The given search term.
+   * @param state
+   * @param limit - The maximum number of returned results.
+   */
   search<S>(
-    term: string,
+    searchTerm: string,
     state: S,
     limit: number
   ): IUniversalSearcherResult<unknown>[];
+
+  /**
+   * Search for a specific term in the given state object, while only
+   * using a specific filter.
+   * @param searchTerm - The given search term.
+   * @param state
+   * @param limit - The maximum number of returned results.
+   * @param type - The name of the registered filter.
+   */
   search<T, S>(
-    term: string,
+    searchTerm: string,
     state: S,
     limit: number,
     type: string
@@ -17,25 +41,78 @@ export interface IUniversalSearcher {
 }
 
 export interface IUniversalSearcherResult<T> {
+  /**
+   * The name of the registered filter which returned the value.
+   */
   type: string;
+  /**
+   * The search result data structure.
+   */
   result: T;
 }
 
 export interface IUniversalSearcherFilter<T, S> {
+  /**
+   * A unique name to identify the filter.
+   */
   type: string;
+
+  /**
+   * A React component factory that will render the result row,
+   * in the search results dropdown.
+   *
+   * Note: To get consistent styling, wrap your component in
+   * the `components/UniversalSearch/filtersRendererWrapper` component.
+   */
   renderer: UniversalSearcherRenderer<T>;
+
+  /**
+   * The filter generator that will yield each search result.
+   *
+   * This is a generator because it allows filters to have
+   * 100% control on stuff such as the order of the search
+   * results, and it makes possible having a unique limit for
+   * the number of search results from all filters, without
+   * unnecessary computation of additional results.
+   * @param state
+   * @param term - The search term.
+   */
   searcher: (state: S, term: string) => Iterator<T>;
+
+  /**
+   * The factory that will generate the URL that the user
+   * will be redirected to when clicking on the search result.
+   */
   urlFactory: UniversalSearchURLFactory<T>;
 }
 
+/**
+ * A React component factory that will render the result row,
+ * in the search results dropdown.
+ * @param state
+ * @param searchTerm
+ * @param type - The name of the registered filter.
+ */
 export type UniversalSearcherRenderer<T> = (
   result: T,
   searchTerm: string,
   type: string
 ) => ReactNode;
 
-export type UniversalSearchURLFactory<T> = (result: T, term: string) => string;
+/**
+ * The factory that will generate the URL that the user
+ * will be redirected to when clicking on the search result.
+ * @param result - The search result.
+ * @param searchTerm
+ */
+export type UniversalSearchURLFactory<T> = (
+  result: T,
+  searchTerm: string
+) => string;
 
+/**
+ * A map of all the registered search filters.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type UniversalSearchFilterMap<T = unknown, S = any> = Record<
   string,
--- src/lib/UniversalSearcher/__tests__/UniversalSearcher.ts
+++ src/lib/UniversalSearcher/__tests__/UniversalSearcher.ts
@@ -24,6 +24,63 @@ interface ITestState {
 }
 
 describe('UniversalSearcher', () => {
+  it('returns all registered filters', () => {
+    const us = new UniversalSearcherImpl();
+
+    registerClusterSearcherFilter(us);
+    registerAppSearcherFilter(us);
+
+    const filters = us.getFilters();
+    expect(Object.entries(filters).length).toBe(2);
+    expect(filters.cluster.type).toBe('cluster');
+    expect(filters.app.type).toBe('app');
+  });
+
+  it('can search for different terms, with single registered type', () => {
+    const limit = 10;
+    const stateToSearch: Partial<ITestState> = {
+      clusters: {
+        items: [
+          {
+            id: '1sd1f',
+            name: 'Random cluster',
+            creationDate: 123313120,
+          },
+          {
+            id: '19sd1',
+            name: 'Some weird cluster',
+            creationDate: 4301234123,
+          },
+          {
+            id: '23sa1',
+            name: 'Some awesome cluster',
+            creationDate: 12312030123,
+          },
+          {
+            id: '1230ad',
+            name: `A cluster. That's it`,
+            creationDate: 15440450400,
+          },
+        ],
+      },
+    };
+    const us = new UniversalSearcherImpl();
+
+    registerClusterSearcherFilter(us);
+
+    let result = us.search('some', stateToSearch, limit);
+    expect(result.length).toBe(2);
+
+    result = us.search('amazing', stateToSearch, limit);
+    expect(result.length).toBe(0);
+
+    result = us.search('random', stateToSearch, limit);
+    expect(result.length).toBe(1);
+
+    result = us.search('crazy', stateToSearch, limit);
+    expect(result.length).toBe(0);
+  });
+
   it('can search for different terms, with multiple registered types', () => {
     const limit = 10;
     const stateToSearch: ITestState = {
@@ -93,6 +150,117 @@ describe('UniversalSearcher', () => {
     result = us.search('crazy', stateToSearch, limit);
     expect(result.length).toBe(0);
   });
+
+  it('can search for different terms, with multiple registered types, and single filter selected', () => {
+    const limit = 10;
+    const stateToSearch: ITestState = {
+      clusters: {
+        items: [
+          {
+            id: '1sd1f',
+            name: 'Random cluster',
+            creationDate: 123313120,
+          },
+          {
+            id: '19sd1',
+            name: 'Some weird cluster',
+            creationDate: 4301234123,
+          },
+          {
+            id: '23sa1',
+            name: 'Some awesome cluster',
+            creationDate: 12312030123,
+          },
+          {
+            id: '1230ad',
+            name: `A cluster. That's it`,
+            creationDate: 15440450400,
+          },
+        ],
+      },
+      apps: {
+        items: [
+          {
+            name: 'A cool app',
+            creationDate: 12321312,
+            catalog: 'random-catalog',
+          },
+          {
+            name: 'A not so cool app',
+            creationDate: 1234312365,
+            catalog: 'not-so-random-catalog',
+          },
+          {
+            name: 'An amazing app',
+            creationDate: 12549054,
+            catalog: 'random-catalog',
+          },
+          {
+            name: 'Aaaaa...aaapp',
+            creationDate: 12435055,
+            catalog: 'some-catalog',
+          },
+        ],
+      },
+    };
+    const us = new UniversalSearcherImpl();
+
+    registerClusterSearcherFilter(us);
+    registerAppSearcherFilter(us);
+
+    let result = us.search('some', stateToSearch, limit, 'app');
+    expect(result.length).toBe(0);
+
+    result = us.search('amazing', stateToSearch, limit, 'app');
+    expect(result.length).toBe(1);
+
+    result = us.search('random', stateToSearch, limit, 'app');
+    expect(result.length).toBe(0);
+
+    result = us.search('crazy', stateToSearch, limit, 'app');
+    expect(result.length).toBe(0);
+  });
+
+  it('limits the number of search results to the given value', () => {
+    const stateToSearch: Partial<ITestState> = {
+      clusters: {
+        items: [
+          {
+            id: '1sd1f',
+            name: 'A random cluster',
+            creationDate: 123313120,
+          },
+          {
+            id: '19sd1',
+            name: 'A very random cluster',
+            creationDate: 4301234123,
+          },
+          {
+            id: '23sa1',
+            name: 'A super random cluster',
+            creationDate: 12312030123,
+          },
+          {
+            id: '1230ad',
+            name: 'You guessed it. Another random cluster',
+            creationDate: 15440450400,
+          },
+        ],
+      },
+    };
+    const us = new UniversalSearcherImpl();
+
+    registerClusterSearcherFilter(us);
+
+    let result = us.search('random', stateToSearch, 2);
+    expect(result.length).toBe(2);
+
+    result = us.search('random', stateToSearch, 3);
+    expect(result.length).toBe(3);
+
+    result = us.search('random', stateToSearch, 9);
+    expect(result.length).toBe(4);
+  });
 });
 
 function registerClusterSearcherFilter(searcher: IUniversalSearcher) {
@@ -102,13 +270,11 @@ function registerClusterSearcherFilter(searcher: IUniversalSearcher) {
   ): Iterator<ITestCluster> {
     const termLowerCased = term.toLowerCase();
 
-    let containsTermInID = false;
-    let containsTermInName = false;
     for (const item of state.clusters.items) {
-      containsTermInID = item.id.toLowerCase().includes(termLowerCased);
-      containsTermInName = item.name.toLowerCase().includes(termLowerCased);
-
-      if (containsTermInID || containsTermInName) {
+      if (
+        item.id.toLowerCase().includes(termLowerCased) ||
+        item.name.toLowerCase().includes(termLowerCased)
+      ) {
         yield item;
       }
     }
