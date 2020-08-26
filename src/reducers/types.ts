85ccb3a44d9d0abfe02d3cea5cc5c9f920f11fb7
axbarsan <mnlk123@yahoo.com>
2020-08-26 10:15:05 +0200

    Revert adding cluster labels stuff to global state

--- src/reducers/types.ts
+++ src/reducers/types.ts
@@ -1,7 +1,7 @@
 import { IAppCatalogsState } from 'stores/appcatalog/types';
 import { ICPAuthState } from 'stores/cpauth/types';
 
-// Giving state a generic type for now, until whole state is typed
+// Giving state a generic type for now, until whole state is typed.
 export interface IState {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
@@ -10,10 +10,6 @@ export interface IState {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any;
     cpAuth: ICPAuthState;
-    clusterLabels: {
-      requestInProgress: string;
-      error: Error | null;
-    };
     catalogs: IAppCatalogsState;
   };
 }
