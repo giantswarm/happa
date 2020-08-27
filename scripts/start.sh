10b0a411d62c9efc99ef1c17863753c4cb6143ee
Gerald Pape <gerald@giantswarm.io>
2020-08-27 15:15:29 +0200

    split container build & update nginx image version

--- scripts/start.sh
+++ scripts/start.sh
@@ -60,8 +60,8 @@ gzip -f -9 -k /www/metadata.json
 
 # Create file to include into nginx config to use
 # the pods nameserver as resolver
-echo resolver $(awk '/^nameserver/{print $2}' /etc/resolv.conf) ";" > /etc/nginx/config/resolvers.conf
+echo resolver $(awk '/^nameserver/{print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
 
 echo ""
 echo "--- Starting Happa nginx server ---"
-exec nginx -c /etc/nginx/config/nginx.conf -g "daemon off;"
+exec nginx -c /etc/nginx/nginx.conf -g "daemon off;"
