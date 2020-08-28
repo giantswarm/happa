fe3cbdd0d3c609ad9a6a43dfd2751f6b05fb3ae9
axbarsan <mnlk123@yahoo.com>
2020-08-28 17:56:47 +0200

    Rewrite 'patchedAirbrakeRequester' in TypeScript

--- src/lib/patchedAirbrakeRequester.ts
+++ src/lib/patchedAirbrakeRequester.ts
@@ -3,27 +3,33 @@
 
 // Airbrake doesn't expose a easy interface for setting custom headers for each
 // request. We need to set headers for authentication against our API.
+import {
+  IHttpRequest,
+  IHttpResponse,
+} from '@airbrake/browser/dist/http_req/api';
 import fetch from 'cross-fetch';
+import { Store } from 'redux';
 import { StatusCodes } from 'shared/constants';
 
 export class Requester {
-  constructor(store) {
+  constructor(store: Store) {
     this.store = store;
   }
 
-  request = (req) => {
+  request = async (req: IHttpRequest): Promise<IHttpResponse> => {
     if (!this.store.getState().main.loggedInUser) {
       const err = new Error(
         `user is not logged in yet, unable to report error to GS API`
       );
-      throw err;
+
+      return Promise.reject(err);
     }
 
     const scheme = this.store.getState().main.loggedInUser.auth.scheme;
     const token = this.store.getState().main.loggedInUser.auth.token;
     const authHeader = `${scheme} ${token}`;
 
-    const opt = {
+    const opt: RequestInit = {
       method: req.method,
       body: req.body,
       headers: {
@@ -31,17 +37,17 @@ export class Requester {
       },
     };
 
-    return fetch(req.url, opt).then((resp) => {
-      if (resp.status === StatusCodes.Created) {
-        return { json: null };
-      }
+    const response = await fetch(req.url, opt);
+    if (response.status === StatusCodes.Created) {
+      return { json: null };
+    }
+    const body = await response.text();
+    const err = new Error(
+      `airbrake: fetch: unexpected response: code=${response.status} body='${body}'`
+    );
 
-      return resp.text().then((body) => {
-        const err = new Error(
-          `airbrake: fetch: unexpected response: code=${resp.status} body='${body}'`
-        );
-        throw err;
-      });
-    });
+    return Promise.reject(err);
   };
+
+  private readonly store: Store;
 }
