159deb811b35da9da7a35be830913adcd17de533
axbarsan <mnlk123@yahoo.com>
2020-09-01 14:00:44 +0200

    Add response types to passage client

--- src/lib/passageClient.ts
+++ src/lib/passageClient.ts
@@ -7,10 +7,50 @@ export interface IPassageConfig {
   timeout_ms?: number;
 }
 
-export interface IPassageInvitation {
+export interface IPassageCheckInviteResponse {
+  is_valid: boolean;
   email: string;
+  invite_date: string;
+}
+
+export interface IPassageCreateAccountResponse {
+  username: string;
+  email: string;
+  token: string;
+}
+
+export interface IRequestPasswordRecoveryTokenResponse {
+  message: string;
+  email: string;
+  valid_until: string;
+}
+
+export interface IVerifyPasswordRecoveryTokenResponse {
+  token: string;
+  email: string;
+  is_valid: boolean;
+}
+
+export interface ISetNewPasswordResponse {
+  message: string;
+  email: string;
+  username: string;
+  token: string;
+}
+
+export interface IPassageInvitation {
+  created: string;
+  email: string;
+  expiry: string;
+  invited_by: string;
   organizations: string;
-  sendEmail: boolean;
+  status: string;
+}
+
+export interface IPassageCreateInvitationResponse extends IPassageInvitation {
+  invitation_accept_link: string;
+  invitation_accept_token: string;
+  invitation_token_hash: string;
 }
 
 /**
@@ -51,7 +91,7 @@ class Passage {
     }
 
     const url = `${this.config.endpoint}/invite/${params.token}`;
-    const res = await HttpClientImpl.get(url, {
+    const res = await HttpClientImpl.get<IPassageCheckInviteResponse>(url, {
       timeout: this.config.timeout_ms,
     });
     if (!res.data.is_valid) {
@@ -72,7 +112,7 @@ class Passage {
     helpers.validateOrRaise(params, constraints);
 
     const url = `${this.config.endpoint}/accounts/`;
-    const res = await HttpClientImpl.post(url, {
+    const res = await HttpClientImpl.post<IPassageCreateAccountResponse>(url, {
       timeout: this.config.timeout_ms,
       data: {
         invite_token: params.inviteToken,
@@ -97,7 +137,9 @@ class Passage {
     helpers.validateOrRaise(params, constraints);
 
     const url = `${this.config.endpoint}/recovery/`;
-    const res = await HttpClientImpl.post(url, {
+    const res = await HttpClientImpl.post<
+      IRequestPasswordRecoveryTokenResponse
+    >(url, {
       timeout: this.config.timeout_ms,
       data: {
         email: params.email,
@@ -131,15 +173,18 @@ class Passage {
     }
 
     const url = `${this.config.endpoint}/recovery/${params.token}/`;
-    const res = await HttpClientImpl.post(url, {
-      timeout: this.config.timeout_ms,
-      data: {
-        email: params.email,
-      },
-      headers: {
-        ContentType: 'application/json',
-      },
-    });
+    const res = await HttpClientImpl.post<IVerifyPasswordRecoveryTokenResponse>(
+      url,
+      {
+        timeout: this.config.timeout_ms,
+        data: {
+          email: params.email,
+        },
+        headers: {
+          ContentType: 'application/json',
+        },
+      }
+    );
     if (!res.data.is_valid) {
       throw new Error('InvalidToken');
     }
@@ -166,7 +211,7 @@ class Passage {
     helpers.validateOrRaise(params, constraints);
 
     const url = `${this.config.endpoint}/recovery/${params.token}/password/`;
-    const res = await HttpClientImpl.post(url, {
+    const res = await HttpClientImpl.post<ISetNewPasswordResponse>(url, {
       timeout: this.config.timeout_ms,
       data: {
         email: params.email,
@@ -187,7 +232,7 @@ class Passage {
    */
   public async getInvitations(authToken: string) {
     const url = `${this.config.endpoint}/invites/`;
-    const res = await HttpClientImpl.get(url, {
+    const res = await HttpClientImpl.get<IPassageInvitation[]>(url, {
       timeout: this.config.timeout_ms,
       headers: {
         ContentType: 'application/json',
@@ -207,7 +252,11 @@ class Passage {
    */
   public async createInvitation(
     authToken: string,
-    invitation: IPassageInvitation
+    invitation: {
+      email: string;
+      organizations: string;
+      sendEmail: boolean;
+    }
   ) {
     const constraints = {
       email: { presence: true, email: true },
@@ -217,18 +266,21 @@ class Passage {
     helpers.validateOrRaise(invitation, constraints);
 
     const url = `${this.config.endpoint}/invite/`;
-    const res = await HttpClientImpl.post(url, {
-      timeout: this.config.timeout_ms,
-      data: {
-        email: invitation.email,
-        organizations: invitation.organizations,
-        send_email: invitation.sendEmail,
-      },
-      headers: {
-        ContentType: 'application/json',
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
+    const res = await HttpClientImpl.post<IPassageCreateInvitationResponse>(
+      url,
+      {
+        timeout: this.config.timeout_ms,
+        data: {
+          email: invitation.email,
+          organizations: invitation.organizations,
+          send_email: invitation.sendEmail,
+        },
+        headers: {
+          ContentType: 'application/json',
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
 
     return res.data;
   }
