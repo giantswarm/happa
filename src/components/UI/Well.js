c2bc064b89d329cae26f9c7438250c7af1bec1ec
Gerald Pape <ubergesundheit@users.noreply.github.com>
2020-09-17 15:56:42 +0200

    add cluster not-ready notice to cluster detail (#1821)
    * add cluster not-ready notice to cluster detail
    
    * fix InstallIngressButton tests
    
    * change margin & padding of notice
    
    * display status and disable cluster selection on status in app cluster picker
    
    * fade non clickable clusters in install app modal cluster picker
    
    * use theme disabledOpacity in ClusterPicker
    
    * refactor variable usage
    
    * move additional text in install ingress button to useMemo variable
    
    * change AppDetail available clusterid selection
    
    * proper english
    
    * indicate app installation disabled in cluster details when cluster is not ready
    
    * Update src/components/Cluster/ClusterDetail/ClusterDetailView.js
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>

--- src/components/UI/Well.js
+++ src/components/UI/Well.js
@@ -2,11 +2,11 @@ import styled from '@emotion/styled';
 
 const Well = styled.div`
   background-color: ${({ theme }) => theme.colors.shade7};
-  border-radius: 5px;
+  border-radius: ${({ theme }) => theme.border_radius};
   border: 0px;
   min-height: 20px;
-  padding: 19px;
-  margin-bottom: 20px;
+  padding: ${({ theme }) => theme.spacingPx * 4}px;
+  margin-bottom: ${({ theme }) => theme.spacingPx * 5}px;
 `;
 
 export default Well;
