5970d2e99bb916fd7b09c80a1e816fa2bfa18cdd
axbarsan <mnlk123@yahoo.com>
2020-09-21 22:01:21 +0200

    Rewrite actions in TypeScript

--- src/@types/organizations.d.ts
+++ src/@types/organizations.d.ts
@@ -2,7 +2,35 @@ interface IOrganizationMember {
   email: string;
 }
 
+interface IAzureCredential {
+  credential: {
+    client_id: string;
+    subscription_id: string;
+    tenant_id: string;
+  };
+}
+
+interface IAWSCredential {
+  roles: {
+    admin: string;
+    awsoperator: string;
+  };
+}
+
+interface ICredential {
+  id: string;
+  provider: import('shared/types').PropertiesOf<
+    typeof import('shared/constants').Providers
+  >;
+
+  azure?: IAzureCredential;
+  aws?: IAWSCredential;
+}
+
 interface IOrganization {
   id: string;
+
+  // Injected by client-side.
   members?: IOrganizationMember[] | null;
+  credentials?: ICredential[] | null;
 }
--- src/stores/organization/types.ts
+++ src/stores/organization/types.ts
@@ -1,5 +1,17 @@
+import { ORGANIZATION_SELECT } from 'stores/organization/constants';
+
 export interface IOrganizationState {
   lastUpdated: number;
   isFetching: boolean;
   items: Record<string, IOrganization>;
+}
+
+export interface IOrganizationSelectAction {
+  type: typeof ORGANIZATION_SELECT;
+  orgId: string;
+}
+
+export interface IOrganizationSelectAction {
+  type: typeof ORGANIZATION_SELECT;
+  orgId: string;
 }
