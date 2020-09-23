03fc14e6c24f56f2e0a582a623a6d3d51845c7c8
Adrian Barsan <mnlk123@yahoo.com>
2020-09-23 10:35:02 +0200

    Refactor node pools state management to a domain-specific way (#1847)

--- src/reducers/modalReducer.js
+++ src/reducers/modalReducer.js
@@ -1,5 +1,9 @@
 import * as types from 'actions/actionTypes';
 import produce from 'immer';
+import {
+  NODEPOOL_DELETE,
+  NODEPOOL_DELETE_CONFIRMED_REQUEST,
+} from 'stores/nodepool/constants';
 
 const initialState = { visible: false };
 
@@ -109,10 +113,10 @@ const modalReducer = produce((draft, action) => {
 
       break;
 
-    case types.NODEPOOL_DELETE:
+    case NODEPOOL_DELETE:
       draft.visible = true;
       draft.templateValues = {
-        clusterId: action.clusterId,
+        clusterId: action.clusterID,
         nodePool: action.nodePool,
         loading: false,
       };
@@ -120,10 +124,10 @@ const modalReducer = produce((draft, action) => {
 
       break;
 
-    case types.NODEPOOL_DELETE_CONFIRMED_REQUEST:
+    case NODEPOOL_DELETE_CONFIRMED_REQUEST:
       draft.visible = true;
       draft.templateValues = {
-        clusterId: action.clusterId,
+        clusterId: action.clusterID,
         nodePool: action.nodePool,
         loading: true,
       };
