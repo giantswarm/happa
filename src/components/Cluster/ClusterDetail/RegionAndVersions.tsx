3696a467fba06fae60f358a47671a9eb620fb445
Gerald Pape <gerald@giantswarm.io>
2020-09-28 16:24:14 +0200

    implement src/components/Cluster/ClusterDetail/RegionAndVersions in Typescript

---
+++ src/components/Cluster/ClusterDetail/RegionAndVersions.tsx
@@ -0,0 +1,118 @@
+import styled from '@emotion/styled';
+import ClusterStatus from 'Home/ClusterStatus';
+import { relativeDate } from 'lib/helpers';
+import ReleaseDetailsModal from 'Modals/ReleaseDetailsModal';
+import PropTypes from 'prop-types';
+import React, { FC, RefObject, useRef } from 'react';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import Tooltip from 'react-bootstrap/lib/Tooltip';
+import { Code, Dot } from 'styles';
+import RefreshableLabel from 'UI/RefreshableLabel';
+
+interface IRegionAndVersions {
+  createDate?: string;
+  release?: IRelease;
+  clusterId: string;
+  showUpgradeModal?(): void;
+  region?: string;
+}
+
+const ReleaseDetail = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.white2};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.white1};
+  }
+`;
+
+const showReleaseDetailsModal = (
+  modalRef: RefObject<ReleaseDetailsModal>
+) => () => {
+  const { current: modalElement } = modalRef;
+
+  if (modalElement) modalElement.show();
+};
+
+// Versions data and icons at the top of cluster details view.
+const RegionAndVersions: FC<IRegionAndVersions> = ({
+  createDate,
+  release,
+  clusterId,
+  showUpgradeModal,
+  region,
+}) => {
+  const releaseDetailsModal = useRef<ReleaseDetailsModal | null>(null);
+  const onReleaseDetailClick = showReleaseDetailsModal(releaseDetailsModal);
+
+  return (
+    <>
+      {region && (
+        <OverlayTrigger
+          overlay={<Tooltip id='tooltip'>Region</Tooltip>}
+          placement='top'
+        >
+          <Code>{region}</Code>
+        </OverlayTrigger>
+      )}
+      <div>
+        <span>Created {createDate ? relativeDate(createDate) : 'n/a'}</span>
+        {release && (
+          <>
+            <span>
+              <RefreshableLabel value={release.version}>
+                <>
+                  <Dot style={{ paddingRight: 0 }} />
+                  <ReleaseDetail onClick={onReleaseDetailClick}>
+                    <i className='fa fa-version-tag' />
+                    {release.version}
+                  </ReleaseDetail>
+                </>
+              </RefreshableLabel>
+            </span>
+            <ReleaseDetail onClick={onReleaseDetailClick}>
+              <Dot />
+              <i className='fa fa-kubernetes' />
+              {release.kubernetesVersion}
+            </ReleaseDetail>
+          </>
+        )}
+      </div>
+      {showUpgradeModal && (
+        <ClusterStatus clusterId={clusterId} onClick={showUpgradeModal} />
+      )}
+      {release && (
+        <ReleaseDetailsModal ref={releaseDetailsModal} releases={[release]} />
+      )}
+    </>
+  );
+};
+
+RegionAndVersions.propTypes = {
+  createDate: PropTypes.string,
+  release: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    changelog: PropTypes.arrayOf(
+      PropTypes.shape({
+        component: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    components: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        version: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    kubernetesVersion: PropTypes.string.isRequired,
+    releaseNotesURL: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    version: PropTypes.string.isRequired,
+  }),
+  clusterId: PropTypes.string.isRequired,
+  showUpgradeModal: PropTypes.func,
+  region: PropTypes.string,
+};
+
+export default RegionAndVersions;
