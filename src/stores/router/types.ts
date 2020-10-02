f955452c6521e9e5b70635ed507e435ae4bd7e91
axbarsan <mnlk123@yahoo.com>
2020-10-02 14:13:08 +0200

    Add missing state fields

--- src/stores/router/types.ts
+++ src/stores/router/types.ts
@@ -1,3 +1,6 @@
-import { LocationChangeAction } from 'connected-react-router';
+import { LocationChangeAction, RouterState } from 'connected-react-router';
+import { History } from 'history';
+
+export interface IRouterState extends RouterState<History.LocationState> {}
 
 export type RouterActions = LocationChangeAction;
--- src/stores/state.ts
+++ src/stores/state.ts
@@ -12,13 +12,11 @@ import { IModalState } from 'stores/modal/types';
 import { INodePoolState } from 'stores/nodepool/types';
 import { IOrganizationState } from 'stores/organization/types';
 import { IReleaseState } from 'stores/releases/types';
+import { IRouterState } from 'stores/router/types';
 import { IUserState } from 'stores/user/types';
 
-// Giving state a generic type for now, until whole state is typed.
 export interface IState {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
-
+  router: IRouterState;
   main: IMainState;
   loadingFlags: ILoadingState;
   loadingFlagsByEntity: IEntityLoadingState;
@@ -27,7 +25,6 @@ export interface IState {
   metadata: IMetadataState;
   modal: IModalState;
   entities: {
-    cpAuth?: ICPAuthState;
     catalogs: IAppCatalogsState;
     organizations: IOrganizationState;
     nodePools: INodePoolState;
@@ -35,5 +32,6 @@ export interface IState {
     clusters: IClusterState;
     releases: IReleaseState;
     clusterLabels: IClusterLabelsState;
+    cpAuth?: ICPAuthState;
   };
 }
