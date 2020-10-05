40f4fc2aea9c3740b80a6f14dfb8dc295c9494f8
Marian Steinbach <marian@giantswarm.io>
2020-10-05 17:59:22 +0200

    Extract k8s version into component

---
+++ src/components/UI/KubernetesVersionLabel.tsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+interface IKubernetesVersionLabelProps {
+  version: string;
+}
+
+const KubernetesVersionLabel = ({
+  version,
+}: IKubernetesVersionLabelProps) => {
+  let versionLabel = 'n/a';
+  if (version) {
+    // only show major and minor k8s version
+    const v = version.split('.');
+    versionLabel = [v[0], v[1]].join('.');
+  }
+
+  return (
+    <span>
+      <i className='fa fa-kubernetes' title='Kubernetes version' />{' '}
+      {versionLabel}
+    </span>
+  );
+};
+
+KubernetesVersionLabel.propTypes = {
+  version: PropTypes.string,
+};
+
+KubernetesVersionLabel.defaultProps = {
+  version: '',
+};
+
+export default KubernetesVersionLabel;
