c31bc28d31622eb581378904bdcc4a7731a53c80
axbarsan <mnlk123@yahoo.com>
2020-10-07 12:51:57 +0200

    Create utility function for extracting error messages out of errors

---
+++ src/utils/__tests__/errorUtils.ts
@@ -0,0 +1,68 @@
+import { extractMessageFromError } from '../errorUtils';
+
+describe('errorUtils', () => {
+  describe('extractMessageFromError', () => {
+    it('extracts error message from superagent errors', () => {
+      expect(
+        extractMessageFromError(
+          {
+            response: {
+              body: { message: 'Something is really wrong this time' },
+            },
+          },
+          'An error'
+        )
+      ).toBe('Something is really wrong this time');
+
+      expect(
+        extractMessageFromError(
+          {
+            response: {},
+          },
+          'An error'
+        )
+      ).toBe('An error');
+
+      expect(extractMessageFromError({}, 'An error')).toBe('An error');
+    });
+
+    it('extracts error message from axios errors', () => {
+      expect(
+        extractMessageFromError(
+          {
+            response: {
+              data: { message: 'Something is really wrong this time' },
+            },
+          },
+          'An error'
+        )
+      ).toBe('Something is really wrong this time');
+
+      expect(
+        extractMessageFromError(
+          {
+            response: {},
+          },
+          'An error'
+        )
+      ).toBe('An error');
+
+      expect(extractMessageFromError({}, 'An error')).toBe('An error');
+    });
+
+    it('extracts error message from javascript errors', () => {
+      expect(
+        extractMessageFromError(
+          new Error('Something is really wrong this time'),
+          'An error'
+        )
+      ).toBe('Something is really wrong this time');
+
+      expect(extractMessageFromError(new Error(), 'An error')).toBe('An error');
+
+      expect(extractMessageFromError(new Error(''), 'An error')).toBe(
+        'An error'
+      );
+    });
+  });
+});
