6a6aa00c0f0addd7beb368c7d4bd41d1a96ecfd7
axbarsan <mnlk123@yahoo.com>
2020-10-21 13:41:27 +0200

    Apply wording suggestions from PR review

--- src/components/Cluster/ClusterDetail/AddNodePool/AddNodePoolAZSelection.tsx
+++ src/components/Cluster/ClusterDetail/AddNodePool/AddNodePoolAZSelection.tsx
@@ -215,9 +215,9 @@ const AddNodePoolAZSelection: React.FC<IAddNodePoolAZSelectionProps> = ({
           <StyledPanelCollapse>
             <p>
               By not specifying an availability zone, Azure will select a zone
-              by itself, where the requested VM size has the best availability.
-              This is especially useful for GPU instances, which are not
-              available in all regions.
+              by itself, where the requested virtual machine size has the best
+              availability. This is especially useful for virtual machine sizes
+              with GPU, which are not available in all availability zones.
             </p>
           </StyledPanelCollapse>
         </StyledPanel>
