5bf00982c2d426f347e7b06f99de4c23ca37fb21
Oliver Ponder <oliverponder@giantswarm.io>
2020-12-18 21:42:23 +0800

    Convert to typescript file.

---
+++ src/components/Apps/Apps.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Breadcrumb } from 'react-breadcrumbs';
+import { Route, Switch } from 'react-router-dom';
+import { AppsRoutes } from 'shared/constants/routes';
+
+const Apps: React.FC = () => {
+  // eslint-disable-next-line class-methods-use-this
+  return (
+    <Breadcrumb
+      data={{
+        title: 'Apps'.toUpperCase(),
+        pathname: AppsRoutes.Home,
+      }}
+    >
+      <Switch>
+        <Route
+          exact
+          path={AppsRoutes.AppDetail}
+          render={() => <h1 data-testid='app-detail'>App Detail</h1>}
+        />
+        <Route
+          path={AppsRoutes.Home}
+          render={() => <h1 data-testid='apps-browser'>Apps</h1>}
+        />
+      </Switch>
+    </Breadcrumb>
+  );
+};
+
+export default Apps;
