c785342dd615effd279fd47eb7a70bcdaa0dc8b0
Oliver Ponder <oliverponder@giantswarm.io>
2021-01-04 19:40:51 +0800

    Add routes for /apps and render a placeholder (#2008)
    * Add routes for /apps and render a placeholder
    
    * Fix incorrect reference to AppsRoutes in MainMenu
    
    * Fix broken tests.
    
    * Convert to typescript file.
    
    * Remove eslint comment thats no longer needed.
    
    * trigger ci?

---
+++ src/components/Apps/Apps.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Breadcrumb } from 'react-breadcrumbs';
+import { Route, Switch } from 'react-router-dom';
+import { AppsRoutes } from 'shared/constants/routes';
+
+const Apps: React.FC = () => {
+  return (
+    <Breadcrumb
+      data={{
+        title: 'Apps'.toUpperCase(),
+        pathname: AppsRoutes.Home,
+      }}
+    >
+      <Switch>
+        <Route
+          exact
+          path={AppsRoutes.AppDetail}
+          render={() => <h1 data-testid='app-detail'>App Detail</h1>}
+        />
+        <Route
+          path={AppsRoutes.Home}
+          render={() => <h1 data-testid='apps-browser'>Apps</h1>}
+        />
+      </Switch>
+    </Breadcrumb>
+  );
+};
+
+export default Apps;
