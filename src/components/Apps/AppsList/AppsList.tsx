166063966f4e572ce61893e8bfcf5e49960a7bb0
Oliver Ponder <oliverponder@giantswarm.io>
2021-01-15 22:50:51 +0800

    Add new app component and app listing area (#2041)
    * Add an  app and app list component.
    
    * Adjust color of separator
    
    * Add dynamic icon generation with outlined colorful app names
    
    * Use styled-components
    
    * Fix broken image icon flicker before falling back.

---
+++ src/components/Apps/AppsList/AppsList.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import App from 'UI/AppList/App';
+
+const ListAndFacets = styled.div`
+  display: flex;
+`;
+
+const Facets = styled.div`
+  width: 230px;
+  flex-shrink: 0;
+`;
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  gap: 20px;
+  border-left: 1px solid ${({ theme }) => theme.colors.darkBlueLighter1};
+  width: 100%;
+  padding-left: 20px;
+`;
+
+const AppsList: React.FC = () => {
+  return (
+    <div>
+      <h1 data-testid='apps-browser'>Apps</h1>
+      <p>
+        Managed apps for use in your clusters. Learn more in our{' '}
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://docs.giantswarm.io/basics/app-platform/'
+        >
+          app platform documentation
+          <i className='fa fa-open-in-new' />
+        </a>
+      </p>
+      <hr />
+
+      <ListAndFacets>
+        <Facets />
+        <List>
+          {[
+            'g8s-prometheus',
+            'apm-server',
+            'elastabot',
+            'elastic-stack',
+            'elasticsearch-curator',
+            'fluentd',
+            'prometheus-mongodb-exporter',
+            'prometheus-pushgateway',
+            'prometheus-postgres-exporter',
+            ...Array(100).fill('other app'),
+          ].map((name) => (
+            <App
+              key={name}
+              name={name}
+              catalog='Giant Swarm Control Plane'
+              to='https://google.com'
+            />
+          ))}
+        </List>
+      </ListAndFacets>
+    </div>
+  );
+};
+
+export default AppsList;
