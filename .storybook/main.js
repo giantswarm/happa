bda9a30a0498f51be212b68860d0650a5adc263b
Adrian Barsan <mnlk123@yahoo.com>
2021-01-22 17:52:35 +0100

    Remove unused argument from storybook method

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, { _ }) => {
     config.resolve.modules = [
       ...(config.resolve.modules || []),
       'node_modules',
