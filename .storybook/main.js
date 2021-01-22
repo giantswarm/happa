de7e7ea6c93cf9abb17c5c8ba884ce7b19a64f61
Adrian Barsan <mnlk123@yahoo.com>
2021-01-22 15:49:43 +0100

    Include all base styles in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 
-  webpackFinal: async (config, { _ }) => {
+  webpackFinal: async (config, { configType }) => {
     config.resolve.modules = [
       ...(config.resolve.modules || []),
       'node_modules',
       path.resolve(`${__dirname}/../src`),
       path.resolve(`${__dirname}/../src/components`),
     ];
+
+    config.module.rules.push({
+      test: /\.sass$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        'sass-loader', // if scss then add 'sass-loader'
+      ],
+    });
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name]-[contenthash].css',
+        chunkFilename: '[id]-[contenthash].css',
+      })
+    );
 
     return config;
   },
