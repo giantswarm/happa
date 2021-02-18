8c6818dab51287f8b39da2f70f2cf68d09b7a2e8
Marian Steinbach <marian@giantswarm.io>
2021-02-18 21:51:43 +0100

    Add stories

---
+++ src/components/UI/Display/Cluster/ComponentChangelog/stories/ComponentChangelog.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React, { ComponentPropsWithoutRef } from 'react';
+
+import ComponentChangelog from '..';
+
+const Template: Story<ComponentPropsWithoutRef<typeof ComponentChangelog>> = (
+  args
+) => <ComponentChangelog {...args} />;
+
+export const Simple = Template.bind({});
+Simple.args = {
+  name: 'mycomponent',
+  changes: ['Fix update process node termination in http://example.com/.'],
+};
+
+export const MultiParagraph = Template.bind({});
+MultiParagraph.args = {
+  name: 'kubernetes',
+  changes: [
+    `Here is a long component change description that shows us how a changelog item consisting of several paragraphs would be rendered.
+
+  Here is the second and last line of the markdown text.`,
+    'This is a second change item.',
+  ],
+};
+
+export const Complex = Template.bind({});
+Complex.args = {
+  name: 'kubernetes',
+  changes: [
+    'Mount `/var/log` directory in an EBS volume.',
+    'Use proper hostname annotation for nodes.',
+    'Update to 1.13.4 ([CVE-2019-1002100](https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-1002100)).',
+  ],
+};
+
+export default {
+  title: 'Display/Releases/ComponentChangelog',
+  component: ComponentChangelog,
+} as Meta;
