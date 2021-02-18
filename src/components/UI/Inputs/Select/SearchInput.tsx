56f9485700cc5f2dc06f17c295f3f9bc1242496f
Adrian Barsan <mnlk123@yahoo.com>
2021-02-18 16:59:07 +0100

    Pass ref to the Select SearchInput component

--- src/components/UI/Inputs/Select/SearchInput.tsx
+++ src/components/UI/Inputs/Select/SearchInput.tsx
@@ -4,19 +4,23 @@ import * as React from 'react';
 import TextInput from '../TextInput';
 
 interface ISearchInputProps
-  extends React.ComponentPropsWithoutRef<typeof TextInput> {}
+  extends React.ComponentPropsWithRef<typeof TextInput> {}
 
-const SearchInput: React.FC<ISearchInputProps> = (props) => {
+const SearchInput: React.FC<ISearchInputProps> = React.forwardRef<
+  HTMLInputElement,
+  ISearchInputProps
+>((props, ref) => {
   return (
     <Box pad={{ horizontal: 'xsmall', vertical: 'small' }} background='border'>
       <TextInput
-        {...props}
         icon={<i className='fa fa-search' />}
         margin='none'
         size='small'
+        {...props}
+        ref={ref}
       />
     </Box>
   );
-};
+});
 
 export default SearchInput;
