a493b3e02dd9ff1348e89a0af52f5e20def24f77
Marian Steinbach <marian@giantswarm.io>
2021-02-18 18:19:36 +0100

    Add storybook stories for VersionPicker

---
+++ src/components/UI/Controls/VersionPicker/stories/Simple.tsx
@@ -0,0 +1,126 @@
+import { Story } from '@storybook/react/types-6-0';
+import React, { ComponentPropsWithoutRef } from 'react';
+
+import VersionPicker from '../VersionPicker';
+
+export function onChange() {
+  // eslint-disable-next-line no-alert
+  alert('Changed');
+}
+
+export const Simple: Story<ComponentPropsWithoutRef<typeof VersionPicker>> = (
+  args
+) => {
+  return (
+    <VersionPicker
+      onChange={onChange}
+      selectedVersion={args.selectedVersion}
+      versions={[
+        {
+          created: '2020-12-15T16:39:27Z',
+          includesVersion: '2.0.0',
+          chartVersion: '1.0.5',
+          test: false,
+        },
+        {
+          created: '2020-12-14T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '1.0.3',
+          test: false,
+        },
+        {
+          created: '2020-12-13T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '1.0.2',
+          test: false,
+        },
+        {
+          created: '2020-12-12T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '0.8.9',
+          test: false,
+        },
+        {
+          created: '2020-12-11T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '0.8.8',
+          test: false,
+        },
+        {
+          created: '2020-12-10T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '0.8.7',
+          test: false,
+        },
+        {
+          created: '2020-11-10T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '0.8.6',
+          test: false,
+        },
+        {
+          created: '2020-10-10T16:39:27Z',
+          includesVersion: '1.0.0',
+          chartVersion: '0.8.5',
+          test: false,
+        },
+        {
+          created: '2020-09-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.8.4',
+          test: false,
+        },
+        {
+          created: '2020-08-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.8.3',
+          test: false,
+        },
+        {
+          created: '2020-07-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.8.2',
+          test: false,
+        },
+        {
+          created: '2020-06-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.8.1',
+          test: false,
+        },
+        {
+          created: '2020-05-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.8.0',
+          test: false,
+        },
+        {
+          created: '2020-04-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.7.10',
+          test: false,
+        },
+        {
+          created: '2020-03-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.7.8',
+          test: false,
+        },
+        {
+          created: '2020-02-10T16:39:27Z',
+          includesVersion: '0.0.0',
+          chartVersion: '0.7.3',
+          test: false,
+        },
+      ]}
+    />
+  );
+};
+
+Simple.args = {
+  selectedVersion: '1.0.2',
+};
+
+Simple.argTypes = {
+  selectedVersion: { control: { type: 'text' } },
+};
