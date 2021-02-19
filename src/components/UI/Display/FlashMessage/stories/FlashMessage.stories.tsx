59a12a722697ff009b6546934cdeeba19334629d
Marian Steinbach <marian@giantswarm.io>
2021-02-19 17:58:42 +0100

    Add FlashMessage to storybook  (#2182)
    * Move module into subfolder
    
    * Add stories for FlashMessage

---
+++ src/components/UI/Display/FlashMessage/stories/FlashMessage.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React, { ComponentPropsWithoutRef } from 'react';
+import { FlashMessageType } from 'styles';
+
+import FlashMessage from '..';
+
+const Template: Story<ComponentPropsWithoutRef<typeof FlashMessage>> = (
+  args
+) => <FlashMessage {...args}>Here is some text</FlashMessage>;
+
+export const Success = Template.bind({});
+Success.args = {
+  type: FlashMessageType.Success,
+  className: '',
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  type: FlashMessageType.Info,
+  className: '',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  type: FlashMessageType.Warning,
+  className: '',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  type: FlashMessageType.Danger,
+  className: '',
+};
+
+export default {
+  title: 'Display/FlashMessage',
+  component: FlashMessage,
+} as Meta;
