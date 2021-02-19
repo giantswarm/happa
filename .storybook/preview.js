751420c6a5893c8128a989026dd67d9a0edb6ef3
Adrian Barsan <mnlk123@yahoo.com>
2021-02-19 17:47:33 +0100

    Use the new DateInput inside the keypair expiry selection modal

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,7 +7,6 @@ import 'babel-polyfill';
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'noty/lib/noty.css';
-import 'react-datepicker/dist/react-datepicker.css';
 import 'styles/app.sass';
 
 export const parameters = {
