f52ebb44236dc8f345c1bea827490e693c89ffa3
Adrian Barsan <mnlk123@yahoo.com>
2021-02-23 18:35:12 +0100

    Add simple snapshot test

---
+++ src/components/UI/Inputs/NumberPicker/__tests__/index.tsx
@@ -0,0 +1,15 @@
+import { renderWithTheme } from 'testUtils/renderUtils';
+
+import NumberPicker from '..';
+
+describe('NumberPicker', () => {
+  it('renders a simple input', () => {
+    const { container } = renderWithTheme(NumberPicker, {
+      value: 3,
+      label: 'Count your money',
+      info: 'Aha! Did not know that',
+      help: 'A very helpful message',
+    });
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
