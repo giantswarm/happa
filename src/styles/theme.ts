6b4e6f8495e97e55bba39db86dd9aedec5eeaefd
Adrian Barsan <mnlk123@yahoo.com>
2021-02-26 12:50:59 +0100

    Add table styling, storybook example and snapshot test

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -726,6 +726,26 @@ const theme = deepMerge(generate(16), {
       small: '16px',
     },
   },
+  table: {
+    header: {
+      extend: () => css`
+        text-transform: uppercase;
+        font-size: ${(props) => props.theme.text.xsmall.size};
+        color: ${(props) => props.theme.global.colors['text-weak'].dark};
+      `,
+    },
+    body: {
+      pad: 'small',
+    },
+    row: {},
+    extend: () => css`
+      caption {
+        font-size: ${(props) => props.theme.text.large.size};
+        color: ${(props) => props.theme.global.colors.text.dark};
+        font-weight: bold;
+      }
+    `,
+  },
 });
 
 /* eslint-enable no-magic-numbers */
