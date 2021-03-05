469db149f558e94306790044abbd86c08d3136a4
Adrian Barsan <mnlk123@yahoo.com>
2021-03-05 17:21:53 +0100

    Write tests for the new user localstorage functionality

--- src/utils/__tests__/localStorageUtils.ts
+++ src/utils/__tests__/localStorageUtils.ts
@@ -2,6 +2,10 @@ import { LoggedInUserTypes } from 'stores/main/types';
 import { fetchUserFromStorage } from 'utils/localStorageUtils';
 
 describe('localStorageUtils', () => {
+  afterEach(() => {
+    localStorage.removeItem('user');
+  });
+
   describe('fetchUserFromStorage', () => {
     it('retrieves the user from storage', () => {
       const user: ILoggedInUser = {
@@ -16,14 +20,10 @@ describe('localStorageUtils', () => {
       localStorage.setItem('user', JSON.stringify(user));
 
       expect(fetchUserFromStorage()).toStrictEqual(user);
-      localStorage.removeItem('user');
     });
 
     it(`returns 'null' if the user cannot be parsed`, () => {
       localStorage.setItem('user', 'I want to be JSON when I grow up');
-      expect(fetchUserFromStorage()).toBeNull();
-
-      localStorage.removeItem('user');
       expect(fetchUserFromStorage()).toBeNull();
     });
 
@@ -50,7 +50,45 @@ describe('localStorageUtils', () => {
         authToken: '123',
         type: LoggedInUserTypes.GS,
       });
-      localStorage.removeItem('user');
+    });
+
+    it('migrates user logged in before introducing token types', () => {
+      const user: Partial<ILoggedInUser> = {
+        auth: {
+          scheme: 'giantswarm',
+          token: '',
+        },
+        email: '',
+        isAdmin: false,
+        authToken: '123',
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+
+      expect(fetchUserFromStorage()).toStrictEqual({
+        auth: {
+          scheme: 'giantswarm',
+          token: '123',
+        },
+        email: '',
+        isAdmin: false,
+        authToken: '123',
+        type: LoggedInUserTypes.GS,
+      });
+    });
+
+    it('does not deserialize user logged in via Auth0', () => {
+      const user: Partial<ILoggedInUser> = {
+        auth: {
+          scheme: 'Bearer',
+          token: '',
+        },
+        email: '',
+        isAdmin: false,
+        authToken: '123',
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+
+      expect(fetchUserFromStorage()).toBeNull();
     });
   });
 });
