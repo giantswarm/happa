ed502d798cc9a859fd83f3c594668ed18b6be31f
Marian Steinbach <marian@giantswarm.io>
2021-03-12 11:17:50 +0100

    Add storybook on colors (#2260)
    * Remove unused assets
    
    * Remove unused imports
    
    * Add color stories
    
    * Remove blank space
    
    * Remove empty line
    
    * Remove unused color darkBlueMuted
    
    * Remove unused color whiteInput
    
    * Remove unused color red
    
    * Adding colors
    
    * Fix validation problem

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -40,8 +40,6 @@ const theme = deepMerge(generate(16), {
     darkBlueLighter8: '#d0d8dd',
     darkBlueLighter9: '#e7ebee',
 
-    darkBlueMuted: '#020a29',
-
     shade1: '#1E4156',
     shade2: '#224A61',
     shade3: '#234D65',
@@ -59,11 +57,9 @@ const theme = deepMerge(generate(16), {
     white4: '#eee',
     white5: '#ccd',
 
-    whiteInput: '#f0f0f0',
     yellow1: '#ddb03a',
     gold: '#ce990f',
     goldBackground: '#a97904',
-    red: 'red',
     gray: '#ccc',
     error: '#e49090',
 
