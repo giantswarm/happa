df4c7aac8f47aa62e982e2ed2f7bba6677a32e61
Oliver Ponder <oliver.ponder@gmail.com>
2021-03-26 20:25:26 +0800

    Fix the tetsts

--- testUtils/modelMocks.js
+++ testUtils/modelMocks.js
@@ -1,2 +1,6 @@
 jest.mock('model/services/giantSwarm/info');
 jest.mock('model/services/metadata/configuration');
+
+jest.mock('model/services/mapi/authorizationv1/selfSubjectAccessReview');
+jest.mock('model/services/mapi/authorizationv1/selfSubjectRulesReview');
+jest.mock('model/services/mapi/organizations/getOrganizationList');
