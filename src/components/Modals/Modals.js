e45f74d85ce8b79b3f1231a209a50b06133ad47c
Marian Steinbach <marian@giantswarm.io>
2021-03-29 17:51:16 +0200

    Update the validation regex for organization names

--- src/components/Modals/Modals.js
+++ src/components/Modals/Modals.js
@@ -19,7 +19,7 @@ import ClusterIDLabel from 'UI/Display/Cluster/ClusterIDLabel';
 import TextInput from 'UI/Inputs/TextInput';
 
 const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const organizationNameRegexp = /^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$/;
+const organizationNameRegexp = /^(([a-z][-a-z0-9]*)?[a-z0-9])?$/;
 
 const NodePoolTextDiv = styled.div`
   strong {
