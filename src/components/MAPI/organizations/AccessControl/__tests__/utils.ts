75aaebcba9c737fb5dcf9a5b22e23b028b8a34af
Adrian Barsan <mnlk123@yahoo.com>
2021-03-29 16:36:04 +0200

    Add all files necessary for rendering the subjects tab of the Access Control UI

---
+++ src/components/MAPI/organizations/AccessControl/__tests__/utils.ts
@@ -0,0 +1,41 @@
+import { getUserNameParts, parseSubjects } from '../utils';
+
+describe('AccessControl/utils', () => {
+  describe('parseSubjects', () => {
+    test.each`
+      subjects                          | expected
+      ${''}                             | ${[]}
+      ${'subject1'}                     | ${['subject1']}
+      ${'subject1 subject2'}            | ${['subject1', 'subject2']}
+      ${'subject1,subject2'}            | ${['subject1', 'subject2']}
+      ${'subject1, subject2'}           | ${['subject1', 'subject2']}
+      ${'subject1 ,subject2'}           | ${['subject1', 'subject2']}
+      ${'subject1 , subject2'}          | ${['subject1', 'subject2']}
+      ${'subject1 ,subject2, subject3'} | ${['subject1', 'subject2', 'subject3']}
+      ${'subject1;subject2'}            | ${['subject1', 'subject2']}
+      ${'subject1; subject2'}           | ${['subject1', 'subject2']}
+      ${'subject1 ;subject2'}           | ${['subject1', 'subject2']}
+      ${'subject1 ; subject2'}          | ${['subject1', 'subject2']}
+      ${'subject1 ;subject2; subject3'} | ${['subject1', 'subject2', 'subject3']}
+      ${'subject1	subject2; subject3'}   | ${['subject1', 'subject2', 'subject3']}
+      ${'subject1			subject2 subject3'}    | ${['subject1', 'subject2', 'subject3']}
+      ${'subject1			subject2 subject3						'}    | ${['subject1', 'subject2', 'subject3']}
+    `(`gets subjects from '$subjects'`, ({ subjects, expected }) => {
+      const parsed = parseSubjects(subjects);
+      expect(parsed).toStrictEqual(expected);
+    });
+  });
+
+  describe('getUserNameParts', () => {
+    test.each`
+      userName                             | expected
+      ${''}                                | ${['']}
+      ${'test'}                            | ${['test']}
+      ${'test@test.com'}                   | ${['test', 'test.com']}
+      ${'test@other-test@some-other-test'} | ${['test', 'other-test']}
+    `(`gets userName parts from '$userName'`, ({ userName, expected }) => {
+      const parts = getUserNameParts(userName);
+      expect(parts).toStrictEqual(expected);
+    });
+  });
+});
