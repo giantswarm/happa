66abe6c73d351e2de12ab8bf104aaa07dee56e53
Adrian Barsan <mnlk123@yahoo.com>
2021-03-29 10:10:02 +0200

    access control: add components needed for displaying the role list sidebar (#2298)

---
+++ src/components/UI/Display/MAPI/AccessControl/AccessControlRoleListItem.tsx
@@ -0,0 +1,130 @@
+import { Box, Card, CardBody, CardHeader, Heading, Text } from 'grommet';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import styled from 'styled-components';
+
+import {
+  IAccessControlRoleItem,
+  IAccessControlRoleItemPermission,
+} from './types';
+
+function formatResourceCounter(
+  permissions: IAccessControlRoleItemPermission[]
+): string {
+  let totalCount = 0;
+  for (const perm of permissions) {
+    for (const group of perm.apiGroups) {
+      if (group === '*') return 'All';
+    }
+    for (const resource of perm.resources) {
+      if (resource === '*') return 'All';
+    }
+
+    totalCount += perm.resourceNames.length + perm.resources.length;
+  }
+
+  return formatCounter(totalCount);
+}
+
+function formatCounter(fromValue: number): string {
+  switch (true) {
+    case fromValue === 0:
+      return 'None';
+    // eslint-disable-next-line no-magic-numbers
+    case fromValue > 99:
+      return '99+';
+    default:
+      return fromValue.toString();
+  }
+}
+
+const StyledCard = styled(Card)`
+  min-height: auto;
+  box-shadow: ${(props) =>
+    props['aria-selected']
+      ? `0 0 0 1px ${props.theme.global.colors.text.dark}`
+      : 'none'};
+  outline: ${(props) => props['aria-selected'] && 'none'};
+  transition: opacity 0.1s ease-out, box-shadow 0.1s ease-in-out;
+
+  :hover,
+  :focus {
+    box-shadow: ${(props) =>
+      `0 0 0 1px ${props.theme.global.colors.text.dark}`};
+  }
+
+  :active {
+    opacity: 0.7;
+  }
+`;
+
+interface IAccessControlRoleListItemProps
+  extends IAccessControlRoleItem,
+    React.ComponentPropsWithoutRef<typeof Card> {
+  active?: boolean;
+}
+
+const AccessControlRoleListItem = React.forwardRef<
+  HTMLDivElement,
+  IAccessControlRoleListItemProps
+>(({ name, namespace, active, permissions, groups, users, ...props }, ref) => {
+  const resourceCount = formatResourceCounter(permissions);
+  const groupCount = formatCounter(Object.values(groups).length);
+  const userCount = formatCounter(Object.values(users).length);
+
+  return (
+    <StyledCard
+      pad={{ vertical: 'small', horizontal: 'medium' }}
+      round='xsmall'
+      elevation='none'
+      overflow='visible'
+      background='background-front'
+      aria-selected={active}
+      role='button'
+      tabIndex={active ? -1 : 0}
+      {...props}
+      ref={ref}
+    >
+      <CardHeader>
+        <Heading level={5} margin='none'>
+          <Box direction='row' align='center'>
+            <Text truncate={true}>{name}</Text>
+
+            {namespace.length < 1 && (
+              <Text margin={{ left: 'xxsmall' }}>
+                <i className='fa fa-globe' aria-label='Cluster role' />
+              </Text>
+            )}
+          </Box>
+        </Heading>
+      </CardHeader>
+      <CardBody margin={{ top: 'xsmall' }}>
+        <Box justify='between' direction='row'>
+          <Box width='120px'>
+            <Text color='text-weak'>Resources: {resourceCount}</Text>
+          </Box>
+          <Box width='100px'>
+            <Text color='text-weak'>Groups: {groupCount}</Text>
+          </Box>
+          <Box width='100px'>
+            <Text color='text-weak'>Users: {userCount}</Text>
+          </Box>
+        </Box>
+      </CardBody>
+    </StyledCard>
+  );
+});
+
+AccessControlRoleListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  namespace: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  // @ts-expect-error
+  permissions: PropTypes.arrayOf(PropTypes.object.isRequired),
+  // @ts-expect-error
+  groups: PropTypes.object.isRequired,
+  // @ts-expect-error
+  users: PropTypes.object.isRequired,
+};
+
+export default AccessControlRoleListItem;
---
+++ src/components/UI/Display/MAPI/AccessControl/__tests__/AccessControlRoleList.tsx
@@ -0,0 +1,330 @@
+import {
+  fireEvent,
+  screen,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react';
+import { getComponentWithTheme, renderWithTheme } from 'testUtils/renderUtils';
+
+import AccessControlRoleList from '../AccessControlRoleList';
+import { IAccessControlRoleSubjectItem } from '../types';
+
+function makeSubjects(
+  count: number,
+  prefix: string = 'subject'
+): Record<string, IAccessControlRoleSubjectItem> {
+  const subjects: Record<string, IAccessControlRoleSubjectItem> = {};
+
+  for (let i = 0; i < count; i++) {
+    const name = `${prefix}-${i}`;
+    subjects[name] = {
+      name,
+      isEditable: true,
+      roleBindings: [],
+    };
+  }
+
+  return subjects;
+}
+
+describe('AccessControlRoleList', () => {
+  it('renders without crashing', () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: [],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+  });
+
+  it('renders various roles correctly', () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: [
+        {
+          name: 'some-role',
+          managedBy: '',
+          namespace: '',
+          groups: {},
+          users: {},
+          serviceAccounts: {},
+          permissions: [],
+        },
+        {
+          name: 'some-other-role',
+          managedBy: '',
+          namespace: 'test-namespace',
+          groups: {
+            'group-1': {
+              name: 'group-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          users: {
+            'user-1': {
+              name: 'user-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          serviceAccounts: {},
+          permissions: [
+            {
+              apiGroups: ['*'],
+              resources: ['*'],
+              resourceNames: [],
+              verbs: ['*'],
+            },
+          ],
+        },
+        {
+          name: 'some-cool-role',
+          managedBy: '',
+          namespace: 'test-namespace',
+          groups: {},
+          // eslint-disable-next-line no-magic-numbers
+          users: makeSubjects(150),
+          serviceAccounts: {},
+          permissions: [
+            {
+              apiGroups: ['*'],
+              resources: ['*'],
+              resourceNames: [],
+              verbs: ['*'],
+            },
+          ],
+        },
+      ],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    let role = screen
+      .getByText('some-role')
+      .closest('[role=button]') as HTMLElement;
+    expect(within(role).getByLabelText('Cluster role')).toBeInTheDocument();
+    expect(within(role).getByText('Resources: None')).toBeInTheDocument();
+    expect(within(role).getByText('Groups: None')).toBeInTheDocument();
+    expect(within(role).getByText('Users: None')).toBeInTheDocument();
+
+    role = screen
+      .getByText('some-other-role')
+      .closest('[role=button]') as HTMLElement;
+    expect(
+      within(role).queryByLabelText('Cluster role')
+    ).not.toBeInTheDocument();
+    expect(within(role).getByText('Resources: All')).toBeInTheDocument();
+    expect(within(role).getByText('Groups: 1')).toBeInTheDocument();
+    expect(within(role).getByText('Users: 1')).toBeInTheDocument();
+
+    role = screen
+      .getByText('some-cool-role')
+      .closest('[role=button]') as HTMLElement;
+    expect(
+      within(role).queryByLabelText('Cluster role')
+    ).not.toBeInTheDocument();
+    expect(within(role).getByText('Resources: All')).toBeInTheDocument();
+    expect(within(role).getByText('Groups: None')).toBeInTheDocument();
+    expect(within(role).getByText('Users: 99+')).toBeInTheDocument();
+  });
+
+  it('renders a loading placeholder if the data is not present', () => {
+    const { rerender } = renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: undefined,
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+    expect(screen.getAllByTitle('Loading...').length).toBeGreaterThan(0);
+
+    rerender(
+      getComponentWithTheme(AccessControlRoleList, {
+        activeRoleName: '',
+        setActiveRoleName: jest.fn(),
+        roles: [],
+      })
+    );
+
+    expect(screen.queryByTitle('Loading...')).not.toBeInTheDocument();
+  });
+
+  it('renders a placeholder if the data is present, but empty', () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: [],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    expect(screen.getByText('No roles available')).toBeInTheDocument();
+  });
+
+  it('can select a specific active role', () => {
+    const setActiveRoleNameMockFn = jest.fn();
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: setActiveRoleNameMockFn,
+      roles: [
+        {
+          name: 'some-role',
+          managedBy: '',
+          namespace: '',
+          groups: {},
+          users: {},
+          serviceAccounts: {},
+          permissions: [],
+        },
+        {
+          name: 'some-other-role',
+          managedBy: '',
+          namespace: 'test-namespace',
+          groups: {
+            'group-1': {
+              name: 'group-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          users: {
+            'user-1': {
+              name: 'user-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          serviceAccounts: {},
+          permissions: [
+            {
+              apiGroups: ['some-group.domain.com'],
+              resources: ['*'],
+              resourceNames: [],
+              verbs: ['*'],
+            },
+          ],
+        },
+      ],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    fireEvent.click(screen.getByText('some-role').closest('[role=button]')!);
+
+    expect(setActiveRoleNameMockFn).toHaveBeenCalledWith('some-role');
+  });
+
+  it('can search for a specific role', async () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: [
+        {
+          name: 'some-role',
+          managedBy: '',
+          namespace: '',
+          groups: {},
+          users: {},
+          serviceAccounts: {},
+          permissions: [],
+        },
+        {
+          name: 'some-other-role',
+          managedBy: '',
+          namespace: 'test-namespace',
+          groups: {
+            'group-1': {
+              name: 'group-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          users: {
+            'user-1': {
+              name: 'user-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          serviceAccounts: {},
+          permissions: [
+            {
+              apiGroups: ['*'],
+              resources: ['*'],
+              resourceNames: [],
+              verbs: ['*'],
+            },
+          ],
+        },
+      ],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    const searchInput = screen.getByPlaceholderText('Find role');
+    fireEvent.change(searchInput, { target: { value: 'some-role' } });
+
+    expect(screen.getByText('some-role')).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.getByText('some-other-role'));
+  });
+
+  it('displays a placeholder if searching for something returned no values', async () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: [
+        {
+          name: 'some-role',
+          managedBy: '',
+          namespace: '',
+          groups: {},
+          users: {},
+          serviceAccounts: {},
+          permissions: [],
+        },
+        {
+          name: 'some-other-role',
+          managedBy: '',
+          namespace: 'test-namespace',
+          groups: {
+            'group-1': {
+              name: 'group-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          users: {
+            'user-1': {
+              name: 'user-1',
+              isEditable: true,
+              roleBindings: [],
+            },
+          },
+          serviceAccounts: {},
+          permissions: [
+            {
+              apiGroups: ['*'],
+              resources: ['*'],
+              resourceNames: [],
+              verbs: ['*'],
+            },
+          ],
+        },
+      ],
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    const searchInput = screen.getByPlaceholderText('Find role');
+    fireEvent.change(searchInput, { target: { value: 'randomjajaja' } });
+
+    await waitForElementToBeRemoved(() => screen.getByText('some-role'));
+    expect(screen.queryByText('some-other-role')).not.toBeInTheDocument();
+
+    expect(
+      screen.getByText('No roles found for your search')
+    ).toBeInTheDocument();
+  });
+
+  it('displays a provided error', () => {
+    renderWithTheme(AccessControlRoleList, {
+      activeRoleName: '',
+      setActiveRoleName: jest.fn(),
+      roles: undefined,
+      errorMessage: 'Oh no! There was an error.',
+    } as React.ComponentPropsWithoutRef<typeof AccessControlRoleList>);
+
+    expect(
+      screen.getByText('Error loading roles: Oh no! There was an error.')
+    ).toBeInTheDocument();
+  });
+});
