fb32f29076d41fd21b149067c8c3cc393c7eb4c7
Adrian Barsan <mnlk123@yahoo.com>
2021-04-01 13:14:55 +0200

    Refactor cluster lister to a way that doesn't let the token expire

--- src/model/services/mapi/capiv1alpha3/getClusterList.ts
+++ src/model/services/mapi/capiv1alpha3/getClusterList.ts
@@ -1,32 +1,45 @@
+import { IOAuth2Provider } from 'lib/OAuth2/OAuth2';
 import { HttpRequestMethods, IHttpClient } from 'model/clients/HttpClient';
 import * as k8sUrl from 'model/services/mapi/k8sUrl';
-import { LoggedInUserTypes } from 'stores/main/types';
 
 import { IClusterList } from './';
 
-export function getClusterList(client: IHttpClient, user: ILoggedInUser) {
-  return async () => {
-    const url = k8sUrl.create({
-      baseUrl: window.config.mapiEndpoint,
-      apiVersion: 'cluster.x-k8s.io/v1alpha3',
-      kind: 'clusters',
-    });
+export async function getClusterList(
+  client: IHttpClient,
+  auth: IOAuth2Provider
+) {
+  const user = await auth.getLoggedInUser();
+  if (!user) {
+    return Promise.reject(new Error('You must be logged in.'));
+    // eslint-disable-next-line no-magic-numbers
+  } else if (Date.now() / 1000 > user.expiresAt) {
+    return Promise.reject(
+      new Error('Your token is invalid. Please log in again.')
+    );
+  }
 
-    client.setURL(url.toString());
-    client.setHeader('Accept', 'application/json');
-    client.setRequestMethod(HttpRequestMethods.GET);
-    client.setAuthorizationToken(user.auth.scheme, user.auth.token);
+  client.setAuthorizationToken(user.authorizationType, user.idToken);
+  const response = await client.execute<IClusterList>();
 
-    const response = await client.execute<IClusterList>();
-
-    return response.data;
-  };
+  return response.data;
 }
 
-export function getClusterListKey(user: ILoggedInUser | null): string | null {
-  if (!user || user.type !== LoggedInUserTypes.MAPI) return null;
+export function getClusterListKey(client: IHttpClient, auth: IOAuth2Provider) {
+  const url = k8sUrl.create({
+    baseUrl: window.config.mapiEndpoint,
+    apiVersion: 'cluster.x-k8s.io/v1alpha3',
+    kind: 'clusters',
+  });
+
+  client.setRequestConfig({
+    url: url.toString(),
+    method: HttpRequestMethods.GET,
+    headers: {
+      Accept: 'application/json',
+    },
+  });
 
   // TODO(axbarsan): This might be a good place to handle permissions.
 
-  return 'getClusterList';
+  return [client, auth, url.toString()];
 }
