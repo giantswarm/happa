147e8655a28d76abdf0e778d70f0462f81b5165b
Marian Steinbach <marian@giantswarm.io>
2021-04-09 17:13:42 +0200

    Add props to mitigate warnings in storybook

--- src/components/UI/Display/Apps/AppDetailNew/AppDetailPage.stories.tsx
+++ src/components/UI/Display/Apps/AppDetailNew/AppDetailPage.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import React from 'react';
+import Button from 'UI/Controls/Button';
 
 import AppDetailPage, { IAppDetailPageProps } from './AppDetailPage';
 import markdownSample from './markdown_sample.md';
@@ -7,6 +8,11 @@ import markdownSample from './markdown_sample.md';
 export default {
   title: 'Display/Apps/AppDetailPage',
   component: AppDetailPage,
+  argTypes: {
+    selectVersion: {
+      action: 'click',
+    },
+  },
 } as Meta;
 
 const Template: Story<IAppDetailPageProps> = (args) => (
@@ -18,6 +24,7 @@ WithReadme.args = {
   appTitle: 'efk-stack-app',
   appIconURL: 'https://dummyimage.com/125/125/fff',
   catalogName: 'Giant Swarm Managed',
+  catalogDescription: 'Here goes the catalog description',
   chartVersion: 'v0.3.2',
   createDate: '2020-08-28T09:13:58.177126739Z',
   includesVersion: 'v1.9.0',
@@ -26,6 +33,8 @@ WithReadme.args = {
   keywords: ['elk', 'database', 'fluentd', 'logging', 'search'],
   readme: markdownSample,
   hasReadme: true,
+  otherVersions: [],
+  installAppModal: <Button>Install</Button>,
 };
 
 export const WithoutReadme = Template.bind({});
@@ -33,10 +42,14 @@ WithoutReadme.args = {
   appTitle: 'efk-stack-app',
   appIconURL: 'https://dummyimage.com/125/125/fff',
   catalogName: 'Giant Swarm Managed',
+  catalogDescription: 'Here goes the catalog description',
   chartVersion: 'v0.3.2',
   createDate: '2020-08-28T09:13:58.177126739Z',
   includesVersion: 'v1.9.0',
   description: 'Open Distro for ElasticSearch',
   website: 'github.com/giantswarm/efk-stack-app',
   keywords: ['elk', 'database', 'fluentd', 'logging', 'search'],
+  hasReadme: false,
+  otherVersions: [],
+  installAppModal: <Button>Install</Button>,
 };
