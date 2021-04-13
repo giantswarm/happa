f327d1a53ae9c2852134810e57eefd43a4a7b6ab
Adrian Barsan <mnlk123@yahoo.com>
2021-04-13 19:08:39 +0200

    Refactor the EditChartVersionPane component to TypeScript

--- src/components/Cluster/ClusterDetail/AppDetailsModal/EditChartVersionPane.tsx
+++ src/components/Cluster/ClusterDetail/AppDetailsModal/EditChartVersionPane.tsx
@@ -2,17 +2,26 @@ import useError from 'lib/hooks/useError';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { updateClusterApp } from 'stores/appcatalog/actions';
+import { FlashMessageType } from 'styles';
 import FlashMessage from 'UI/Display/FlashMessage';
 
-const EditChartVersionPane = (props) => {
+interface IEditChartVersionPaneProps {
+  currentVersion: string;
+  desiredVersion: string;
+}
+
+const EditChartVersionPane: React.FC<IEditChartVersionPaneProps> = ({
+  currentVersion,
+  desiredVersion,
+}) => {
   const { errorMessage } = useError(updateClusterApp().types.error);
 
   return (
     <div data-testid='edit-chart-version-pane'>
-      Current chart version: <code>{props.currentVersion}</code>
+      Current chart version: <code>{currentVersion}</code>
       <br />
       <br />
-      Desired chart version: <code>{props.desiredVersion}</code>
+      Desired chart version: <code>{desiredVersion}</code>
       <br />
       <br />
       <i className='fa fa-warning' /> There could be breaking changes between
@@ -24,7 +33,9 @@ const EditChartVersionPane = (props) => {
           Something went wrong while trying to set the chart version:
           <br />
           <br />
-          <FlashMessage type='danger'>{errorMessage}</FlashMessage>
+          <FlashMessage type={FlashMessageType.Danger}>
+            {errorMessage}
+          </FlashMessage>
         </>
       )}
     </div>
@@ -32,8 +43,8 @@ const EditChartVersionPane = (props) => {
 };
 
 EditChartVersionPane.propTypes = {
-  currentVersion: PropTypes.string,
-  desiredVersion: PropTypes.string,
+  currentVersion: PropTypes.string.isRequired,
+  desiredVersion: PropTypes.string.isRequired,
 };
 
 export default EditChartVersionPane;
