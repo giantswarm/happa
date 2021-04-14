1629103035afe281654fc72a60a297754a6e6f32
Adrian Barsan <mnlk123@yahoo.com>
2021-04-14 10:30:05 +0200

    Use grommet in the EditChartVersionPane component

--- src/components/Cluster/ClusterDetail/AppDetailsModal/EditChartVersionPane.tsx
+++ src/components/Cluster/ClusterDetail/AppDetailsModal/EditChartVersionPane.tsx
@@ -1,3 +1,4 @@
+import { Box, Text } from 'grommet';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FlashMessageType } from 'styles';
@@ -15,28 +16,40 @@ const EditChartVersionPane: React.FC<IEditChartVersionPaneProps> = ({
   errorMessage,
 }) => {
   return (
-    <div>
-      Current chart version: <code>{currentVersion}</code>
-      <br />
-      <br />
-      Desired chart version: <code>{desiredVersion}</code>
-      <br />
-      <br />
-      <i className='fa fa-warning' /> There could be breaking changes between
-      chart versions. It is up to you to verify if you are picking a valid
-      upgrade or downgrade path.
+    <Box direction='column' gap='medium'>
+      <Text>
+        Current chart version: <code>{currentVersion}</code>
+      </Text>
+      <Text>
+        Desired chart version: <code>{desiredVersion}</code>
+      </Text>
+      <Text>
+        <i
+          className='fa fa-warning'
+          role='presentation'
+          aria-hidden='true'
+          aria-label='Breaking changes'
+        />{' '}
+        There could be breaking changes between chart versions. It is up to you
+        to verify if you are picking a valid upgrade or downgrade path.
+      </Text>
       {errorMessage && (
-        <>
-          <hr />
-          Something went wrong while trying to set the chart version:
-          <br />
-          <br />
+        <Box
+          border={{ side: 'top' }}
+          pad={{ top: 'small' }}
+          direction='column'
+          gap='small'
+          margin={{ top: 'small' }}
+        >
+          <Text>
+            Something went wrong while trying to set the chart version:
+          </Text>
           <FlashMessage type={FlashMessageType.Danger}>
             {errorMessage}
           </FlashMessage>
-        </>
+        </Box>
       )}
-    </div>
+    </Box>
   );
 };
 
