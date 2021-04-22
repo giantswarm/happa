fc54ce17e7230fc426424571f9953e9ddac12132
Adrian Barsan <mnlk123@yahoo.com>
2021-04-22 14:28:32 +0200

    Add organizations UI types

---
+++ src/components/UI/Display/Organizations/types.ts
@@ -0,0 +1,21 @@
+export interface IOrganizationDetailClustersSummary {
+  nodesCount?: number;
+  nodesMemory?: number;
+  nodesCPU: number;
+  workerNodesCount?: number;
+  workerNodesMemory?: number;
+  workerNodesCPU: number;
+}
+
+export interface IOrganizationDetailReleasesSummary {
+  oldestReleaseVersion?: string;
+  oldestReleaseK8sVersion?: string;
+  newestReleaseVersion?: string;
+  newestReleaseK8sVersion?: string;
+  releasesInUseCount?: number;
+}
+
+export interface IOrganizationDetailAppsSummary {
+  appsInUseCount?: number;
+  appDeployments?: number;
+}
