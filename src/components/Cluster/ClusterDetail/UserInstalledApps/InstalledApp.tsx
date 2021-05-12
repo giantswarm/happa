edbb901ea0dad5cff70bb82993afb262782a3b11
Adrian Barsan <mnlk123@yahoo.com>
2021-05-12 12:21:42 +0200

    Customize installed app deleting state, to match cluster deletion state

--- src/components/Cluster/ClusterDetail/UserInstalledApps/InstalledApp.tsx
+++ src/components/Cluster/ClusterDetail/UserInstalledApps/InstalledApp.tsx
@@ -1,42 +1,31 @@
-import { Text } from 'grommet';
+import { Box, Text } from 'grommet';
 import { relativeDate } from 'lib/helpers';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 import NotAvailable from 'UI/Display/NotAvailable';
 
-const App = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.foreground};
-  margin-bottom: 14px;
-  padding: 12px;
-  border-radius: 5px;
-  border: 1px solid ${({ theme }) => theme.colors.foreground};
-
-  &:hover {
-    cursor: pointer;
-    border: 1px solid rgb(92, 127, 154);
+const Wrapper = styled(Box)`
+  :hover {
+    box-shadow: ${({ theme }) => `0 0 0 1px ${theme.global.colors.text.dark}`};
   }
 
-  img {
-    width: 36px;
-    height: 36px;
-    margin-right: 16px;
-    float: left;
-    position: relative;
-    top: 3px;
-    border-radius: 5px;
-    background-color: #fff;
-  }
+  &[aria-disabled='true'] {
+    cursor: default;
 
-  small {
-    font-size: 12px;
+    :hover {
+      box-shadow: ${({ theme }) => `0 0 0 0 ${theme.global.colors.text.dark}`};
+    }
   }
 `;
 
-interface IInstalledAppProps {
+const AppIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+interface IInstalledAppProps
+  extends React.ComponentPropsWithoutRef<typeof Box> {
   name: string;
   version: string;
   onIconError: React.ReactEventHandler<HTMLImageElement>;
@@ -56,24 +45,48 @@ const InstalledApp: React.FC<IInstalledAppProps> = ({
   onClick,
   ...rest
 }) => {
+  const isDeleting = typeof deletionTimestamp !== 'undefined';
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (isDeleting) return;
+
+    onClick(e);
+  };
+
   return (
-    <App onClick={onClick} {...rest}>
-      <div className='details'>
+    <Wrapper
+      role='button'
+      aria-disabled={isDeleting}
+      tabIndex={isDeleting ? -1 : 0}
+      onClick={handleClick}
+      direction='row'
+      gap='small'
+      align='center'
+      pad='small'
+      round='xsmall'
+      background={isDeleting ? 'background-back' : 'background-front'}
+      {...rest}
+    >
+      <Box overflow='hidden' round='xsmall' background='white'>
         {logoUrl && !iconErrors?.hasOwnProperty(logoUrl) && (
-          <img alt={`${name} icon`} onError={onIconError} src={logoUrl} />
+          <AppIcon alt={`${name} icon`} onError={onIconError} src={logoUrl} />
         )}
+      </Box>
 
-        <span>{name}</span>
+      <Box>
+        <Text>{name}</Text>
 
-        {deletionTimestamp && (
-          <Text size='xsmall' margin={{ left: 'xsmall' }} color='text-xweak'>
-            deleted {relativeDate(deletionTimestamp)}
+        {!isDeleting ? (
+          <Text size='xsmall' color='text-weak'>
+            Chart version: {version || <NotAvailable />}
+          </Text>
+        ) : (
+          <Text size='xsmall' color='text-xweak'>
+            Deleted {relativeDate(deletionTimestamp)}
           </Text>
         )}
-
-        <small>Chart version: {version || <NotAvailable />}</small>
-      </div>
-    </App>
+      </Box>
+    </Wrapper>
   );
 };
 
