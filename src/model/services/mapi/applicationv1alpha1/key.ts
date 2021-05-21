19cae89a75c874171585523b2900c3766ee8bd8b
Adrian Barsan <mnlk123@yahoo.com>
2021-05-21 12:00:06 +0200

    apps: implement app browsing via MAPI for SSO users (#2441)

--- src/model/services/mapi/applicationv1alpha1/key.ts
+++ src/model/services/mapi/applicationv1alpha1/key.ts
@@ -1,6 +1,23 @@
+import { IAppCatalog } from './';
+
 export const labelAppOperator = 'app-operator.giantswarm.io/version';
 export const labelAppName = 'app.kubernetes.io/name';
 export const labelAppVersion = 'app.kubernetes.io/version';
 export const labelAppCatalog = 'application.giantswarm.io/catalog';
 export const labelManagedBy = 'giantswarm.io/managed-by';
 export const labelLatest = 'latest';
+export const labelCatalogVisibility =
+  'application.giantswarm.io/catalog-visibility';
+export const labelCatalogType = 'application.giantswarm.io/catalog-type';
+
+export function isAppCatalogPublic(catalog: IAppCatalog): boolean {
+  const visibility = catalog.metadata.labels?.[labelCatalogVisibility];
+
+  return visibility === 'public';
+}
+
+export function isAppCatalogStable(catalog: IAppCatalog): boolean {
+  const type = catalog.metadata.labels?.[labelCatalogType];
+
+  return type === 'stable';
+}
