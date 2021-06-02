0cd42474bbfc8480d4f081b360ba40d300d70452
Adrian Barsan <mnlk123@yahoo.com>
2021-06-02 15:35:05 +0200

    Enable further optimization using the external helpers package

--- webpack.common.js
+++ webpack.common.js
@@ -151,6 +151,7 @@ module.exports = {
                   runtime: 'automatic',
                 },
               },
+              externalHelpers: true,
             },
             env: {
               targets: '> 0.25%, not dead',
