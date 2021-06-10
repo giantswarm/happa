0c51c3b133f7976539094c069e7d337e629efbd0
Adrian Barsan <mnlk123@yahoo.com>
2021-06-10 13:21:44 +0200

    Introduce new ControlPlaneNode union type for provider-specific control plane nodes

---
+++ src/components/MAPI/types.ts
@@ -0,0 +1,5 @@
+import * as capzv1alpha3 from 'model/services/mapi/capzv1alpha3';
+
+export type ControlPlaneNode = capzv1alpha3.IAzureMachine;
+
+export type ControlPlaneNodeList = capzv1alpha3.IAzureMachineList;
