7cb663d2c60eeebb403ff62dd07b75ca2c5f78a5
Adrian Barsan <mnlk123@yahoo.com>
2021-06-24 10:55:16 +0200

    Add capiexpv1alpha3 utility for getting machinepool scaling

---
+++ src/model/services/mapi/capiv1alpha3/exp/key.ts
@@ -0,0 +1,33 @@
+import { IMachinePool } from './types';
+
+export const annotationMachinePoolDescription =
+  'machine-pool.giantswarm.io/name';
+export const annotationMachinePoolMinSize =
+  'cluster.k8s.io/cluster-api-autoscaler-node-group-min-size';
+export const annotationMachinePoolMaxSize =
+  'cluster.k8s.io/cluster-api-autoscaler-node-group-max-size';
+
+export function getMachinePoolDescription(machinePool: IMachinePool): string {
+  let name =
+    machinePool.metadata.annotations?.[annotationMachinePoolDescription];
+  name ??= 'Unnamed node pool';
+
+  return name;
+}
+
+export function getMachinePoolScaling(
+  machinePool: IMachinePool
+): readonly [number, number] {
+  const annotations = machinePool.metadata.annotations;
+  if (!annotations) return [-1, -1];
+
+  const minScaling = annotations[annotationMachinePoolMinSize];
+  const maxScaling = annotations[annotationMachinePoolMaxSize];
+  if (!minScaling || !maxScaling) return [-1, -1];
+
+  try {
+    return [parseInt(minScaling, 10), parseInt(maxScaling, 10)];
+  } catch {
+    return [-1, -1];
+  }
+}
