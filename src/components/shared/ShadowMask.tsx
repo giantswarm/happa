ec9fc44ca1ee8bbcf846d1459aa3f83b7db5165b
Oliver Ponder <oliver.ponder@gmail.com>
2021-06-25 19:49:34 +0800

    Remove login form sass

---
+++ src/components/shared/ShadowMask.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ShadowMask = styled.div`
+  background-color: #152531;
+  opacity: 1;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  position: fixed;
+  display: block;
+  z-index: 0;
+`;
+
+export default ShadowMask;
