d9932808b5fdefa8788406967a66b26dfdb723a9
Adrian Barsan <mnlk123@yahoo.com>
2021-06-29 17:27:01 +0200

    Create components for configuring a node pool's name and description

---
+++ src/components/MAPI/workernodes/WorkerNodesCreateNodePoolDescription.tsx
@@ -0,0 +1,75 @@
+import { hasAppropriateLength } from 'lib/helpers';
+import { NodePool } from 'MAPI/types';
+import { getNodePoolDescription } from 'MAPI/utils';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Constants } from 'shared/constants';
+import InputGroup from 'UI/Inputs/InputGroup';
+import TextInput from 'UI/Inputs/TextInput';
+
+import { INodePoolPropertyProps, withNodePoolDescription } from './patches';
+
+function validateValue(newValue: string): string {
+  const { message } = hasAppropriateLength(
+    newValue,
+    Constants.MIN_NAME_LENGTH,
+    Constants.MAX_NAME_LENGTH
+  );
+
+  return message;
+}
+
+interface IWorkerNodesCreateNodePoolDescriptionProps
+  extends INodePoolPropertyProps,
+    Omit<
+      React.ComponentPropsWithoutRef<typeof InputGroup>,
+      'onChange' | 'id'
+    > {}
+
+const WorkerNodesCreateNodePoolDescription: React.FC<IWorkerNodesCreateNodePoolDescriptionProps> = ({
+  id,
+  nodePool,
+  onChange,
+  readOnly,
+  disabled,
+  ...props
+}) => {
+  const [validationError, setValidationError] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const validationResult = validateValue(e.target.value);
+    setValidationError(validationResult);
+
+    onChange({
+      isValid: validationResult.length < 1,
+      patch: withNodePoolDescription(e.target.value),
+    });
+  };
+
+  const value = getNodePoolDescription(nodePool);
+
+  return (
+    <InputGroup htmlFor={id} label='Description' {...props}>
+      <TextInput
+        value={value}
+        id={id}
+        placeholder='Unnamed node pool'
+        error={validationError}
+        onChange={handleChange}
+        help='Pick a description that helps team mates to understand what these nodes are here for. You can change this later. Each node pool also gets a unique identifier.'
+        readOnly={readOnly}
+        disabled={disabled}
+      />
+    </InputGroup>
+  );
+};
+
+WorkerNodesCreateNodePoolDescription.propTypes = {
+  id: PropTypes.string.isRequired,
+  nodePool: (PropTypes.object as PropTypes.Requireable<NodePool>).isRequired,
+  onChange: PropTypes.func.isRequired,
+  readOnly: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+export default WorkerNodesCreateNodePoolDescription;
