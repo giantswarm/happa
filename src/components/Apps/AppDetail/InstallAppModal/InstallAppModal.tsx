a74d0bb2f4be10c00845b3bc0fe9dedbc2f88243
Marian Steinbach <marian@giantswarm.io>
2021-07-02 20:47:41 +0200

    Avoid error notification for invalid app values file (#2537)
    * Do not report error for invalid app values
    
    * Remove more notifications

--- src/components/Apps/AppDetail/InstallAppModal/InstallAppModal.tsx
+++ src/components/Apps/AppDetail/InstallAppModal/InstallAppModal.tsx
@@ -206,8 +206,6 @@ const InstallAppModal: React.FC<IInstallAppModalProps> = (props) => {
         setValuesYAMLError('');
       } catch (err) {
         setValuesYAMLError('Unable to parse valid YAML from this file.');
-
-        ErrorReporter.getInstance().notify(err);
       }
     };
 
@@ -232,8 +230,6 @@ const InstallAppModal: React.FC<IInstallAppModalProps> = (props) => {
         setSecretsYAMLError('');
       } catch (err) {
         setSecretsYAMLError('Unable to parse valid YAML from this file.');
-
-        ErrorReporter.getInstance().notify(err);
       }
     };
 
