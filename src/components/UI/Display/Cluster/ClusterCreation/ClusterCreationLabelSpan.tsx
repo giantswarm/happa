d029bb92bde5d54e54809165b3e8be2a23ffbf30
Oliver Ponder <oliver.ponder@gmail.com>
2021-07-06 16:40:11 +0800

    Consolidate an extracted label style that is only being used in one place.

--- src/components/UI/Display/Cluster/ClusterCreation/ClusterCreationLabelSpan.tsx
+++ src/components/UI/Display/Cluster/ClusterCreation/ClusterCreationLabelSpan.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
-import { baseLabelStyles } from 'styles/cluster-creation';
+import theme from 'styles/theme';
 
 const LabelSpan = styled.span`
-  ${baseLabelStyles};
+  color: ${theme.colors.white1};
+  font-size: 16px;
+  margin-bottom: 14px;
+  font-weight: 400;
 `;
 
 export default LabelSpan;
