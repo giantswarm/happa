1c7c9c68b41a37e13af345ca75d4c690aaffdec1
Adrian Barsan <mnlk123@yahoo.com>
2021-07-15 14:41:08 +0200

    Add mocks for Spark CRs

---
+++ testUtils/mockHttpCalls/gscorev1alpha1/sparks.ts
@@ -0,0 +1,73 @@
+import * as gscorev1alpha1 from 'model/services/mapi/gscorev1alpha1';
+
+export const randomCluster1MachinePool1Spark: gscorev1alpha1.ISpark = {
+  apiVersion: 'core.giantswarm.io/v1alpha1',
+  kind: 'Spark',
+  metadata: {
+    namespace: 'org-org1',
+    name: 't6yo9',
+    labels: {
+      'cluster.x-k8s.io/cluster-name': 'j5y9m',
+      'giantswarm.io/cluster': 'j5y9m',
+    },
+  },
+  spec: {},
+  status: {
+    ready: true,
+    failureMessage: '',
+    dataSecretName: '',
+    failureReason: '',
+    verification: {
+      hash: '',
+      algorithm: '',
+    },
+  },
+};
+
+export const randomCluster1MachinePool2Spark: gscorev1alpha1.ISpark = {
+  apiVersion: 'core.giantswarm.io/v1alpha1',
+  kind: 'Spark',
+  metadata: {
+    namespace: 'org-org1',
+    name: 'f029a',
+    labels: {
+      'cluster.x-k8s.io/cluster-name': 'j5y9m',
+      'giantswarm.io/cluster': 'j5y9m',
+    },
+  },
+  spec: {},
+  status: {
+    ready: true,
+    failureMessage: '',
+    dataSecretName: '',
+    failureReason: '',
+    verification: {
+      hash: '',
+      algorithm: '',
+    },
+  },
+};
+
+export const randomCluster2MachinePool1Spark: gscorev1alpha1.ISpark = {
+  apiVersion: 'core.giantswarm.io/v1alpha1',
+  kind: 'Spark',
+  metadata: {
+    namespace: 'org-org1',
+    name: 't6yo9',
+    labels: {
+      'cluster.x-k8s.io/cluster-name': 'as43z',
+      'giantswarm.io/cluster': 'as43z',
+    },
+  },
+  spec: {},
+  status: {
+    ready: true,
+    failureMessage: '',
+    dataSecretName: '',
+    failureReason: '',
+    verification: {
+      hash: '',
+      algorithm: '',
+    },
+  },
+};
