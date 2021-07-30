7729e358616367deb7be6a37ab1e3642d729236d
Sandy Kuo <kuo.sandys@gmail.com>
2021-07-30 12:02:00 +0200

    Update Organization interface

--- src/model/services/mapi/securityv1alpha1/types.ts
+++ src/model/services/mapi/securityv1alpha1/types.ts
@@ -2,11 +2,16 @@ import { IList, IObjectMeta } from '../metav1/types';
 
 export interface IOrganizationSpec {}
 
+export interface IOrganizationStatus {
+  namespace?: string;
+}
+
 export interface IOrganization {
   apiVersion: string;
   kind: string;
   metadata: IObjectMeta;
   spec: IOrganizationSpec;
+  status?: IOrganizationStatus;
 }
 
 export interface IOrganizationList extends IList<IOrganization> {}
