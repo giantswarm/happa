133da2b20a0d53822342b589e870e732568f81c2
Sandy Kuo <kuo.sandys@gmail.com>
2021-08-18 21:58:39 +0200

    Add CLI guide for creating a node pool

---
+++ src/components/MAPI/workernodes/guides/CreateNodePoolGuide.tsx
@@ -0,0 +1,120 @@
+import { Text } from 'grommet';
+import * as docs from 'lib/docs';
+import LoginGuideStep from 'MAPI/guides/LoginGuideStep';
+import {
+  getCurrentInstallationContextName,
+  makeKubectlGSCommand,
+  withFormatting,
+  withTemplateNodePool,
+} from 'MAPI/guides/utils';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Providers } from 'shared/constants';
+import { PropertiesOf } from 'shared/types';
+import CLIGuide from 'UI/Display/MAPI/CLIGuide';
+import CLIGuideAdditionalInfo from 'UI/Display/MAPI/CLIGuide/CLIGuideAdditionalInfo';
+import CLIGuideStep from 'UI/Display/MAPI/CLIGuide/CLIGuideStep';
+import CLIGuideStepList from 'UI/Display/MAPI/CLIGuide/CLIGuideStepList';
+
+interface ICreateNodePoolGuide
+  extends Omit<React.ComponentPropsWithoutRef<typeof CLIGuide>, 'title'> {
+  provider: PropertiesOf<typeof Providers>;
+  organizationName: string;
+  clusterName: string;
+  description?: string;
+  azureVMSize?: string;
+  nodePoolAZs?: string[];
+  azureUseSpotVMs?: boolean;
+  azureSpotVMsMaxPrice?: number;
+  nodesMin?: number;
+  nodesMax?: number;
+}
+
+const CreateNodePoolGuide: React.FC<ICreateNodePoolGuide> = ({
+  provider,
+  organizationName,
+  clusterName,
+  description,
+  azureVMSize,
+  nodePoolAZs,
+  azureUseSpotVMs,
+  azureSpotVMsMaxPrice,
+  nodesMin,
+  nodesMax,
+  ...props
+}) => {
+  const context = useSelector(getCurrentInstallationContextName);
+
+  return (
+    <CLIGuide
+      title='Create a node pool via the Management API'
+      footer={
+        <CLIGuideAdditionalInfo
+          links={[
+            {
+              label: 'kubectl gs plugin installation',
+              href: docs.kubectlGSInstallationURL,
+              external: true,
+            },
+            {
+              label: 'kubectl gs template nodepool command',
+              href: docs.kubectlGSTemplateNodePoolURL,
+              external: true,
+            },
+            {
+              label: 'Management API introduction',
+              href: docs.managementAPIIntroduction,
+              external: true,
+            },
+          ]}
+        />
+      }
+      {...props}
+    >
+      <CLIGuideStepList>
+        <LoginGuideStep />
+        <CLIGuideStep
+          title='2. Create a node pool manifest'
+          command={makeKubectlGSCommand(
+            withTemplateNodePool({
+              provider,
+              owner: organizationName,
+              clusterName,
+              description,
+              azureVMSize,
+              nodePoolAZs,
+              azureUseSpotVMs,
+              azureSpotVMsMaxPrice,
+              nodesMin,
+              nodesMax,
+              output: `cluster-${clusterName}-nodepool.yaml`,
+            }),
+            withFormatting()
+          )}
+        />
+        <CLIGuideStep
+          title='3. Apply the manifest'
+          command={`kubectl apply --context ${context} -f cluster-${clusterName}-nodepool.yaml`}
+        >
+          <Text>As a result, a node pool will get created.</Text>
+        </CLIGuideStep>
+      </CLIGuideStepList>
+    </CLIGuide>
+  );
+};
+
+CreateNodePoolGuide.propTypes = {
+  provider: PropTypes.oneOf(Object.values(Providers)).isRequired,
+  organizationName: PropTypes.string.isRequired,
+  clusterName: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  azureVMSize: PropTypes.string,
+  nodePoolAZs: PropTypes.arrayOf(PropTypes.string.isRequired),
+  azureUseSpotVMs: PropTypes.bool,
+  azureSpotVMsMaxPrice: PropTypes.number,
+  nodesMin: PropTypes.number,
+  nodesMax: PropTypes.number,
+};
+
+export default CreateNodePoolGuide;
