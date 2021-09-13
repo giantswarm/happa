a9651a69833f380b8ab31f03799711189c3168a1
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2021-09-13 13:32:40 +0200

    Improve Apps browsing performance: virtualize the Apps list (#2755)
    * Create virtualized grid component
    
    * Use variable grid instead for better resizing support
    
    * Fix scroll position restoration
    
    * Rename component to avoid namming conflict
    
    * Update snapshots
    
    * Apply suggestions from code review
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    
    * Remove unnecessary flooring on integers
    
    * Remove unused index argument
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>

---
+++ src/components/UI/Display/Apps/AppList/AppGrid.tsx
@@ -0,0 +1,138 @@
+import React, { memo, useLayoutEffect, useRef, useState } from 'react';
+import { WindowScroller } from 'react-virtualized';
+import {
+  areEqual,
+  GridChildComponentProps,
+  VariableSizeGrid,
+} from 'react-window';
+import styled from 'styled-components';
+import { IAppProps } from 'UI/Display/Apps/AppList/App';
+
+interface IAppGridProps {
+  items: IAppProps[];
+  itemMinWidth: number;
+  itemMinHeight: number;
+  gridGap: number;
+  render: (item: IAppProps) => React.ReactNode;
+}
+
+interface IScrollPosition {
+  scrollTop: number;
+}
+
+interface IDataProps {
+  items: IAppProps[];
+  columnCount: number;
+  gridGap: number;
+  render: (item: IAppProps) => React.ReactNode;
+}
+
+const StyledContainer = styled.div<{ gridGap: number }>`
+  margin: 0 -${(props) => props.gridGap / 2}px;
+`;
+
+const StyledGrid = styled(VariableSizeGrid)<{ minHeight: number }>`
+  height: 100% !important;
+
+  > div {
+    height: ${(props) => props.minHeight}px !important;
+  }
+`;
+
+const getIndex = (
+  rowIndex: number,
+  columnIndex: number,
+  columnCount: number
+) => {
+  return rowIndex * columnCount + columnIndex;
+};
+
+const ItemRenderer: React.FC<GridChildComponentProps> = memo(
+  ({ columnIndex, rowIndex, data, style }) => {
+    const { items, columnCount, render, gridGap }: IDataProps = data;
+    const index = getIndex(rowIndex, columnIndex, columnCount);
+    const item = items[index];
+
+    const patchedStyle = Object.assign({}, style, {
+      padding: `0 ${gridGap / 2}px`,
+    });
+
+    return item ? <div style={patchedStyle}>{render(item)}</div> : null;
+  },
+  areEqual
+);
+
+const AppGrid: React.FC<IAppGridProps> = ({
+  items,
+  itemMinWidth,
+  itemMinHeight,
+  gridGap,
+  render,
+}) => {
+  const [width, setWidth] = useState<number>(0);
+
+  const gridRef = useRef<VariableSizeGrid>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = ({ scrollTop }: IScrollPosition) => {
+    if (gridRef.current) gridRef.current.scrollTo({ scrollTop });
+  };
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      if (containerRef.current) {
+        setWidth(containerRef.current.getBoundingClientRect().width);
+      }
+      if (gridRef.current) {
+        gridRef.current.resetAfterIndices({
+          columnIndex: 0,
+          rowIndex: 0,
+        });
+      }
+    };
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const columnCount = Math.floor(width / (itemMinWidth + gridGap)) || 1;
+  const rowCount = items.length / columnCount;
+  const minHeight = (itemMinHeight + gridGap) * rowCount;
+
+  const columnWidth = () => width / columnCount;
+  const rowHeight = () => itemMinHeight + gridGap;
+
+  const itemData: IDataProps = {
+    items,
+    columnCount,
+    gridGap,
+    render,
+  };
+
+  return (
+    <StyledContainer ref={containerRef} gridGap={gridGap}>
+      <WindowScroller onScroll={handleScroll}>
+        {({ height }) => (
+          <StyledGrid
+            ref={gridRef}
+            width={width}
+            height={height}
+            columnWidth={columnWidth}
+            rowHeight={rowHeight}
+            columnCount={columnCount}
+            rowCount={rowCount}
+            itemData={itemData}
+            overscanRowCount={10}
+            minHeight={minHeight}
+          >
+            {ItemRenderer}
+          </StyledGrid>
+        )}
+      </WindowScroller>
+    </StyledContainer>
+  );
+};
+
+export default AppGrid;
