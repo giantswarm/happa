bf4e4fba063b80b9440456e75f5fcc39088ba19c
Adrian Barsan <mnlk123@yahoo.com>
2021-09-28 15:01:36 +0200

    Add capzv1alpaha4 types and client functions

---
+++ src/model/services/mapi/capzv1alpha4/createAzureMachinePool.ts
@@ -0,0 +1,27 @@
+import { IOAuth2Provider } from 'lib/OAuth2/OAuth2';
+import { IHttpClient } from 'model/clients/HttpClient';
+import * as k8sUrl from 'model/services/mapi/k8sUrl';
+
+import { createResource } from '../generic/createResource';
+import { IAzureMachinePool } from '.';
+
+export function createAzureMachinePool(
+  client: IHttpClient,
+  auth: IOAuth2Provider,
+  azureMachinePool: IAzureMachinePool
+) {
+  const url = k8sUrl.create({
+    baseUrl: window.config.mapiEndpoint,
+    apiVersion: 'infrastructure.cluster.x-k8s.io/v1alpha4',
+    kind: 'azuremachinepools',
+    namespace: azureMachinePool.metadata.namespace!,
+    name: azureMachinePool.metadata.name,
+  } as k8sUrl.IK8sUpdateOptions);
+
+  return createResource<IAzureMachinePool>(
+    client,
+    auth,
+    url.toString(),
+    azureMachinePool
+  );
+}
---
+++ src/model/services/mapi/capzv1alpha4/index.ts
@@ -0,0 +1,3 @@
+export * from './types';
+export * from './getAzureMachinePool';
+export * from './createAzureMachinePool';
