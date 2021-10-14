15334bdb08a72cdb6232e5613b76343c39d99996
Sandy Kuo <kuo.sandys@gmail.com>
2021-10-14 09:15:30 +0200

    Add guides for listing apps in catalogs and listing catalogs

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -67,6 +67,9 @@ export const kubectlGSLoginURL =
 export const kubectlGSGetClustersURL =
   'https://docs.giantswarm.io/ui-api/kubectl-gs/get-clusters/';
 
+export const kubectlGSGetCatalogsURL =
+  'https://docs.giantswarm.io/ui-api/kubectl-gs/get-catalogs/';
+
 export const kubectlGSTemplateClusterURL =
   'https://docs.giantswarm.io/ui-api/kubectl-gs/template-cluster/';
 
@@ -103,6 +106,7 @@ export const crds = {
     app: 'apps.application.giantswarm.io',
     organization: 'organizations.security.giantswarm.io',
     release: 'releases.release.giantswarm.io',
+    catalog: 'catalogs.application.giantswarm.io/',
   },
   xk8sio: {
     azureCluster: 'azureclusters.infrastructure.cluster.x-k8s.io',
