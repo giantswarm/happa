75bba8d0e271d366ba769403a5bca869d32c9a18
Adrian Barsan <mnlk123@yahoo.com>
2021-10-25 18:53:24 +0200

    Add support for deleting AWS node pools via MAPI

---
+++ src/model/services/mapi/capiv1alpha3/deleteMachineDeployment.ts
@@ -0,0 +1,22 @@
+import { IOAuth2Provider } from 'lib/OAuth2/OAuth2';
+import { IHttpClient } from 'model/clients/HttpClient';
+import * as k8sUrl from 'model/services/mapi/k8sUrl';
+
+import { deleteResource } from '../generic/deleteResource';
+import { IMachineDeployment } from './types';
+
+export function deleteMachineDeployment(
+  client: IHttpClient,
+  auth: IOAuth2Provider,
+  machineDeployment: IMachineDeployment
+) {
+  const url = k8sUrl.create({
+    baseUrl: window.config.mapiEndpoint,
+    apiVersion: 'cluster.x-k8s.io/v1alpha3',
+    kind: 'machinedeployments',
+    namespace: machineDeployment.metadata.namespace,
+    name: machineDeployment.metadata.name,
+  } as k8sUrl.IK8sDeleteOptions);
+
+  return deleteResource(client, auth, url.toString());
+}
