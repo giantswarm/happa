926ec8de37283fe916483600ddc708629ee0a6ce
Adrian Barsan <mnlk123@yahoo.com>
2021-10-25 18:16:56 +0200

    Add support for deleting AWS clusters via MAPI

--- src/components/MAPI/clusters/ClusterDetail/utils.ts
+++ src/components/MAPI/clusters/ClusterDetail/utils.ts
@@ -4,14 +4,19 @@ import { IOAuth2Provider } from 'lib/OAuth2/OAuth2';
 import { Cluster, ControlPlaneNode, ProviderCluster } from 'MAPI/types';
 import {
   fetchCluster,
+  fetchControlPlaneNodesForCluster,
+  fetchControlPlaneNodesForClusterKey,
   fetchProviderClusterForCluster,
+  fetchProviderClusterForClusterKey,
   getClusterDescription,
 } from 'MAPI/utils';
+import { GenericResponseError } from 'model/clients/GenericResponseError';
 import { IHttpClient } from 'model/clients/HttpClient';
 import * as capiv1alpha3 from 'model/services/mapi/capiv1alpha3';
 import * as capzv1alpha3 from 'model/services/mapi/capzv1alpha3';
 import * as infrav1alpha3 from 'model/services/mapi/infrastructurev1alpha3';
 import * as legacyCredentials from 'model/services/mapi/legacy/credentials';
+import * as metav1 from 'model/services/mapi/metav1';
 import { Providers } from 'shared/constants';
 import { PropertiesOf } from 'shared/types';
 import { filterLabels } from 'stores/cluster/utils';
@@ -80,19 +85,21 @@ export async function updateClusterDescription(
 }
 
 export async function deleteCluster(
-  httpClient: IHttpClient,
+  httpClientFactory: HttpClientFactory,
   auth: IOAuth2Provider,
   cluster: Cluster
 ) {
   if (cluster.kind === capiv1alpha3.Cluster) {
+    const client = httpClientFactory();
+
     const updatedCluster = await capiv1alpha3.getCluster(
-      httpClient,
+      client,
       auth,
       cluster.metadata.namespace!,
       cluster.metadata.name
     );
 
-    await capiv1alpha3.deleteCluster(httpClient, auth, updatedCluster);
+    await capiv1alpha3.deleteCluster(client, auth, updatedCluster);
 
     updatedCluster.metadata.deletionTimestamp = new Date().toISOString();
 
@@ -125,6 +132,123 @@ export async function deleteCluster(
   }
 
   return Promise.reject(new Error('Unsupported provider.'));
+}
+
+export async function deleteProviderClusterForCluster(
+  httpClientFactory: HttpClientFactory,
+  auth: IOAuth2Provider,
+  cluster: Cluster
+) {
+  const providerCluster = await fetchProviderClusterForCluster(
+    httpClientFactory,
+    auth,
+    cluster
+  );
+
+  switch (providerCluster?.apiVersion) {
+    case 'infrastructure.giantswarm.io/v1alpha3': {
+      const client = httpClientFactory();
+
+      await infrav1alpha3.deleteAWSCluster(client, auth, providerCluster);
+
+      providerCluster.metadata.deletionTimestamp = new Date().toISOString();
+
+      mutate(
+        fetchProviderClusterForClusterKey(cluster),
+        providerCluster,
+        false
+      );
+
+      return providerCluster;
+    }
+
+    default:
+      return Promise.reject(new Error('Unsupported provider.'));
+  }
+}
+
+export async function deleteControlPlaneNodesForCluster(
+  httpClientFactory: HttpClientFactory,
+  auth: IOAuth2Provider,
+  cluster: Cluster
+) {
+  const controlPlaneNodes = await fetchControlPlaneNodesForCluster(
+    httpClientFactory,
+    auth,
+    cluster
+  );
+
+  const requests = controlPlaneNodes.map(async (controlPlaneNode) => {
+    switch (controlPlaneNode.kind) {
+      case infrav1alpha3.AWSControlPlane: {
+        const client = httpClientFactory();
+
+        await infrav1alpha3.deleteAWSControlPlane(
+          client,
+          auth,
+          controlPlaneNode
+        );
+
+        controlPlaneNode.metadata.deletionTimestamp = new Date().toISOString();
+
+        return controlPlaneNode;
+      }
+
+      case infrav1alpha3.G8sControlPlane: {
+        const client = httpClientFactory();
+
+        await infrav1alpha3.deleteG8sControlPlane(
+          client,
+          auth,
+          controlPlaneNode
+        );
+
+        controlPlaneNode.metadata.deletionTimestamp = new Date().toISOString();
+
+        return controlPlaneNode;
+      }
+
+      default:
+        return Promise.reject(new Error('Unsupported provider.'));
+    }
+  });
+
+  const updatedControlPlaneNodes = await Promise.all(requests);
+
+  mutate(
+    fetchControlPlaneNodesForClusterKey(cluster),
+    updatedControlPlaneNodes,
+    false
+  );
+}
+
+export async function deleteClusterResources(
+  httpClientFactory: HttpClientFactory,
+  auth: IOAuth2Provider,
+  cluster: Cluster
+) {
+  try {
+    await deleteCluster(httpClientFactory, auth, cluster);
+
+    if (
+      cluster.spec?.infrastructureRef?.apiVersion ===
+      'infrastructure.giantswarm.io/v1alpha3'
+    ) {
+      await deleteProviderClusterForCluster(httpClientFactory, auth, cluster);
+      await deleteControlPlaneNodesForCluster(httpClientFactory, auth, cluster);
+    }
+  } catch (err) {
+    if (
+      !metav1.isStatusError(
+        (err as GenericResponseError).data,
+        metav1.K8sStatusErrorReasons.NotFound
+      )
+    ) {
+      return Promise.reject(err);
+    }
+  }
+
+  return Promise.resolve();
 }
 
 export function getVisibleLabels(cluster?: capiv1alpha3.ICluster) {
