0fe4ef2f324d852233a7bb3adece97ab3c80100d
Adrian Barsan <mnlk123@yahoo.com>
2021-11-02 12:25:32 +0100

    Display if a node pool is using alike instance types

--- src/components/MAPI/workernodes/WorkerNodesNodePoolItemMachineType.tsx
+++ src/components/MAPI/workernodes/WorkerNodesNodePoolItemMachineType.tsx
@@ -1,9 +1,17 @@
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
 import { NodePool, ProviderNodePool } from 'MAPI/types';
 import { getProviderNodePoolMachineType } from 'MAPI/utils';
 import React from 'react';
+import styled from 'styled-components';
 import { Code } from 'styles';
+import NotAvailable from 'UI/Display/NotAvailable';
 import OptionalValue from 'UI/Display/OptionalValue/OptionalValue';
+import { Tooltip, TooltipContainer } from 'UI/Display/Tooltip';
+
+const MixedInstanceType = styled(Code)`
+  background: ${({ theme }) => theme.global.colors['accent-strong'].dark};
+  color: ${({ theme }) => theme.global.colors.text.dark};
+`;
 
 function formatMachineTypeLabel(providerNodePool?: ProviderNodePool) {
   switch (providerNodePool?.apiVersion) {
@@ -33,14 +41,61 @@ const WorkerNodesNodePoolItemMachineType: React.FC<IWorkerNodesNodePoolItemMachi
       ? getProviderNodePoolMachineType(providerNodePool)
       : undefined;
 
+    const useAlikeInstanceTypes =
+      providerNodePool?.apiVersion ===
+        'infrastructure.giantswarm.io/v1alpha3' &&
+      providerNodePool.spec.provider.worker.useAlikeInstanceTypes;
+    const instanceTypes =
+      providerNodePool?.apiVersion === 'infrastructure.giantswarm.io/v1alpha3'
+        ? providerNodePool.status?.provider?.worker?.instanceTypes
+        : undefined;
+
     return (
       <Box align='center' {...props}>
         <OptionalValue value={machineType} loaderWidth={130}>
-          {(value) => (
-            <Code aria-label={formatMachineTypeLabel(providerNodePool)}>
-              {value}
-            </Code>
-          )}
+          {(value) =>
+            useAlikeInstanceTypes ? (
+              <TooltipContainer
+                content={
+                  <Tooltip
+                    id={`${providerNodePool.metadata.name}-instance-types`}
+                  >
+                    <Box width='180px'>
+                      <Text size='xsmall' textAlign='center'>
+                        Similar instances enabled.
+                      </Text>
+                      <Text size='xsmall' textAlign='center'>
+                        Currently used:{' '}
+                        {instanceTypes?.join(', ') ?? <NotAvailable />}
+                      </Text>
+                    </Box>
+                  </Tooltip>
+                }
+              >
+                <div>
+                  <MixedInstanceType
+                    aria-label={formatMachineTypeLabel(providerNodePool)}
+                  >
+                    {value}
+                  </MixedInstanceType>
+                  {instanceTypes && instanceTypes.length > 1 && (
+                    <Text
+                      size='xsmall'
+                      margin={{ left: 'xsmall' }}
+                      color='text-weak'
+                    >
+                      +{instanceTypes.length - 1} more
+                    </Text>
+                  )}
+                </div>
+              </TooltipContainer>
+            ) : (
+              <Code aria-label={formatMachineTypeLabel(providerNodePool)}>
+                {' '}
+                {value}
+              </Code>
+            )
+          }
         </OptionalValue>
       </Box>
     );
