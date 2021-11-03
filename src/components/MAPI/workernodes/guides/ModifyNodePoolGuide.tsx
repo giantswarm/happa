866d72e5ecf59991e8ce0978fff67b9b3945095d
Adrian Barsan <mnlk123@yahoo.com>
2021-11-03 15:19:31 +0100

    node pools: adapt NP modifying guide to work for AWS (#2892)

--- src/components/MAPI/workernodes/guides/ModifyNodePoolGuide.tsx
+++ src/components/MAPI/workernodes/guides/ModifyNodePoolGuide.tsx
@@ -3,6 +3,8 @@ import * as docs from 'lib/docs';
 import LoginGuideStep from 'MAPI/guides/LoginGuideStep';
 import { getCurrentInstallationContextName } from 'MAPI/guides/utils';
 import React from 'react';
+import { Providers } from 'shared/constants';
+import { PropertiesOf } from 'shared/types';
 import CLIGuide from 'UI/Display/MAPI/CLIGuide';
 import CLIGuideAdditionalInfo from 'UI/Display/MAPI/CLIGuide/CLIGuideAdditionalInfo';
 import CLIGuideStep from 'UI/Display/MAPI/CLIGuide/CLIGuideStep';
@@ -11,10 +13,12 @@ import CLIGuideStepList from 'UI/Display/MAPI/CLIGuide/CLIGuideStepList';
 interface IModifyNodePoolGuideProps
   extends Omit<React.ComponentPropsWithoutRef<typeof CLIGuide>, 'title'> {
   clusterNamespace: string;
+  provider: PropertiesOf<typeof Providers>;
 }
 
 const ModifyNodePoolGuide: React.FC<IModifyNodePoolGuideProps> = ({
   clusterNamespace,
+  provider,
   ...props
 }) => {
   const context = getCurrentInstallationContextName();
@@ -49,13 +53,22 @@ const ModifyNodePoolGuide: React.FC<IModifyNodePoolGuideProps> = ({
         <LoginGuideStep />
         <CLIGuideStep
           title='2. Update the node pool description'
-          command={`
+          command={
+            provider === Providers.AWS
+              ? `
+          kubectl --context ${context} \\
+            patch awsmachinedeployments.infrastructure.giantswarm.io my-np \\
+            --namespace ${clusterNamespace} \\
+            --type merge \\
+            --patch '{"spec": {"nodePool": {"description": "General purpose nodes"}}}'`
+              : `
           kubectl --context ${context} \\
             patch machinepools.exp.cluster.x-k8s.io my-np \\
             --namespace ${clusterNamespace} \\
             --type merge \\
             --patch '{"metadata": {"annotations": {"machine-pool.giantswarm.io/name": "General purpose nodes"}}}'
-          `}
+          `
+          }
         >
           <Text>
             Make sure to replace <code>my-np</code> with the correct node pool
@@ -64,13 +77,23 @@ const ModifyNodePoolGuide: React.FC<IModifyNodePoolGuideProps> = ({
         </CLIGuideStep>
         <CLIGuideStep
           title='3. Update the node pool scaling range'
-          command={`
+          command={
+            provider === Providers.AWS
+              ? `
+          kubectl --context ${context} \\
+            patch awsmachinedeployments.infrastructure.giantswarm.io my-np \\
+            --namespace ${clusterNamespace} \\
+            --type merge \\
+            --patch '{"spec": {"nodePool": {"scaling": {"min": 3, "max": 10}}}}'
+          `
+              : `
           kubectl --context ${context} \\
             patch machinepools.exp.cluster.x-k8s.io my-np \\
             --namespace ${clusterNamespace} \\
             --type merge \\
             --patch '{"metadata": {"annotations": {"cluster.k8s.io/cluster-api-autoscaler-node-group-min-size": "3", "cluster.k8s.io/cluster-api-autoscaler-node-group-max-size": "10"}}}'
-          `}
+          `
+          }
         >
           <Text>
             Again, make sure to replace <code>my-np</code> with the correct node
