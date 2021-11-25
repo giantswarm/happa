cd3d0e416a9753d3c24d90f65d914dd92b7ebf0d
Sandy Kuo <kuo.sandys@gmail.com>
2021-11-25 20:54:11 +0100

    Refactor cluster delete button with Anchor component for more control over style

--- src/components/UI/Display/Cluster/ClusterLabels/DeleteLabelButton.tsx
+++ src/components/UI/Display/Cluster/ClusterLabels/DeleteLabelButton.tsx
@@ -1,10 +1,33 @@
-import React, { ComponentPropsWithRef, FC } from 'react';
-import Button from 'UI/Controls/Button';
+import { Anchor } from 'grommet';
+import React, { FC } from 'react';
+import styled from 'styled-components';
 
-interface IButtonProps extends ComponentPropsWithRef<typeof Button> {}
+interface IButtonProps {}
+
+const StyledAnchor = styled(Anchor)`
+  color: ${({ theme }) => theme.colors.darkBlueLighter4};
+
+  :focus {
+    outline: 0;
+  }
+
+  :focus:not(:focus-visible) {
+    box-shadow: none;
+  }
+
+  i:focus {
+    outline: 0;
+  }
+
+  :hover {
+    color: ${({ theme }) => theme.colors.darkBlueDarker2};
+  }
+`;
 
 const DeleteLabelButton: FC<IButtonProps> = (props) => (
-  <Button size='small' link={true} {...props} />
+  <StyledAnchor size='large' tabIndex={0} {...props}>
+    <i className='fa fa-close' role='presentation' title='Delete' />
+  </StyledAnchor>
 );
 
 export default DeleteLabelButton;
