6ff9e38defda5473655406b733f94271a1877dfa
Sandy Kuo <kuo.sandys@gmail.com>
2021-11-30 12:45:03 +0100

    Add permission hook for node pools

---
+++ src/components/MAPI/workernodes/permissions/usePermissionsForNodePools.ts
@@ -0,0 +1,261 @@
+import { usePermissions } from 'MAPI/permissions/usePermissions';
+import { hasPermission } from 'MAPI/permissions/utils';
+import { Providers } from 'model/constants';
+
+// eslint-disable-next-line complexity
+export function usePermissionsForNodePools(
+  provider: PropertiesOf<typeof Providers>,
+  namespace: string
+) {
+  const { data: permissions } = usePermissions();
+
+  const computed = {
+    canGet: false,
+    canList: false,
+    canUpdate: false,
+    canCreate: false,
+    canDelete: false,
+  };
+
+  if (!permissions) return computed;
+
+  switch (provider) {
+    case Providers.AWS:
+      computed.canCreate =
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'cluster.x-k8s.io',
+          'machinedeployments'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'infrastructure.giantswarm.io',
+          'awsmachinedeployments'
+        );
+
+      computed.canDelete =
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'cluster.x-k8s.io',
+          'machinedeployments'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'infrastructure.giantswarm.io',
+          'awsmachinedeployments'
+        );
+
+      computed.canUpdate =
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'cluster.x-k8s.io',
+          'machinedeployments'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'infrastructure.giantswarm.io',
+          'awsmachinedeployments'
+        );
+
+      computed.canGet =
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'cluster.x-k8s.io',
+          'machinedeployments'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'infrastructure.giantswarm.io',
+          'awsmachinedeployments'
+        );
+
+      computed.canList =
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'cluster.x-k8s.io',
+          'machinedeployments'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'infrastructure.giantswarm.io',
+          'awsmachinedeployments'
+        );
+
+      break;
+
+    case Providers.AZURE:
+      computed.canCreate =
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'exp.cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'create',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        );
+
+      computed.canDelete =
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'exp.cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'delete',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        );
+
+      computed.canUpdate =
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'exp.cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'update',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        );
+
+      computed.canGet =
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'exp.cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'get',
+          'exp.infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        );
+
+      computed.canList =
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'exp.cluster.x-k8s.io',
+          'machinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        ) &&
+        hasPermission(
+          permissions,
+          namespace,
+          'list',
+          'infrastructure.cluster.x-k8s.io',
+          'azuremachinepools'
+        );
+
+      break;
+  }
+
+  return computed;
+}
