abd715564ca82bff150fb3b3eeb46b473acbaba9
Sandy Kuo <kuo.sandys@gmail.com>
2021-12-16 21:56:20 +0100

    Add permissions to kubernetes API widget tests; add test case for not displaying  button

--- src/components/MAPI/clusters/ClusterDetail/__tests__/ClusterDetailWidgetKubernetesAPI.tsx
+++ src/components/MAPI/clusters/ClusterDetail/__tests__/ClusterDetailWidgetKubernetesAPI.tsx
@@ -1,15 +1,34 @@
 import { screen } from '@testing-library/react';
+import { usePermissionsForKeyPairs } from 'MAPI/keypairs/permissions/usePermissionsForKeyPairs';
 import * as capiv1alpha3Mocks from 'test/mockHttpCalls/capiv1alpha3';
 import { renderWithStore } from 'test/renderUtils';
 
 import ClusterDetailWidgetKubernetesAPI from '../ClusterDetailWidgetKubernetesAPI';
 
+const defaultPermissions = {
+  canGet: true,
+  canList: true,
+  canUpdate: true,
+  canCreate: true,
+  canDelete: true,
+};
+
+jest.mock('MAPI/keypairs/permissions/usePermissionsForKeypairs');
+
 describe('ClusterDetailWidgetKubernetesAPI', () => {
   it('renders without crashing', () => {
+    (usePermissionsForKeyPairs as jest.Mock).mockReturnValue(
+      defaultPermissions
+    );
+
     renderWithStore(ClusterDetailWidgetKubernetesAPI, {});
   });
 
   it('displays loading animations if the cluster is still loading', () => {
+    (usePermissionsForKeyPairs as jest.Mock).mockReturnValue(
+      defaultPermissions
+    );
+
     renderWithStore(ClusterDetailWidgetKubernetesAPI, {
       cluster: undefined,
     });
@@ -18,6 +37,10 @@ describe('ClusterDetailWidgetKubernetesAPI', () => {
   });
 
   it('displays the kubernetes API endpoint URL for the cluster', () => {
+    (usePermissionsForKeyPairs as jest.Mock).mockReturnValue(
+      defaultPermissions
+    );
+
     renderWithStore(ClusterDetailWidgetKubernetesAPI, {
       cluster: capiv1alpha3Mocks.randomCluster1,
     });
@@ -26,6 +49,10 @@ describe('ClusterDetailWidgetKubernetesAPI', () => {
   });
 
   it('displays the getting started button', () => {
+    (usePermissionsForKeyPairs as jest.Mock).mockReturnValue(
+      defaultPermissions
+    );
+
     renderWithStore(ClusterDetailWidgetKubernetesAPI, {
       cluster: capiv1alpha3Mocks.randomCluster1,
     });
@@ -35,5 +62,22 @@ describe('ClusterDetailWidgetKubernetesAPI', () => {
         name: 'Get started',
       })
     ).toBeInTheDocument();
+  });
+
+  it('does not display the getting started button if the user does not have permission to create key pairs', () => {
+    (usePermissionsForKeyPairs as jest.Mock).mockReturnValue({
+      ...defaultPermissions,
+      canCreate: false,
+    });
+
+    renderWithStore(ClusterDetailWidgetKubernetesAPI, {
+      cluster: capiv1alpha3Mocks.randomCluster1,
+    });
+
+    expect(
+      screen.queryByRole('button', {
+        name: 'Get started',
+      })
+    ).not.toBeInTheDocument();
   });
 });
