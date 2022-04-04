ed3e5e6cc7b6a34368fee3d08865871ef71bf728
Dmitry Gusev <gusevda90@gmail.com>
2022-04-04 13:44:18 +0300

    Handle the error when items are missing in a release list

--- src/model/services/mapi/releasev1alpha1/getReleaseList.ts
+++ src/model/services/mapi/releasev1alpha1/getReleaseList.ts
@@ -1,4 +1,6 @@
+import { GenericResponseError } from 'model/clients/GenericResponseError';
 import { IHttpClient } from 'model/clients/HttpClient';
+import { StatusCodes } from 'model/constants';
 import * as k8sUrl from 'model/services/mapi/k8sUrl';
 import { IOAuth2Provider } from 'utils/OAuth2/OAuth2';
 
@@ -12,7 +14,22 @@ export function getReleaseList(client: IHttpClient, auth: IOAuth2Provider) {
     kind: 'releases',
   });
 
-  return getResource<IReleaseList>(client, auth, url.toString());
+  return getResource<IReleaseList>(client, auth, url.toString()).then(
+    (data) => {
+      if (data.items === undefined) {
+        const invalidResponseError = new GenericResponseError();
+        invalidResponseError.status = StatusCodes.Ok;
+        invalidResponseError.requestConfig = client.getRequestConfig();
+        invalidResponseError.data = data;
+        invalidResponseError.message =
+          'Invalid response. Release list items are missing.';
+
+        throw invalidResponseError;
+      }
+
+      return data;
+    }
+  );
 }
 
 export function getReleaseListKey() {
