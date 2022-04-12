c478d3b0d608f0913cd590398dee6201ef7785ec
Sandy Kuo <kuo.sandys@gmail.com>
2022-04-12 11:56:53 +0200

    Add test case for displaying app install status in app catalogs page

--- src/components/MAPI/apps/AppList/__tests__/index.tsx
+++ src/components/MAPI/apps/AppList/__tests__/index.tsx
@@ -10,17 +10,24 @@ import AppsProvider from 'MAPI/apps/AppsProvider';
 import { usePermissionsForAppCatalogEntries } from 'MAPI/apps/permissions/usePermissionsForAppCatalogEntries';
 import { usePermissionsForCatalogs } from 'MAPI/apps/permissions/usePermissionsForCatalogs';
 import { StatusCodes } from 'model/constants';
+import { IState } from 'model/stores/state';
 import nock from 'nock';
 import React from 'react';
 import { SWRConfig } from 'swr';
+import { withMarkup } from 'test/assertUtils';
 import * as applicationv1alpha1Mocks from 'test/mockHttpCalls/applicationv1alpha1';
 import * as authorizationv1Mocks from 'test/mockHttpCalls/authorizationv1';
+import * as capiv1beta1Mocks from 'test/mockHttpCalls/capiv1beta1';
+import preloginState from 'test/preloginState';
 import { getComponentWithStore } from 'test/renderUtils';
 import TestOAuth2 from 'utils/OAuth2/TestOAuth2';
 
 import AppList from '../';
 
-function getComponent(props: React.ComponentPropsWithoutRef<typeof AppList>) {
+function getComponent(
+  props: React.ComponentPropsWithoutRef<typeof AppList>,
+  selectedClusterID?: string
+) {
   const history = createMemoryHistory();
   const auth = new TestOAuth2(history, true);
 
@@ -32,10 +39,19 @@ function getComponent(props: React.ComponentPropsWithoutRef<typeof AppList>) {
     </SWRConfig>
   );
 
+  const state = {
+    ...preloginState,
+    main: {
+      ...preloginState.main,
+      loggedInUser: { isAdmin: true },
+      selectedClusterID: selectedClusterID ?? null,
+    },
+  } as IState;
+
   return getComponentWithStore(
     Component,
     props,
-    undefined,
+    state,
     undefined,
     history,
     auth
@@ -247,6 +263,80 @@ describe('AppList', () => {
 
     expect(
       await screen.findByText('No apps found for your search')
+    ).toBeInTheDocument();
+  });
+
+  it('displays whether an app is installed in the current selected cluster', async () => {
+    nock(window.config.mapiEndpoint)
+      .get('/apis/application.giantswarm.io/v1alpha1/catalogs/')
+      .reply(StatusCodes.Ok, applicationv1alpha1Mocks.catalogList);
+    nock(window.config.mapiEndpoint)
+      .post('/apis/authorization.k8s.io/v1/selfsubjectaccessreviews/', {
+        apiVersion: 'authorization.k8s.io/v1',
+        kind: 'SelfSubjectAccessReview',
+        spec: {
+          resourceAttributes: {
+            namespace: '',
+            verb: 'list',
+            group: 'application.giantswarm.io',
+            resource: 'appcatalogentries',
+          },
+        },
+      })
+      .reply(
+        StatusCodes.Ok,
+        authorizationv1Mocks.selfSubjectAccessReviewCanListAppCatalogEntriesAtClusterScope
+      );
+    nock(window.config.mapiEndpoint)
+      .get(
+        '/apis/application.giantswarm.io/v1alpha1/appcatalogentries/?labelSelector=latest%3Dtrue'
+      )
+      .reply(
+        StatusCodes.Ok,
+        applicationv1alpha1Mocks.latestAppCatalogEntryList
+      );
+    nock(window.config.mapiEndpoint)
+      .post('/apis/authorization.k8s.io/v1/selfsubjectaccessreviews/', {
+        apiVersion: 'authorization.k8s.io/v1',
+        kind: 'SelfSubjectAccessReview',
+        spec: {
+          resourceAttributes: {
+            namespace: '',
+            verb: 'list',
+            group: 'cluster.x-k8s.io',
+            resource: 'clusters',
+          },
+        },
+      })
+      .reply(
+        StatusCodes.Ok,
+        authorizationv1Mocks.selfSubjectAccessReviewCanListClustersAtClusterScope
+      );
+    nock(window.config.mapiEndpoint)
+      .get('/apis/cluster.x-k8s.io/v1beta1/clusters/')
+      .reply(StatusCodes.Ok, capiv1beta1Mocks.randomClusterList);
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/application.giantswarm.io/v1alpha1/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.name}/apps/`
+      )
+      .reply(StatusCodes.Ok, applicationv1alpha1Mocks.randomCluster1AppsList);
+
+    render(getComponent({}, capiv1beta1Mocks.randomCluster1.metadata.name));
+
+    await waitForElementToBeRemoved(() =>
+      screen.getAllByLabelText('Loading...')
+    );
+
+    expect(
+      withMarkup(screen.getByText)(
+        ' Your selected cluster is j5y9m and install status will be shown for this cluster.'
+      )
+    ).toBeInTheDocument();
+
+    const appNameContainer = screen.getByText('coredns');
+
+    expect(
+      await within(appNameContainer).findByTitle('Installed in this cluster')
     ).toBeInTheDocument();
   });
 });
