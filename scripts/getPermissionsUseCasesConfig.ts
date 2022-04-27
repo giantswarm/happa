6758a0ebb1b716b32b7e4651aafaeeb7897590a5
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-04-27 11:59:18 +0200

    Parse permissions use cases file (#3385)
    * Make contents of permissions use cases file available on window.config object as JSON data
    
    * Add utility function to get permissions use cases and add relevant types
    
    * Use default schema for loading yaml file contents
    
    * Still set some defaults for config if the configuration content is an empty string
    
    * Fix type of IPermissionsUseCase.scope.namespaces
    
    * Use a replacer function to replace single and double quotes with HTML entities in JSON string
    
    * Reuse getPermissionsUseCaseConfig function in prepare.ts script
    
    * Get rid of unused imports

---
+++ scripts/getPermissionsUseCasesConfig.ts
@@ -0,0 +1,34 @@
+import fs from 'fs/promises';
+
+import yaml from 'js-yaml';
+
+function replacer(_: any, value: any) {
+  if (typeof value === 'string') {
+    return value.replace(/\'/g, '&apos;').replace(/\"/g, '&quot;');
+  }
+  return value;
+}
+
+/**
+ * This function fetches the contents of the permissions use cases
+ * file from the given path and returns its contents as stringified
+ * JSON under the `permissions-use-cases-json` key.
+ */
+export default async function getPermissionsUseCasesConfig(
+  filePath: string
+): Promise<string> {
+  const useCasesFileContents = await fs.readFile(filePath);
+  const useCases = yaml.load(useCasesFileContents.toString()) as Record<
+    string,
+    any
+  >;
+
+  const useCasesObject = {
+    'permissions-use-cases-json': JSON.stringify(
+      useCases['useCases'],
+      replacer
+    ),
+  };
+
+  return yaml.dump(useCasesObject);
+}
