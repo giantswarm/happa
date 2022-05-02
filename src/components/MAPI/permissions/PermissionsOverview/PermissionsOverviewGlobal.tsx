b948e747143530776d9b70b3b97f5fab962a83d5
Sandy Kuo <kuo.sandys@gmail.com>
2022-05-02 18:23:16 +0200

    Display permissions for use cases in Global tab

--- src/components/MAPI/permissions/PermissionsOverview/PermissionsOverviewGlobal.tsx
+++ src/components/MAPI/permissions/PermissionsOverview/PermissionsOverviewGlobal.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { useAuthProvider } from 'Auth/MAPI/MapiAuthProvider';
+import { GenericResponseError } from 'model/clients/GenericResponseError';
+import { getLoggedInUser } from 'model/stores/main/selectors';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import useSWR from 'swr';
 import PermissionsUseCases from 'UI/Display/MAPI/permissions/PermissionsUseCases';
+import { useHttpClientFactory } from 'utils/hooks/useHttpClientFactory';
 
-import { IPermissionsUseCase } from '../types';
+import { IPermissionMap, IPermissionsUseCase } from '../types';
+import {
+  fetchPermissionsAtClusterScope,
+  fetchPermissionsAtClusterScopeKey,
+  getStatusesForUseCases,
+} from '../utils';
 
 interface IPermissionsOverviewGlobalProps {
   useCases: IPermissionsUseCase[];
@@ -10,10 +21,27 @@ interface IPermissionsOverviewGlobalProps {
 const PermissionsOverviewGlobal: React.FC<IPermissionsOverviewGlobalProps> = ({
   useCases,
 }) => {
-  const useCasesStatuses: Record<string, Record<string, boolean>> = {};
-  useCases.forEach((useCase) => {
-    useCasesStatuses[useCase.name] = { '': true };
-  });
+  const clientFactory = useHttpClientFactory();
+  const auth = useAuthProvider();
+  const user = useSelector(getLoggedInUser);
+
+  const { data: permissionsAtClusterScope } = useSWR<
+    IPermissionMap,
+    GenericResponseError
+  >(fetchPermissionsAtClusterScopeKey(user?.email, user?.groups), () =>
+    fetchPermissionsAtClusterScope(
+      clientFactory,
+      auth,
+      user?.email,
+      user?.groups
+    )
+  );
+
+  const useCasesStatuses = useMemo(() => {
+    if (typeof permissionsAtClusterScope === 'undefined') return undefined;
+
+    return getStatusesForUseCases(permissionsAtClusterScope, useCases);
+  }, [permissionsAtClusterScope, useCases]);
 
   return (
     <PermissionsUseCases
