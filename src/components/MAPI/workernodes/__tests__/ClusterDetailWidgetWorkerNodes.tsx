e6255ae9dce7c51a76f2c0fca6b44a63a329c912
Sandy S Kuo <kuo.sandys@gmail.com>
2022-07-15 15:17:46 +0200

    Add tests for ClusterDetailWidgetWorkerNodes on AWS

--- src/components/MAPI/workernodes/__tests__/ClusterDetailWidgetWorkerNodes.tsx
+++ src/components/MAPI/workernodes/__tests__/ClusterDetailWidgetWorkerNodes.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import { StatusCodes } from 'model/constants';
+import { Providers, StatusCodes } from 'model/constants';
 import * as capiexpv1alpha3 from 'model/services/mapi/capiv1alpha3/exp';
 import * as metav1 from 'model/services/mapi/metav1';
 import nock from 'nock';
@@ -10,6 +10,7 @@ import * as capiexpv1alpha3Mocks from 'test/mockHttpCalls/capiv1alpha3/exp';
 import * as capiv1beta1Mocks from 'test/mockHttpCalls/capiv1beta1';
 import * as capzexpv1alpha3Mocks from 'test/mockHttpCalls/capzv1alpha3/exp';
 import * as capzv1beta1Mocks from 'test/mockHttpCalls/capzv1beta1';
+import * as infrav1alpha3Mocks from 'test/mockHttpCalls/infrastructurev1alpha3';
 import { getComponentWithStore } from 'test/renderUtils';
 import TestOAuth2 from 'utils/OAuth2/TestOAuth2';
 
@@ -49,19 +50,17 @@ const defaultPermissions = {
 jest.mock('MAPI/workernodes/permissions/usePermissionsForNodePools');
 
 describe('ClusterDetailWidgetWorkerNodes', () => {
-  it('renders without crashing', () => {
+  beforeAll(() => {
     (usePermissionsForNodePools as jest.Mock).mockReturnValue(
       defaultPermissions
     );
+  });
 
+  it('renders without crashing', () => {
     render(getComponent({}));
   });
 
   it('displays loading animations if the cluster is still loading', () => {
-    (usePermissionsForNodePools as jest.Mock).mockReturnValue(
-      defaultPermissions
-    );
-
     render(
       getComponent({
         cluster: undefined,
@@ -70,12 +69,23 @@ describe('ClusterDetailWidgetWorkerNodes', () => {
 
     expect(screen.getAllByLabelText('Loading...').length).toEqual(4);
   });
+});
 
-  it('displays a placeholder if there are no node pools', async () => {
+describe('ClusterDetailWidgetWorkerNodes on Azure', () => {
+  const provider: PropertiesOf<typeof Providers> =
+    window.config.info.general.provider;
+
+  beforeAll(() => {
+    window.config.info.general.provider = Providers.AZURE;
     (usePermissionsForNodePools as jest.Mock).mockReturnValue(
       defaultPermissions
     );
+  });
+  afterAll(() => {
+    window.config.info.general.provider = provider;
+  });
 
+  it('displays a placeholder if there are no node pools', async () => {
     nock(window.config.mapiEndpoint)
       .get(
         `/apis/exp.cluster.x-k8s.io/v1alpha3/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.namespace}/machinepools/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomCluster1.metadata.name}`
@@ -99,40 +109,7 @@ describe('ClusterDetailWidgetWorkerNodes', () => {
     ).toBeInTheDocument();
   });
 
-  it('does not display the link button to add node pools if the user does not have permissions to do so', async () => {
-    (usePermissionsForNodePools as jest.Mock).mockReturnValue({
-      ...defaultPermissions,
-      canCreate: false,
-    });
-
-    nock(window.config.mapiEndpoint)
-      .get(
-        `/apis/exp.cluster.x-k8s.io/v1alpha3/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.namespace}/machinepools/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomCluster1.metadata.name}`
-      )
-      .reply(StatusCodes.Ok, {
-        apiVersion: 'exp.cluster.x-k8s.io/v1alpha3',
-        kind: capiexpv1alpha3.MachinePoolList,
-        metadata: {},
-        items: [],
-      });
-
-    render(
-      getComponent({
-        cluster: capiv1beta1Mocks.randomCluster1,
-      })
-    );
-
-    expect(await screen.findByText('No node pools')).toBeInTheDocument();
-    expect(
-      screen.queryByRole('button', { name: 'Add node pool' })
-    ).not.toBeInTheDocument();
-  });
-
   it('displays stats about node pools', async () => {
-    (usePermissionsForNodePools as jest.Mock).mockReturnValue(
-      defaultPermissions
-    );
-
     nock(window.config.mapiEndpoint)
       .get(
         `/apis/exp.cluster.x-k8s.io/v1alpha3/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.namespace}/machinepools/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomCluster1.metadata.name}`
@@ -171,10 +148,6 @@ describe('ClusterDetailWidgetWorkerNodes', () => {
   });
 
   it('displays stats about node pools with non-experimental MachinePools and AzureMachinePools', async () => {
-    (usePermissionsForNodePools as jest.Mock).mockReturnValue(
-      defaultPermissions
-    );
-
     nock(window.config.mapiEndpoint)
       .get(
         `/apis/cluster.x-k8s.io/v1beta1/namespaces/${capiv1beta1Mocks.randomCluster3.metadata.namespace}/machinepools/?labelSelector=cluster.x-k8s.io%2Fcluster-name%3D${capiv1beta1Mocks.randomCluster3.metadata.name}`
@@ -199,10 +172,6 @@ describe('ClusterDetailWidgetWorkerNodes', () => {
   });
 
   it('only displays a part of the stats if provider-specific resources fail to load', async () => {
-    (usePermissionsForNodePools as jest.Mock).mockReturnValue(
-      defaultPermissions
-    );
-
     nock(window.config.mapiEndpoint)
       .get(
         `/apis/exp.cluster.x-k8s.io/v1alpha3/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.namespace}/machinepools/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomCluster1.metadata.name}`
@@ -297,6 +266,200 @@ describe('ClusterDetailWidgetWorkerNodes', () => {
 
     expect(await screen.findByLabelText('2 node pools')).toBeInTheDocument();
     expect(await screen.findByLabelText('10 nodes')).toBeInTheDocument();
+    expect(
+      await screen.findByLabelText('CPUs not available')
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByLabelText('GB RAM not available')
+    ).toBeInTheDocument();
+  });
+});
+
+describe('ClusterDetailWidgetWorkerNodes when user cannot create node pools on Azure', () => {
+  const provider: PropertiesOf<typeof Providers> =
+    window.config.info.general.provider;
+
+  beforeAll(() => {
+    window.config.info.general.provider = Providers.AZURE;
+    (usePermissionsForNodePools as jest.Mock).mockReturnValue({
+      ...defaultPermissions,
+      canCreate: false,
+    });
+  });
+  afterAll(() => {
+    window.config.info.general.provider = provider;
+  });
+
+  it('does not display the link button to add node pools if the user does not have permissions to do so', async () => {
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/exp.cluster.x-k8s.io/v1alpha3/namespaces/${capiv1beta1Mocks.randomCluster1.metadata.namespace}/machinepools/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomCluster1.metadata.name}`
+      )
+      .reply(StatusCodes.Ok, {
+        apiVersion: 'exp.cluster.x-k8s.io/v1alpha3',
+        kind: capiexpv1alpha3.MachinePoolList,
+        metadata: {},
+        items: [],
+      });
+
+    render(
+      getComponent({
+        cluster: capiv1beta1Mocks.randomCluster1,
+      })
+    );
+
+    expect(await screen.findByText('No node pools')).toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: 'Add node pool' })
+    ).not.toBeInTheDocument();
+  });
+});
+
+describe('ClusterDetailWidgetWorkerNodes on AWS', () => {
+  const provider: PropertiesOf<typeof Providers> =
+    window.config.info.general.provider;
+
+  beforeAll(() => {
+    window.config.info.general.provider = Providers.AWS;
+    (usePermissionsForNodePools as jest.Mock).mockReturnValue(
+      defaultPermissions
+    );
+  });
+  afterAll(() => {
+    window.config.info.general.provider = provider;
+  });
+
+  it('displays stats about node pools', async () => {
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/cluster.x-k8s.io/v1beta1/namespaces/${capiv1beta1Mocks.randomClusterAWS1.metadata.namespace}/machinedeployments/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomClusterAWS1.metadata.name}`
+      )
+      .reply(
+        StatusCodes.Ok,
+        capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList
+      );
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[0].metadata.name}/`
+      )
+      .reply(
+        StatusCodes.Ok,
+        infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment1
+      );
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[1].metadata.name}/`
+      )
+      .reply(
+        StatusCodes.Ok,
+        infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment2
+      );
+
+    render(
+      getComponent({
+        cluster: capiv1beta1Mocks.randomClusterAWS1,
+      })
+    );
+
+    expect(await screen.findByLabelText('2 node pools')).toBeInTheDocument();
+    expect(await screen.findByLabelText('6 nodes')).toBeInTheDocument();
+    expect(await screen.findByLabelText('24 CPUs')).toBeInTheDocument();
+    expect(await screen.findByLabelText('96 GB RAM')).toBeInTheDocument();
+  });
+
+  it('only displays a part of the stats if provider-specific resources fail to load', async () => {
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/cluster.x-k8s.io/v1beta1/namespaces/${capiv1beta1Mocks.randomClusterAWS1.metadata.namespace}/machinedeployments/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomClusterAWS1.metadata.name}`
+      )
+      .reply(
+        StatusCodes.Ok,
+        capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList
+      );
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[0].metadata.name}/`
+      )
+      .reply(StatusCodes.NotFound);
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[1].metadata.name}/`
+      )
+      .reply(StatusCodes.NotFound);
+
+    render(
+      getComponent({
+        cluster: capiv1beta1Mocks.randomClusterAWS1,
+      })
+    );
+
+    render(
+      getComponent({
+        cluster: capiv1beta1Mocks.randomClusterAWS1,
+      })
+    );
+
+    expect(await screen.findByLabelText('2 node pools')).toBeInTheDocument();
+    expect(await screen.findByLabelText('6 nodes')).toBeInTheDocument();
+    expect(
+      await screen.findByLabelText('CPUs not available')
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByLabelText('GB RAM not available')
+    ).toBeInTheDocument();
+  });
+
+  it('only displays a part of the stats if one of the machine types is unknown', async () => {
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/cluster.x-k8s.io/v1beta1/namespaces/${capiv1beta1Mocks.randomClusterAWS1.metadata.namespace}/machinedeployments/?labelSelector=giantswarm.io%2Fcluster%3D${capiv1beta1Mocks.randomClusterAWS1.metadata.name}`
+      )
+      .reply(
+        StatusCodes.Ok,
+        capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList
+      );
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[0].metadata.name}/`
+      )
+      .reply(
+        StatusCodes.Ok,
+        infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment1
+      );
+
+    nock(window.config.mapiEndpoint)
+      .get(
+        `/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/${capiv1beta1Mocks.randomClusterAWS1MachineDeploymentList.items[1].metadata.name}/`
+      )
+      .reply(StatusCodes.Ok, {
+        ...infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment2,
+        spec: {
+          ...infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment1.spec,
+          provider: {
+            ...infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment1.spec
+              .provider,
+            worker: {
+              ...infrav1alpha3Mocks.randomClusterAWS1AWSMachineDeployment1.spec
+                .provider.worker,
+              instanceType: 'nonexistent-machine',
+            },
+          },
+        },
+      });
+
+    render(
+      getComponent({
+        cluster: capiv1beta1Mocks.randomClusterAWS1,
+      })
+    );
+
+    expect(await screen.findByLabelText('2 node pools')).toBeInTheDocument();
+    expect(await screen.findByLabelText('6 nodes')).toBeInTheDocument();
     expect(
       await screen.findByLabelText('CPUs not available')
     ).toBeInTheDocument();
--- test/mockHttpCalls/infrastructurev1alpha3/awsMachineDeployments.ts
+++ test/mockHttpCalls/infrastructurev1alpha3/awsMachineDeployments.ts
@@ -1,7 +1,7 @@
 import * as infrav1alpha3 from 'model/services/mapi/infrastructurev1alpha3';
 
 // AWSMachineDeployment for randomClusterAWS1's MachineDeploymentList item
-export const randomClusterAWS1AWSMachineDeployment: infrav1alpha3.IAWSMachineDeployment =
+export const randomClusterAWS1AWSMachineDeployment1: infrav1alpha3.IAWSMachineDeployment =
   {
     apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
     kind: 'AWSMachineDeployment',
@@ -28,6 +28,69 @@ export const randomClusterAWS1AWSMachineDeployment: infrav1alpha3.IAWSMachineDep
       resourceVersion: '992723656',
       selfLink:
         '/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/4snbn',
+      uid: '140893be-7c92-46f7-a1dd-5d628d45fc1f',
+    },
+    spec: {
+      nodePool: {
+        description: 'workload',
+        machine: {
+          dockerVolumeSizeGB: 100,
+          kubeletVolumeSizeGB: 100,
+        },
+        scaling: {
+          max: 40,
+          min: 1,
+        },
+      },
+      provider: {
+        availabilityZones: ['eu-central-1b', 'eu-central-1a'],
+        instanceDistribution: {
+          onDemandBaseCapacity: 0,
+          onDemandPercentageAboveBaseCapacity: 100,
+        },
+        worker: {
+          instanceType: 'm4.xlarge',
+          useAlikeInstanceTypes: false,
+        },
+      },
+    },
+    status: {
+      provider: {
+        worker: {
+          instanceTypes: ['m4.xlarge'],
+        },
+      },
+    },
+  };
+
+// AWSMachineDeployment for randomClusterAWS1's MachineDeploymentList item
+export const randomClusterAWS1AWSMachineDeployment2: infrav1alpha3.IAWSMachineDeployment =
+  {
+    apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
+    kind: 'AWSMachineDeployment',
+    metadata: {
+      annotations: {
+        'machine-deployment.giantswarm.io/subnet': '10.1.60.0/24',
+      },
+      creationTimestamp: '2022-05-24T08:31:09Z',
+      finalizers: [
+        'operatorkit.giantswarm.io/aws-operator-machine-deployment-controller',
+        'operatorkit.giantswarm.io/aws-operator-drainer-controller',
+      ],
+      generation: 1,
+      labels: {
+        'aws-operator.giantswarm.io/version': '11.1.0',
+        'cluster.x-k8s.io/cluster-name': 'c7hm5',
+        'giantswarm.io/cluster': 'c7hm5',
+        'giantswarm.io/machine-deployment': 'f72hf',
+        'giantswarm.io/organization': 'org1',
+        'release.giantswarm.io/version': '17.2.0',
+      },
+      name: 'f72hf',
+      namespace: 'org-org1',
+      resourceVersion: '992723656',
+      selfLink:
+        '/apis/infrastructure.giantswarm.io/v1alpha3/namespaces/org-org1/awsmachinedeployments/f72hf',
       uid: '140893be-7c92-46f7-a1dd-5d628d45fc1f',
     },
     spec: {
