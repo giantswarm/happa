0d0d93a4b35d48bf7a34957ccb54f7f2e3b658b5
Sandy S Kuo <kuo.sandys@gmail.com>
2022-07-19 12:05:34 +0200

    Add snapshot tests

--- src/components/UI/Display/MAPI/CLIGuide/__tests__/index.tsx
+++ src/components/UI/Display/MAPI/CLIGuide/__tests__/index.tsx
@@ -1,7 +1,9 @@
 import { fireEvent, screen } from '@testing-library/react';
+import * as React from 'react';
 import { renderWithTheme } from 'test/renderUtils';
 
 import CLIGuide from '..';
+import CLIGuidesList from '../CLIGuidesList';
 
 describe('CLIGuide', () => {
   it('renders without crashing', () => {
@@ -23,6 +25,31 @@ describe('CLIGuide', () => {
     );
 
     expect(await screen.findByText('Just some content')).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+describe('CLIGuidesList', () => {
+  it('renders without crashing', () => {
+    const { container } = renderWithTheme(CLIGuidesList, {
+      title: 'Use the API to …',
+      children: '',
+    });
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders the contents when open', async () => {
+    const { container } = renderWithTheme(CLIGuidesList, {
+      title: 'Use the API to …',
+      children: <CLIGuide title='Get something from somewhere' />,
+    });
+
+    fireEvent.click(screen.getByRole('tab', { name: /Use the API to …/ }));
+
+    expect(
+      await screen.findByText('Get something from somewhere')
+    ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
