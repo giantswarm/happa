45c24c2a51455825626e800234fb7af27ecd2449
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-08-12 18:46:30 +0200

    Add test cases for WorkerNodesNodePoolListItem on AWS

--- test/mockHttpCalls/capiv1beta1/machineDeployments.ts
+++ test/mockHttpCalls/capiv1beta1/machineDeployments.ts
@@ -1,5 +1,118 @@
 import * as capiv1beta1 from 'model/services/mapi/capiv1beta1';
 
+// MachineDeployment1 for randomClusterAWS1
+export const randomClusterAWS1MachineDeployment1: capiv1beta1.IMachineDeployment =
+  {
+    apiVersion: 'cluster.x-k8s.io/v1beta1',
+    kind: 'MachineDeployment',
+    metadata: {
+      annotations: {
+        'node.giantswarm.io/cgroupv1': '',
+      },
+      creationTimestamp: '2022-05-24T08:31:11Z',
+      finalizers: [
+        'operatorkit.giantswarm.io/cluster-operator-machine-deployment-controller',
+      ],
+      generation: 1,
+      labels: {
+        'cluster-operator.giantswarm.io/version': '4.0.1',
+        'cluster.x-k8s.io/cluster-name': 'c7hm5',
+        'giantswarm.io/cluster': 'c7hm5',
+        'giantswarm.io/machine-deployment': '4snbn',
+        'giantswarm.io/organization': 'org1',
+        'release.giantswarm.io/version': '17.2.0',
+      },
+      name: '4snbn',
+      namespace: 'org-org1',
+      resourceVersion: '1089201040',
+      selfLink:
+        '/apis/cluster.x-k8s.io/v1beta1/namespaces/org-org1/machinedeployments/4snbn',
+      uid: '41611b5d-ba3c-4c7f-8761-18786dc4ee99',
+    },
+    spec: {
+      clusterName: 'c7hm5',
+      replicas: 1,
+      selector: {},
+      template: {
+        metadata: { name: '' },
+        spec: {
+          bootstrap: {},
+          clusterName: 'c7hm5',
+          infrastructureRef: {
+            apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
+            kind: 'AWSMachineDeployment',
+            name: '4snbn',
+            namespace: 'org-org1',
+            resourceVersion: '256658380',
+            uid: 'e1a16ab1-b089-498d-93f9-6c8372b400b1',
+          },
+        },
+      },
+    },
+    status: {
+      availableReplicas: 0,
+      readyReplicas: 3,
+      replicas: 3,
+      unavailableReplicas: 0,
+      updatedReplicas: 0,
+    },
+  };
+// MachineDeployment2 for randomClusterAWS1
+export const randomClusterAWS1MachineDeployment2: capiv1beta1.IMachineDeployment =
+  {
+    apiVersion: 'cluster.x-k8s.io/v1beta1',
+    kind: 'MachineDeployment',
+    metadata: {
+      annotations: {
+        'node.giantswarm.io/cgroupv1': '',
+      },
+      creationTimestamp: '2022-05-24T08:31:11Z',
+      finalizers: [
+        'operatorkit.giantswarm.io/cluster-operator-machine-deployment-controller',
+      ],
+      generation: 1,
+      labels: {
+        'cluster-operator.giantswarm.io/version': '4.0.1',
+        'cluster.x-k8s.io/cluster-name': 'c7hm5',
+        'giantswarm.io/cluster': 'c7hm5',
+        'giantswarm.io/machine-deployment': 'vud0a',
+        'giantswarm.io/organization': 'org1',
+        'release.giantswarm.io/version': '17.2.0',
+      },
+      name: 'vud0a',
+      namespace: 'org-org1',
+      resourceVersion: '1087343787',
+      selfLink:
+        '/apis/cluster.x-k8s.io/v1beta1/namespaces/org-org1/machinedeployments/vud0a',
+      uid: '3c52c5d2-9f2b-4feb-abbb-9cc2813a00aa',
+    },
+    spec: {
+      clusterName: 'c7hm5',
+      replicas: 1,
+      selector: {},
+      template: {
+        metadata: { name: '' },
+        spec: {
+          bootstrap: {},
+          clusterName: 'c7hm5',
+          infrastructureRef: {
+            apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
+            kind: 'AWSMachineDeployment',
+            name: 'vud0a',
+            namespace: 'org-org1',
+          },
+        },
+      },
+    },
+    status: {
+      availableReplicas: 0,
+      readyReplicas: 3,
+      replicas: 3,
+      unavailableReplicas: 0,
+      updatedReplicas: 0,
+    },
+  };
+
 // MachineDeploymentList for randomClusterAWS1
 export const randomClusterAWS1MachineDeploymentList: capiv1beta1.IMachineDeploymentList =
   {
@@ -11,114 +124,8 @@ export const randomClusterAWS1MachineDeploymentList: capiv1beta1.IMachineDeploym
         '/apis/cluster.x-k8s.io/v1beta1/namespaces/org-org1/machinedeployments/',
     },
     items: [
-      {
-        apiVersion: 'cluster.x-k8s.io/v1beta1',
-        kind: 'MachineDeployment',
-        metadata: {
-          annotations: {
-            'node.giantswarm.io/cgroupv1': '',
-          },
-          creationTimestamp: '2022-05-24T08:31:11Z',
-          finalizers: [
-            'operatorkit.giantswarm.io/cluster-operator-machine-deployment-controller',
-          ],
-          generation: 1,
-          labels: {
-            'cluster-operator.giantswarm.io/version': '4.0.1',
-            'cluster.x-k8s.io/cluster-name': 'c7hm5',
-            'giantswarm.io/cluster': 'c7hm5',
-            'giantswarm.io/machine-deployment': '4snbn',
-            'giantswarm.io/organization': 'org1',
-            'release.giantswarm.io/version': '17.2.0',
-          },
-          name: '4snbn',
-          namespace: 'org-org1',
-          resourceVersion: '1089201040',
-          selfLink:
-            '/apis/cluster.x-k8s.io/v1beta1/namespaces/org-org1/machinedeployments/4snbn',
-          uid: '41611b5d-ba3c-4c7f-8761-18786dc4ee99',
-        },
-        spec: {
-          clusterName: 'c7hm5',
-          replicas: 1,
-          selector: {},
-          template: {
-            metadata: { name: '' },
-            spec: {
-              bootstrap: {},
-              clusterName: 'c7hm5',
-              infrastructureRef: {
-                apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
-                kind: 'AWSMachineDeployment',
-                name: '4snbn',
-                namespace: 'org-org1',
-                resourceVersion: '256658380',
-                uid: 'e1a16ab1-b089-498d-93f9-6c8372b400b1',
-              },
-            },
-          },
-        },
-        status: {
-          availableReplicas: 0,
-          readyReplicas: 3,
-          replicas: 3,
-          unavailableReplicas: 0,
-          updatedReplicas: 0,
-        },
-      },
-      {
-        apiVersion: 'cluster.x-k8s.io/v1beta1',
-        kind: 'MachineDeployment',
-        metadata: {
-          annotations: {
-            'node.giantswarm.io/cgroupv1': '',
-          },
-          creationTimestamp: '2022-05-24T08:31:11Z',
-          finalizers: [
-            'operatorkit.giantswarm.io/cluster-operator-machine-deployment-controller',
-          ],
-          generation: 1,
-          labels: {
-            'cluster-operator.giantswarm.io/version': '4.0.1',
-            'cluster.x-k8s.io/cluster-name': 'c7hm5',
-            'giantswarm.io/cluster': 'c7hm5',
-            'giantswarm.io/machine-deployment': 'vud0a',
-            'giantswarm.io/organization': 'org1',
-            'release.giantswarm.io/version': '17.2.0',
-          },
-          name: 'vud0a',
-          namespace: 'org-org1',
-          resourceVersion: '1087343787',
-          selfLink:
-            '/apis/cluster.x-k8s.io/v1beta1/namespaces/org-org1/machinedeployments/vud0a',
-          uid: '3c52c5d2-9f2b-4feb-abbb-9cc2813a00aa',
-        },
-        spec: {
-          clusterName: 'c7hm5',
-          replicas: 1,
-          selector: {},
-          template: {
-            metadata: { name: '' },
-            spec: {
-              bootstrap: {},
-              clusterName: 'c7hm5',
-              infrastructureRef: {
-                apiVersion: 'infrastructure.giantswarm.io/v1alpha3',
-                kind: 'AWSMachineDeployment',
-                name: 'vud0a',
-                namespace: 'org-org1',
-              },
-            },
-          },
-        },
-        status: {
-          availableReplicas: 0,
-          readyReplicas: 3,
-          replicas: 3,
-          unavailableReplicas: 0,
-          updatedReplicas: 0,
-        },
-      },
+      randomClusterAWS1MachineDeployment1,
+      randomClusterAWS1MachineDeployment2,
     ],
   };
 
