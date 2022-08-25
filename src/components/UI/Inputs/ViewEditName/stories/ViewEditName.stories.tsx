350a89b3a82401ce78cbd72c873e017626accb8b
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-08-25 18:07:44 +0200

    Align read-only version of ViewAndEditName component and add Stories to document variants

---
+++ src/components/UI/Inputs/ViewEditName/stories/ViewEditName.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@storybook/react';
+
+import ViewAndEditName from '..';
+
+export { Simple } from './Simple';
+export { Unauthorized } from './Unauthorized';
+export { ReadOnly } from './ReadOnly';
+
+export default {
+  title: 'Inputs/ViewAndEditName',
+  component: ViewAndEditName,
+} as Meta;
