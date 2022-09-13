4424154cd4a0951b65561c27e40afdbf070b124a
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-09-13 16:29:40 +0200

    Display --cluster-admin flag for CLI guide login step if the current logged in user is an admin and not impersonating a non-admin

--- src/components/MAPI/guides/LoginGuideStep.tsx
+++ src/components/MAPI/guides/LoginGuideStep.tsx
@@ -1,5 +1,10 @@
 import { getK8sAPIUrl } from 'MAPI/utils';
+import {
+  getIsImpersonatingNonAdmin,
+  getUserIsAdmin,
+} from 'model/stores/main/selectors';
 import React, { useRef } from 'react';
+import { useSelector } from 'react-redux';
 import CLIGuideStep from 'UI/Display/MAPI/CLIGuide/CLIGuideStep';
 
 interface ILoginGuideStepProps
@@ -14,13 +19,20 @@ const LoginGuideStep: React.FC<
   const provider = window.config.info.general.provider;
   const k8sAPIUrl = useRef(getK8sAPIUrl(provider));
 
+  const isAdmin = useSelector(getUserIsAdmin);
+  const isImpersonatingNonAdmin = useSelector(getIsImpersonatingNonAdmin);
+
+  const displayClusterAdminFlag = isAdmin && !isImpersonatingNonAdmin;
+
   return (
     <CLIGuideStep
       {...props}
       title={`
           1. Make sure you are logged in and have the right context selected for
           this installation's Management API.`}
-      command={`kubectl gs login ${k8sAPIUrl.current}`}
+      command={`kubectl gs login ${k8sAPIUrl.current}${
+        displayClusterAdminFlag ? ' --cluster-admin' : ''
+      }`}
     />
   );
 };
