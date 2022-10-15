ecdec27660b04718ad0c736332d12d95c2594ea8
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-10-15 22:53:36 +0200

    Catch errors

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,3 +1,7 @@
 export function log(message: string, newLine: boolean = true): void {
   process.stdout.write(`${message}${newLine ? '\n' : ''}`);
 }
+
+export function error(message: string): void {
+  process.stderr.write(`${message}`);
+}
