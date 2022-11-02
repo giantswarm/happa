86666bf4f320c5d0642c58abf2924515030cf7c7
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-11-02 15:53:03 +0100

    Use resource kind TS type instead of string

--- src/components/MAPI/clusters/utils.ts
+++ src/components/MAPI/clusters/utils.ts
@@ -18,6 +18,7 @@ import {
 import { IProviderNodePoolForNodePool } from 'MAPI/workernodes/utils';
 import { GenericResponse } from 'model/clients/GenericResponse';
 import { Constants, Providers } from 'model/constants';
+import * as capgv1beta1 from 'model/services/mapi/capgv1beta1';
 import * as capiv1beta1 from 'model/services/mapi/capiv1beta1';
 import * as capzv1beta1 from 'model/services/mapi/capzv1beta1';
 import * as corev1 from 'model/services/mapi/corev1';
@@ -1148,7 +1149,7 @@ export function fetchControlPlaneNodesK8sVersionsKey(
 }
 
 export function getClusterCreationDuration(cluster: Cluster): string {
-  if (cluster.spec?.infrastructureRef?.kind === 'GCPCluster') {
+  if (cluster.spec?.infrastructureRef?.kind === capgv1beta1.GCPCluster) {
     return '5 minutes';
   }
 
