f75e7c09304a74ae127881cb7a0a52557bcdb1b9
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-11-08 09:13:29 +0100

    Provide link to catalog and catalog item for installed apps (#3863)
    * Display link to installed app details page if the user has permission to inspect the appcatalogentry
    
    * Display link to app catalogs page with installed app's catalog preselected, if the user has permissions to list catalogs
    
    * Add test cases
    
    * Don't try to deconstruct state if it's undefined

--- src/components/MAPI/apps/ClusterDetailAppListWidgetName.tsx
+++ src/components/MAPI/apps/ClusterDetailAppListWidgetName.tsx
@@ -1,8 +1,14 @@
 import { Text } from 'grommet';
+import { AppsRoutes } from 'model/constants/routes';
 import * as applicationv1alpha1 from 'model/services/mapi/applicationv1alpha1';
-import React from 'react';
+import { isAppManagedByFlux } from 'model/services/mapi/applicationv1alpha1';
+import React, { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import ClusterDetailAppListWidget from 'UI/Display/MAPI/apps/ClusterDetailAppListWidget';
 import OptionalValue from 'UI/Display/OptionalValue/OptionalValue';
+import RoutePath from 'utils/routePath';
+
+import { normalizeAppVersion } from './utils';
 
 interface IClusterDetailAppListWidgetNameProps
   extends Omit<
@@ -10,15 +16,43 @@ interface IClusterDetailAppListWidgetNameProps
     'title'
   > {
   app?: applicationv1alpha1.IApp;
+  canListAppCatalogEntries?: boolean;
 }
 
 const ClusterDetailAppListWidgetName: React.FC<
   React.PropsWithChildren<IClusterDetailAppListWidgetNameProps>
-> = ({ app, ...props }) => {
+> = ({ app, canListAppCatalogEntries, ...props }) => {
+  const currentVersion = useMemo(() => {
+    if (!app) return undefined;
+    const appVersion = applicationv1alpha1.getAppCurrentVersion(app);
+
+    return isAppManagedByFlux(app)
+      ? normalizeAppVersion(appVersion)
+      : appVersion;
+  }, [app]);
+
+  const appPath = useMemo(() => {
+    if (!app || !currentVersion) return '';
+
+    return RoutePath.createUsablePath(AppsRoutes.AppDetail, {
+      catalogName: app.spec.catalog,
+      app: app.spec.name,
+      version: currentVersion,
+    });
+  }, [app, currentVersion]);
+
   return (
     <ClusterDetailAppListWidget title='App name' titleColor='text' {...props}>
       <OptionalValue value={app?.spec.name} loaderWidth={100}>
-        {(value) => <Text aria-label={`App name: ${value}`}>{value}</Text>}
+        {(value) =>
+          canListAppCatalogEntries && appPath ? (
+            <Link to={appPath}>
+              <Text aria-label={`App name: ${value}`}>{value}</Text>
+            </Link>
+          ) : (
+            <Text aria-label={`App name: ${value}`}>{value}</Text>
+          )
+        }
       </OptionalValue>
     </ClusterDetailAppListWidget>
   );
