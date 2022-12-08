ade0ca760b111c8d614ef9608533c83713b58555
Dmitry Gusev <gusevda90@gmail.com>
2022-12-08 16:36:13 +0300

    Fix generateUID util function

--- src/components/MAPI/__tests__/utils.ts
+++ src/components/MAPI/__tests__/utils.ts
@@ -8,15 +8,23 @@ describe('mapi::utils', () => {
   describe('generateUID', () => {
     it('generates unique IDs', () => {
       const tries = 250;
+      const minLength = 3;
+      const maxLength = 10;
 
       const values = new Set<string>();
       for (let i = 0; i < tries; i++) {
-        const id = generateUID(5);
+        const randomLength = Math.floor(minLength + maxLength * Math.random());
+        const id = generateUID(randomLength);
 
+        expect(id).toHaveLength(randomLength);
         expect(values.has(id)).toBeFalsy();
 
         values.add(id);
       }
+    });
+
+    it('throws an error if the length parameter is incorrect', () => {
+      expect(() => generateUID(2)).toThrow('Length is too short');
     });
   });
 
