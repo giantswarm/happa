e9cfa589f0e1f11175876b67ee0aa2a097416c54
Dmitry Gusev <gusevda90@gmail.com>
2022-12-20 15:48:19 +0300

    Fix BaseInputTemplate component

--- src/components/UI/JSONSchemaForm/BaseInputTemplate/index.tsx
+++ src/components/UI/JSONSchemaForm/BaseInputTemplate/index.tsx
@@ -1,5 +1,4 @@
 import { getInputProps, WidgetProps } from '@rjsf/utils';
-import { Box } from 'grommet';
 import React from 'react';
 import NumberPicker from 'UI/Inputs/NumberPicker';
 import TextInput from 'UI/Inputs/TextInput';
@@ -35,39 +34,42 @@ const BaseInputTemplate: React.FC<WidgetProps> = ({
 
   const displayLabel = simplifiedView ? '' : label;
 
-  return (
-    <Box
-      margin={{ top: simplifiedView ? 'small' : 'none' }}
-      width={{ max: inputProps.type === 'number' ? 'small' : 'auto' }}
-    >
-      {inputProps.type === 'number' ? (
-        <NumberPicker
-          id={id}
-          label={displayLabel}
-          help={description}
-          value={value}
-          placeholder={placeholder}
-          disabled={disabled}
-          readOnly={readonly}
-          min={inputProps.min}
-          max={inputProps.max}
-          step={inputProps.step}
-          onChange={handleNumberChange}
-        />
-      ) : (
-        <TextInput
-          id={id}
-          label={displayLabel}
-          help={description}
-          value={value}
-          placeholder={placeholder}
-          disabled={disabled}
-          readOnly={readonly}
-          onChange={handleChange}
-          {...inputProps}
-        />
-      )}
-    </Box>
+  const margin = {
+    top: simplifiedView ? 'small' : 'none',
+    bottom: 'small',
+  };
+
+  return inputProps.type === 'number' ? (
+    <NumberPicker
+      id={id}
+      label={displayLabel}
+      help={description}
+      value={value}
+      placeholder={placeholder}
+      disabled={disabled}
+      readOnly={readonly}
+      min={inputProps.min}
+      max={inputProps.max}
+      step={inputProps.step}
+      margin={margin}
+      contentProps={{
+        width: { max: inputProps.type === 'number' ? 'small' : 'auto' },
+      }}
+      onChange={handleNumberChange}
+    />
+  ) : (
+    <TextInput
+      id={id}
+      label={displayLabel}
+      help={description}
+      value={value}
+      placeholder={placeholder}
+      disabled={disabled}
+      readOnly={readonly}
+      margin={margin}
+      onChange={handleChange}
+      {...inputProps}
+    />
   );
 };
 
