415d4e800d82a0c9e502d7b0abf6848ec92e598b
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2023-03-03 17:09:26 +0100

    Route cluster creation to page based on app bundles for CAPI installations; make it admin only

--- src/components/MAPI/clusters/Cluster.tsx
+++ src/components/MAPI/clusters/Cluster.tsx
@@ -1,17 +1,28 @@
+import { ProviderFlavors } from 'model/constants';
 import { OrganizationsRoutes } from 'model/constants/routes';
+import { getUserIsAdmin } from 'model/stores/main/selectors';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Switch } from 'react-router';
 import Route from 'Route';
 
 import ClusterDetail from './ClusterDetail';
 import CreateCluster from './CreateCluster/CreateCluster';
+import CreateClusterAppBundles from './CreateClusterAppBundles';
 import GettingStarted from './GettingStarted';
 
 const Cluster: React.FC<React.PropsWithChildren<{}>> = () => {
+  const providerFlavor = window.config.info.general.providerFlavor;
+  const isAdmin = useSelector(getUserIsAdmin);
+
   return (
     <Switch>
       <Route
-        component={CreateCluster}
+        component={
+          isAdmin && providerFlavor === ProviderFlavors.CAPI
+            ? CreateClusterAppBundles
+            : CreateCluster
+        }
         exact
         path={OrganizationsRoutes.Clusters.New}
       />
